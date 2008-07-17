9a57f024c8ef8fd1c9ecbb826bbb7372ba7451b6
Rubén S. Montero <rubensm@dacya.ucm.es>
2008-07-17 19:42:17 +0000

    Now we have a more consistent src tree
    git-svn-id: http://svn.opennebula.org/trunk@61 3034c82b-c49b-4eb3-8279-a7acafdc01c0

--- install.sh
+++ install.sh
@@ -25,50 +25,68 @@ for d in $DIRS; do
     mkdir -p $DST_DIR$d
 done
 
+# --- Programs & Scripts---
+
 inst_ln src/nebula/oned bin
 inst_ln src/scheduler/mm_sched bin
+
 inst_ln src/client/ruby/onevm bin
 inst_ln src/client/ruby/onehost bin
 
-inst_cp share/etc/oned.conf etc
-inst_ln share/etc/mad/defaultrc etc
-
 inst_ln share/scripts/madcommon.sh libexec
+inst_ln share/scripts/one bin
 
-inst_ln share/etc/mad/vmm_xenrc etc/vmm_xen
-inst_ln share/etc/default/vmm_xen.conf etc/vmm_xen
-inst_ln share/etc/mad/vmm_kvmrc etc/vmm_kvm
-inst_ln share/etc/default/vmm_kvm.conf etc/vmm_kvm
+# --- Ruby Libraries
 
-inst_ln src/vmm_mad/xen/one_vmm_xen.rb bin
-inst_ln src/vmm_mad/xen/one_vmm_xen bin
+inst_ln src/mad/ruby/one_mad.rb lib/ruby
+inst_ln src/mad/ruby/one_ssh.rb lib/ruby
 
-inst_ln src/vmm_mad/kvm/one_vmm_kvm.rb bin
-inst_ln src/vmm_mad/kvm/one_vmm_kvm bin
-
-inst_ln src/im_mad/im_ssh/one_im_ssh.rb bin
-inst_ln src/im_mad/im_ssh/one_im_ssh bin
-inst_ln src/im_mad/im_ssh/one_ssh.rb lib/ruby
-
-inst_ln share/etc/mad/im_kvmrc etc/im_kvm
-inst_cp src/im_mad/kvm/im_kvm.conf etc/im_kvm
-inst_cp src/im_mad/xen/im_xen.conf etc/im_xen
-inst_ln share/etc/mad/im_xenrc etc/im_xen
-
-
-inst_ln src/vmm_mad/xen/one_mad.rb lib/ruby
 inst_ln src/client/ruby/one.rb lib/ruby
 inst_ln src/client/ruby/client_utilities.rb lib/ruby
 inst_ln src/client/ruby/command_parse.rb lib/ruby
 
-inst_ln src/im_mad/im_ssh/architecture.sh lib/im_probes
-inst_ln src/im_mad/im_ssh/cpu.sh lib/im_probes
-inst_ln src/im_mad/im_ssh/name.sh lib/im_probes
+# --- ONE configuration files ---
+
+inst_cp share/etc/oned.conf etc
+inst_cp share/etc/defaultrc etc
+
+# --- XEN driver & configuration files ---
+
+inst_ln src/vmm_mad/xen/one_vmm_xen.rb bin
+inst_ln src/vmm_mad/xen/one_vmm_xen bin
+
 inst_ln src/im_mad/xen/xen.rb lib/im_probes
+
+inst_cp src/vmm_mad/xen/vmm_xenrc etc/vmm_xen
+inst_cp src/vmm_mad/xen/vmm_xen.conf etc/vmm_xen
+
+inst_cp src/im_mad/xen/im_xenrc etc/im_xen
+inst_cp src/im_mad/xen/im_xen.conf etc/im_xen
+
+# --- KVM driver & configuration files ---
+
+inst_ln src/vmm_mad/kvm/one_vmm_kvm.rb bin
+inst_ln src/vmm_mad/kvm/one_vmm_kvm bin
+
 inst_ln src/im_mad/kvm/kvm.rb lib/im_probes
 
-inst_cp share/scripts/one bin
+inst_cp src/vmm_mad/kvm/vmm_kvmrc etc/vmm_kvm
+inst_cp src/vmm_mad/kvm/vmm_kvm.conf etc/vmm_kvm
+
+inst_cp src/im_mad/kvm/im_kvmrc etc/im_kvm
+inst_cp src/im_mad/kvm/im_kvm.conf etc/im_kvm
+
+# --- Information driver & probes ---
+
+inst_ln src/im_mad/im_ssh/one_im_ssh.rb bin
+inst_ln src/im_mad/im_ssh/one_im_ssh bin
+
+inst_ln src/im_mad/host_probes/architecture.sh lib/im_probes
+inst_ln src/im_mad/host_probes/cpu.sh lib/im_probes
+inst_ln src/im_mad/host_probes/name.sh lib/im_probes
+
+# --- Examples ---
 
 inst_cp share/examples/vm.template share/examples
-
+inst_cp share/examples/vm.schema share/examples
 
