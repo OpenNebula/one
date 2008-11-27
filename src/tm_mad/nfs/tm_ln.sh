c65c21a460d4ac289523c24e3cd2de789d69a675
Javier Fontán Muiños <jfontan@fdi.ucm.es>
2008-11-27 18:24:38 +0000

    tm_nfs: LN command now creates vm directory
    git-svn-id: http://svn.opennebula.org/one/trunk@268 3034c82b-c49b-4eb3-8279-a7acafdc01c0

--- src/tm_mad/nfs/tm_ln.sh
+++ src/tm_mad/nfs/tm_ln.sh
@@ -8,5 +8,12 @@ DST=$2
 SRC_PATH=`arg_path $SRC`
 DST_PATH=`arg_path $DST`
 
+DST_DIR=`dirname $DST_PATH`
+
+log "Creating directory $DST_DIR"
+exec_and_log "mkdir -p $DST_DIR"
+exec_and_log "chmod a+w $DST_DIR"
+
 log "Link $SRC_PATH"
 exec_and_log "ln -s $SRC_PATH $DST_PATH"
+
