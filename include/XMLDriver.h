075e016cf56844cf6609aabbbd59fa63862dc669
Rubén S. Montero <rubensm@dacya.ucm.es>
2009-01-26 18:27:03 +0000

    New XML driver implementation
    git-svn-id: http://svn.opennebula.org/one/trunk@331 3034c82b-c49b-4eb3-8279-a7acafdc01c0

---
+++ include/XMLDriver.h
@@ -0,0 +1,58 @@
+/* -------------------------------------------------------------------------- */
+/* Copyright 2002-2009, Distributed Systems Architecture Group, Universidad   */
+/* Complutense de Madrid (dsa-research.org)                                   */
+/*                                                                            */
+/* Licensed under the Apache License, Version 2.0 (the "License"); you may    */
+/* not use this file except in compliance with the License. You may obtain    */
+/* a copy of the License at                                                   */
+/*                                                                            */
+/* http://www.apache.org/licenses/LICENSE-2.0                                 */
+/*                                                                            */
+/* Unless required by applicable law or agreed to in writing, software        */
+/* distributed under the License is distributed on an "AS IS" BASIS,          */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   */
+/* See the License for the specific language governing permissions and        */
+/* limitations under the License.                                             */
+/* -------------------------------------------------------------------------- */
+
+#ifndef XML_DRIVER_H_
+#define XML_DRIVER_H_
+
+#include <map>
+#include <string>
+#include <sstream>
+#include "VirtualMachineManagerDriver.h"
+
+using namespace std;
+
+/**
+ *  XML Driver class implements a Generic VM Manager Driver that uses a neutral
+ *  XML representation for the VM templates.
+ */
+class XMLDriver : public VirtualMachineManagerDriver
+{
+public:
+
+    XMLDriver(
+        int userid,
+        const map<string,string> &attrs,
+        bool sudo,
+        VirtualMachinePool *    pool):
+            VirtualMachineManagerDriver(userid, attrs,sudo,pool)
+    {};
+
+    ~XMLDriver(){};
+
+private:    
+    /**
+     *  Generates a neutral XML deployment file:
+     *    @param vm pointer to a virtual machine
+     *    @param file_name to generate the deployment description
+     *    @return 0 on success
+     */
+    int deployment_description(
+        const VirtualMachine *  vm, 
+        const string&           file_name) const;
+};
+
+#endif /*XML_DRIVER_H_*/
---
+++ src/vmm/XMLDriver.cc
@@ -0,0 +1,46 @@
+/* -------------------------------------------------------------------------- */
+/* Copyright 2002-2009, Distributed Systems Architecture Group, Universidad   */
+/* Complutense de Madrid (dsa-research.org)                                   */
+/*                                                                            */
+/* Licensed under the Apache License, Version 2.0 (the "License"); you may    */
+/* not use this file except in compliance with the License. You may obtain    */
+/* a copy of the License at                                                   */
+/*                                                                            */
+/* http://www.apache.org/licenses/LICENSE-2.0                                 */
+/*                                                                            */
+/* Unless required by applicable law or agreed to in writing, software        */
+/* distributed under the License is distributed on an "AS IS" BASIS,          */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   */
+/* See the License for the specific language governing permissions and        */
+/* limitations under the License.                                             */
+/* -------------------------------------------------------------------------- */
+
+#include "XMLDriver.h"
+#include "Nebula.h"
+#include <sstream>
+#include <fstream>
+#include <math.h>
+
+int XMLDriver::deployment_description(
+    const VirtualMachine *  vm,
+    const string&           file_name) const
+{
+    ofstream    file;
+    string		xml;
+    
+    file.open(file_name.c_str(), ios::out);
+    
+    if (file.fail() == true)
+    {
+    	vm->log("VMM", Log::ERROR, "Could not open XML deployment file.");
+    	return -1;
+    }
+    
+    vm->template_to_xml(xml);
+    
+    file << xml ;
+
+    file.close();
+
+    return 0;
+}
