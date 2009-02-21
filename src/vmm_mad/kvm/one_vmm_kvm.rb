e91583d8aec3fd52d9c2f9dc9f838cd942d78757
Rubén S. Montero <rubensm@dacya.ucm.es>
2009-02-21 01:46:26 +0000

    New version of the KVM driver using the OpenNebulaDriver engine
    git-svn-id: http://svn.opennebula.org/one/trunk@372 3034c82b-c49b-4eb3-8279-a7acafdc01c0

--- src/vmm_mad/kvm/one_vmm_kvm.rb
+++ src/vmm_mad/kvm/one_vmm_kvm.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-
 # -------------------------------------------------------------------------- #
 # Copyright 2002-2009, Distributed Systems Architecture Group, Universidad   #
 # Complutense de Madrid (dsa-research.org)                                   #
@@ -17,243 +16,179 @@
 # limitations under the License.                                             #
 #--------------------------------------------------------------------------- #
 
-ONE_LOCATION=ENV["ONE_LOCATION"]
-DEBUG_LEVEL=ENV["ONE_MAD_DEBUG"]
+# ---------------------------------------------------------------------------- #
+# Set up the environment for the driver                                        #
+# ---------------------------------------------------------------------------- #
+
+ONE_LOCATION = ENV["ONE_LOCATION"]
 
 if !ONE_LOCATION
-    RUBY_LIB_LOCATION="/usr/lib/one/ruby"
+    RUBY_LIB_LOCATION = "/usr/lib/one/ruby"
+    ETC_LOCATION      = "/etc/one/"
 else
-    RUBY_LIB_LOCATION=ONE_LOCATION+"/lib/ruby"
+    RUBY_LIB_LOCATION = ONE_LOCATION + "/lib/ruby"
+    ETC_LOCATION      = ONE_LOCATION + "/etc/"
 end
 
 $: << RUBY_LIB_LOCATION
 
 require 'pp'
+require "VirtualMachineDriver"
+require "CommandManager"
 
-require 'one_mad'
-require 'one_ssh'
-require 'open3'
+# ---------------------------------------------------------------------------- #
+# The main class for the EC2 driver                                            #
+# ---------------------------------------------------------------------------- #
 
+class LibVirtDriver < VirtualMachineDriver
 
-class DM < ONEMad
-    
-    ###########################
-    # Constructor             #
-    ###########################
-    
-    def initialize
-        super(5, 4)
-        
-        if DEBUG_LEVEL and !DEBUG_LEVEL.empty? 
-            set_logger(STDERR,DEBUG_LEVEL)
+    # ------------------------------------------------------------------------ #
+    # Libvirt commands constants                                               #
+    # ------------------------------------------------------------------------ #
+    QEMU_PROTOCOL = "qemu+ssh"
+    LIBVIRT       = {
+        :create   => "virsh create",
+        :shutdown => "virsh shutdown",
+        :cancel   => "virsh destroy",
+        :save     => "virsh save",
+        :restore  => "virsh restore",
+        :migrate  => "virsh migrate --live",
+        :poll     => "virsh dominfo"
+    }
+
+    # ------------------------------------------------------------------------ #
+    # LibvirtDriver constructor                                                #
+    # ------------------------------------------------------------------------ #
+    def initialize()
+        super(15,true)
+    end
+
+    # ------------------------------------------------------------------------ #
+    # DEPLOY action, sends the deployment file to remote host                  #
+    # ------------------------------------------------------------------------ #
+    def deploy(id, host, remote_dfile, not_used)
+
+        local_dfile = get_local_deployment_file(remote_dfile)
+
+        if !local_dfile || File.zero?(local_dfile)
+            send_message(ACTION[:deploy],RESULT[:failure],id,
+                "Can not open deployment file #{local_dfile}")
+            return
         end
-    end
-    
-    ###########################
-    # Actions                 #
-    ###########################
-    
-    def action_init(args)
-        send_message("INIT", "SUCCESS")
-    end
-        
-    def action_deploy(args)
-        action_number=args[1]
-        action_host=args[2]
-        remote_deployment_file=args[3]
-        action_name="DEPLOY"
-        
-        # Get local deployment file
-        local_deployment_file=get_local_deployment_file(remote_deployment_file)
-        
-        # If we can copy the deployment file
-        if local_deployment_file
-            # TODO: review this way of copying files
-            # This command copies deployment file to remote machine
-            # when shared directories are not used
-            copy_deploy="scp #{local_deployment_file} "+
-                "#{action_host}:#{remote_deployment_file}"
-            mad_log("DEPLOY", action_number, "Command: #{copy_deploy}")
-            copy_deploy_exit=execute_local_command(copy_deploy)
 
-            if copy_deploy_exit
-                mad_log("DEPLOY", action_number, 
-                    "Error: "+copy_deploy_exit.to_s)
-            else
-                mad_log("DEPLOY", action_number,
-                    "Copy success")
-            end
-        end
-        
-        command="create #{remote_deployment_file}"
-        std=exec_kvm_command(action_host, command)
-        stdout=std[1].read
-        stderr=std[2].read
-        
-        if !stderr.empty?
-            mad_log("DEPLOY", action_number, stderr)
-        end     
-        
-        write_response(action_name, stdout, stderr, args)
-        
-        #std_action("DEPLOY", "create #{args[3]}", args)
-    end
-    
-    def action_shutdown(args)
-        std_action("SHUTDOWN", "shutdown #{args[3]}", args)
-    end
-    
-    def action_cancel(args)
-        std_action("CANCEL", "destroy #{args[3]}", args)
-    end
-    
-    def action_checkpoint(args)
-        send_message("CHECKPOINT", "FAILURE", args[1], "action not supported for KVM")
-    end
+        tmp    = File.new(local_dfile)
+        domain = String.new
+        tmp.each { |line| domain << line }
+        tmp.close()
 
-    def action_save(args)
-        std_action("SAVE", "save #{args[3]} #{args[4]}", args)
-    end
+        deploy_cmd = "cat > #{remote_dfile} && " \
+                     "#{LIBVIRT[:create]} #{remote_dfile}"
 
-    def action_restore(args)
-        std_action("RESTORE", "restore #{args[3]}", args)
-    end
-    
-    def action_migrate(args)
-        std_action("MIGRATE", "migrate --live #{args[3]} qemu+tcp://#{args[4]}/session", args)
-    end
-    
-    def action_poll(args)
-        action_number=args[1]
-        
-        std=Open3.popen3(
-            "ssh -n #{args[2]} virsh dominfo #{args[3]};"+
-            " echo ExitCode: $? 1>&2")
-        stdout=std[1].read
-        stderr=std[2].read
-        
-        if !stderr.empty?
-            mad_log("POLL", action_number, stderr)
-        end
-        
-        exit_code=get_exit_code(stderr)
-        
-        if exit_code!=0   
-            tmp=stderr.scan(/^error: failed to get domain '#{args[3]}'/)
-            if tmp[0]
-                send_message("POLL", "SUCCESS", args[1], "STATE=d")
-            else
-                send_message("POLL", "FAILURE", args[1])
-            end
-            return nil
-        end
-        
-        info = parse_virsh_dominfo(stdout)      
+        deploy_exe = SSHCommand.run(deploy_cmd, host, log_method(id), domain)
 
-        # TODO -> Get more info, like NET info. Need to know the iface name (e.g. tap0)
-                        
-        send_message("POLL", "SUCCESS", args[1], info)
-    end
-    
-    ###########################
-    # Common action functions #
-    ###########################
-    
-    def std_action(name, command, args)
-        action_number=args[1]
-        std=exec_kvm_command(args[2], command)
-        stdout=std[1].read
-        stderr=std[2].read
-        
-        if !stderr.empty?
-            mad_log(name, action_number, stderr)
-        end     
-                
-        write_response(name, stdout, stderr, args)
-    end
-        
-    def exec_kvm_command(host, command)
-        res=Open3.popen3(
-            "ssh -n #{host} virsh #{command} ;"+
-            " echo ExitCode: $? 1>&2")
-        res[0].close
-        res
-    end
-    
-    def write_response(action, stdout, stderr, args)
-        exit_code=get_exit_code(stderr)
-        
-        if exit_code==0
-            domain_name=get_domain_name(stdout)
-            send_message(action, "SUCCESS", args[1], domain_name)
+        if deploy_exe.code != 0
+            send_message(ACTION[:deploy],RESULT[:failure],id)
+        elsif deploy_exe.stdout.match(/^Domain (.*) created from .*$/)
+            send_message(ACTION[:deploy],RESULT[:success],id,$1)
         else
-            error_message=get_error_message(stderr)
-            send_message(action, "FAILURE", args[1], error_message)
+            send_message(ACTION[:deploy],RESULT[:failure],id,
+                "Domain id not found.")
+        end
+    end
+
+    # ------------------------------------------------------------------------ #
+    # Basic Domain Management Operations                                       #
+    # ------------------------------------------------------------------------ #
+    def shutdown(id, host, deploy_id, not_used)
+        ssh_action("#{LIBVIRT[:shutdown]} #{deploy_id}", id, host, :shutdown)
+    end
+
+    def cancel(id, host, deploy_id, not_used)
+        ssh_action("#{LIBVIRT[:cancel]} #{deploy_id}", id, host, :cancel)
+    end
+
+    def save(id, host, deploy_id, file)
+        ssh_action("#{LIBVIRT[:save]} #{deploy_id} #{file}", id, host, :save)
+    end
+
+    def restore(id, host, file, not_used)
+        ssh_action("#{LIBVIRT[:restore]} #{file}", id, host, :restore)
+    end
+
+    def migrate(id, host, deploy_id, dest_host)
+        cmd = "#{LIBVIRT[:migrate]} #{deploy_id} "\
+              "#{QEMU_PROTOCOL}://#{dest_host}/session"
+
+        ssh_action(cmd, id, host, :migrate)
+    end
+
+    # ------------------------------------------------------------------------ #
+    # Get info from the Libvirt Domain                                         #
+    # ------------------------------------------------------------------------ #
+    def poll(id, host, deploy_id, not_used)
+
+        exe  = SSHCommand.run("#{LIBVIRT[:poll]} #{deploy_id}", host,
+                              log_method(id))
+
+        if exe.code != 0
+            result = :failure
+            info   = "-"
+
+            if exe.stderr.match(/^error: failed to get domain '#{deploy_id}'/)
+                info   = "#{POLL_ATTRIBUTE[:state]}=#{VM_STATE[:deleted]}"
+                result = :success
+            end
+
+            send_message(ACTION[:poll], RESULT[result], id, info)
+            return
         end
 
-    end
-    
+        info = ""
 
-    #########################################
-    # Parsers for virsh output              #
-    #########################################
-
-    # From STDERR if exit code == 1
-    def get_exit_code(str)
-        tmp=str.scan(/^ExitCode: (\d*)$/)
-        return nil if !tmp[0]
-        tmp[0][0].to_i
-    end
-    
-    # From STDERR if exit code == 1
-    def get_error_message(str)
-        tmp=str.scan(/^error: (.*)$/)
-        return "Unknown error" if !tmp[0]
-        tmp[0][0]
-    end
-
-    # From STDOUT if exit code == 0
-    def get_domain_name(str)
-        tmp=str.scan(/^Domain (.*) created from .*$/)
-        return nil if !tmp[0]
-        tmp[0][0]
-    end
-    
-    
-    ###############################
-    # Get information from virsh  #
-    ###############################
-        
-    
-    def parse_virsh_dominfo(returned_info)
-        
-        info  = ""
-        state = "u"
-        
-        returned_info.each_line {|line|
-         
+        exe.stdout.each_line {|line|
             columns=line.split(":").collect {|c| c.strip }
+
             case columns[0]
                 when 'Used memory'
-                        info += "USEDMEMORY=" + (columns[1].to_i).to_s
+                    info << " #{POLL_ATTRIBUTE[:usedmemory]}=" \
+                         << (columns[1].to_i).to_s
                 when 'State'
-                      case columns[1]
-                          when "running","blocked","shutdown","dying"
-                              state = "a"
-                          when "paused"
-                              state = "p"
-                          when "crashed"
-                              state = "e"
-                      end   
-                end     
-        }    
-        
-        info += " STATE=" + state 
-    
-        return info
+                    case columns[1]
+                        when "running","blocked","shutdown","dying"
+                            state = VM_STATE[:active]
+                        when "paused"
+                            state = VM_STATE[:paused]
+                        when "crashed"
+                            state = VM_STATE[:error]
+                        else
+                            state = "-"
+                    end
+
+                    info << " #{POLL_ATTRIBUTE[:state]}=#{state}"
+                end
+        }
+
+        send_message(ACTION[:poll], RESULT[:success], id, info)
     end
-    
+
+private
+
+    def ssh_action(command, id, host, action)
+        command_exe = SSHCommand.run(command, host, log_method(id))
+
+        if command_exe.code == 0
+            result = RESULT[:success]
+        else
+            result = RESULT[:failure]
+        end
+
+        send_message(ACTION[action],result,id)
+    end
 end
 
-dm=DM.new
-dm.loop
+# ---------------------------------------------------------------------------- #
+# LibvirtDriver Main program
+# ---------------------------------------------------------------------------- #
+
+kvm_driver = LibVirtDriver.new
+kvm_driver.start_driver
