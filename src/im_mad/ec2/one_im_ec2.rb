09143f299e8da270c0f5e1f540f5af9d4e43469c
Rubén S. Montero <rubensm@dacya.ucm.es>
2009-03-19 23:24:49 +0000

    Ported the EC2 information driver to the new libraries
    git-svn-id: http://svn.opennebula.org/one/trunk@425 3034c82b-c49b-4eb3-8279-a7acafdc01c0

--- src/im_mad/ec2/one_im_ec2.rb
+++ src/im_mad/ec2/one_im_ec2.rb
@@ -28,49 +28,53 @@ end
 $: << RUBY_LIB_LOCATION
 
 require 'pp'
-require 'one_mad'
+require 'OpenNebulaDriver'
 
-################
-# EC2 IM MAD #
-################
+#-------------------------------------------------------------------------------
+# The EC2 Information Manager Driver
+#-------------------------------------------------------------------------------
+class EC2InformationManagerDriver < OpenNebulaDriver
+    #---------------------------------------------------------------------------
+    # Init the driver, and compute the predefined maximum capacity for this
+    # EC2 cloud
+    #---------------------------------------------------------------------------
+    def initialize(num=15)
+        super(num,false)
 
-class IM < ONEMad
+        register_action(:MONITOR, method("action_monitor"))
 
-    def initialize(sensors=nil)
-        super(3, 4)
+        sinst  = ENV["SMALL_INSTANCES"].to_i
+        linst  = ENV["LARGE_INSTANCES"].to_i
+        xlinst = ENV["EXTRALARGE_INSTANCES"].to_i
+
+        smem  = 1048576 * 1.7 * sinst
+        scpu  = 100 * sinst
+
+        lmem  = 1048576 * 7.5 * linst
+        lcpu  = 400 * linst
+
+        xlmem = 1048576 * 15 * xlinst
+        xlcpu = 800 * xlinst
+
+        totalmemory = smem + lmem + xlmem
+        totalcpu    = scpu + lcpu + xlcpu
+
+        @info = "TOTALMEMORY=#{totalmemory},TOTALCPU=#{totalcpu}" \
+                "CPUSPEED=1000,FREEMEMORY=#{totalmemory},FREECPU=#{totalcpu}"
     end
-        
-    def action_init(args)
-        STDOUT.puts "INIT SUCCESS"
-        STDOUT.flush
+
+    #---------------------------------------------------------------------------
+    # The monitor action, just print the capacity info and hostname
+    #---------------------------------------------------------------------------
+    def action_monitor(num,host)
+       send_message("MONITOR",RESULT[:success],num,"HOSTNAME=#{host},#{@info}")
     end
-    
-    def action_monitor(args)
-        totalmemory=0;
-        totalcpu=0;
-
-        if(ENV["SMALL_INSTANCES"].to_i)
-            totalmemory=totalmemory + 1048576 * 1.7 *  ENV["SMALL_INSTANCES"].to_i
-            totalcpu = totalcpu + 100 * ENV["SMALL_INSTANCES"].to_i
-        end
-
-        if(ENV["LARGE_INSTANCES"].to_i)
-            totalmemory=totalmemory + 1048576 * 7.5 *  ENV["LARGE_INSTANCES"].to_i
-            totalcpu = totalcpu + 400 * ENV["LARGE_INSTANCES"].to_i
-        end
-
-        if(ENV["EXTRALARGE_INSTANCES"].to_i)
-            totalmemory=totalmemory + 1048576 * 15 *  ENV["EXTRALARGE_INSTANCES"].to_i
-            totalcpu = totalcpu + 800 * ENV["EXTRALARGE_INSTANCES"].to_i
-        end
-
-        STDOUT.puts "MONITOR SUCCESS " + args[1].to_s + 
-                    "HOSTNAME=#{args[2]},TOTALMEMORY=#{totalmemory},TOTALCPU=#{totalcpu},CPUSPEED=1000,FREEMEMORY=#{totalmemory},FREECPU=#{totalcpu}"
-        STDOUT.flush
-    end
-    
 end
 
-im=IM.new(nil)
-im.loop
-
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+# The EC2 Information Driver main program
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+im = EC2InformationManagerDriver.new(15)
+im.start_driver
