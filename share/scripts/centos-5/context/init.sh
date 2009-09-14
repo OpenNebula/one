03d3fc57b8f3146f0a6f525c77aa4df697bbf0cd
Jaime Melis <j.melis@fdi.ucm.es>
2009-09-14 15:55:31 +0000

    Added vmcontext scripts for CentOS. Minor correction of ubuntu vm template. (#94)
    git-svn-id: http://svn.opennebula.org/one/trunk@788 3034c82b-c49b-4eb3-8279-a7acafdc01c0

---
+++ share/scripts/centos-5/context/init.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# -------------------------------------------------------------------------- #
+# Copyright 2002-2009, Distributed Systems Architecture Group, Universidad   #
+# Complutense de Madrid (dsa-research.org)                                   #
+#                                                                            #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
+# not use this file except in compliance with the License. You may obtain    #
+# a copy of the License at                                                   #
+#                                                                            #
+# http://www.apache.org/licenses/LICENSE-2.0                                 #
+#                                                                            #
+# Unless required by applicable law or agreed to in writing, software        #
+# distributed under the License is distributed on an "AS IS" BASIS,          #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+# See the License for the specific language governing permissions and        #
+# limitations under the License.                                             #
+#--------------------------------------------------------------------------- #
+
+if [ -f /mnt/context.sh ]
+then
+  . /mnt/context.sh
+fi
+
+
+hostname $HOSTNAME
+sed -i "/HOSTNAME=/s/=.*$/=$HOSTNAME/" /etc/sysconfig/network
+
+if [ -n "$IP_PUBLIC" ]; then
+	ifconfig eth0 $IP_PUBLIC
+fi
+ 
+if [ -n "$NETMASK" ]; then
+	ifconfig eth0 netmask $NETMASK
+fi
+
+
+if [ -f /mnt/$ROOT_PUBKEY ]; then
+	mkdir -p /root/.ssh
+	cat /mnt/$ROOT_PUBKEY >> /root/.ssh/authorized_keys
+	chmod -R 600 /root/.ssh/
+fi
+
+if [ -n "$USERNAME" ]; then
+	useradd $USERNAME
+	if [ -f /mnt/$USER_PUBKEY ]; then
+		mkdir -p /home/$USERNAME/.ssh/
+		cat /mnt/$USER_PUBKEY >> /home/$USERNAME/.ssh/authorized_keys
+		chown -R $USERNAME:$USERNAME /home/$USERNAME/.ssh
+		chmod -R 600 /home/$USERNAME/.ssh/authorized_keys
+	fi
+fi
