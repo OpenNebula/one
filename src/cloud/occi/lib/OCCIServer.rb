f80e93528f4017e02c9db7fc973605ca5d65714f
Constantino Vázquez Blanco <tinova@fdi.ucm.es>
2010-01-21 15:22:42 +0000

    Changing OCCI network XML structure for coherence
    git-svn-id: http://svn.opennebula.org/one/trunk@962 3034c82b-c49b-4eb3-8279-a7acafdc01c0

--- src/cloud/occi/lib/OCCIServer.rb
+++ src/cloud/occi/lib/OCCIServer.rb
@@ -233,7 +233,7 @@ class OCCIServer < CloudServer
 
         begin  
             vntemplate = network.to_one_template(
-                            network_info['NIC'],
+                            network_info['NETWORK'],
                             @config[:bridge])
             rc         = network.allocate(vntemplate)
             network.info
