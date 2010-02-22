fdfe10407ec34251a83604ddc7ab1a69b74f0a86
Javier Fontán Muiños <jfontan@fdi.ucm.es>
2010-02-22 18:00:30 +0100

    Changed copyright notice

--- share/test/src/mad/mad_echo.sh
+++ share/test/src/mad/mad_echo.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # -------------------------------------------------------------------------- 
-# Copyright 2002-2006 GridWay Team, Distributed Systems Architecture         
-# Group, Universidad Complutense de Madrid                                   
+# Copyright 2002-2010, OpenNebula Project Leads (OpenNebula.org)             
 #                                                                            
 # Licensed under the Apache License, Version 2.0 (the "License"); you may    
 # not use this file except in compliance with the License. You may obtain    
