e9b21c84d1941abc24f357341e52a3c96e162ea4
Carlos Martin <cmartins@fdi.ucm.es>
2010-05-17 23:19:20 +0200

    feature #212: More tests for the scheduler

--- src/scheduler/src/pool/test/HostXMLTest.cc
+++ src/scheduler/src/pool/test/HostXMLTest.cc
@@ -42,8 +42,32 @@ public:
     friend class HostXMLTest;
 
     static const string host_dump;
+    static const string xmls[];
 
 protected:
+
+    void add_object(xmlNodePtr node)
+    {
+        xmlNodePtr nodeID;
+
+        xmlChar * str_ptr = xmlNodeGetContent(node->children->next);
+
+        int hid;
+
+        if (str_ptr != 0)
+        {
+            hid = atoi(reinterpret_cast<char *>(str_ptr));
+            xmlFree(str_ptr);
+        }
+        else
+        {
+
+        }
+
+        HostXML* vm = new HostXML( xmls[hid] );
+        objects.insert( pair<int,ObjectXML*>(hid, vm) );
+    };
+
     int load_info(xmlrpc_c::value &result)
     {
         vector<xmlrpc_c::value> arrayData;
@@ -118,11 +142,14 @@ public:
         CPPUNIT_ASSERT( rc == 0 );
 
         CPPUNIT_ASSERT( hp->objects.size() == 4 );
-        CPPUNIT_ASSERT( hp->objects.count(0) == 1);
+        CPPUNIT_ASSERT( hp->objects.count(0) == 0);
         CPPUNIT_ASSERT( hp->objects.count(1) == 1);
-        CPPUNIT_ASSERT( hp->objects.count(2) == 0);
-        CPPUNIT_ASSERT( hp->objects.count(3) == 1);
+        CPPUNIT_ASSERT( hp->objects.count(2) == 1);
+        CPPUNIT_ASSERT( hp->objects.count(3) == 0);
         CPPUNIT_ASSERT( hp->objects.count(4) == 1);
+        CPPUNIT_ASSERT( hp->objects.count(5) == 1);
+        CPPUNIT_ASSERT( hp->objects.count(6) == 0);
+        CPPUNIT_ASSERT( hp->objects.count(7) == 0);
     };
 
     void get_capacity()
@@ -141,7 +168,7 @@ public:
         int cpu, mem, threshold;
         threshold = 100;
 
-        HostXML* host = (HostXML*) hp->objects[4];
+        HostXML* host = (HostXML*) hp->objects[5];
         CPPUNIT_ASSERT(host != 0);
 
         host->get_capacity(cpu, mem, threshold);
@@ -162,7 +189,7 @@ public:
 
         CPPUNIT_ASSERT( hp->objects.size() == 4 );
 
-        HostXML* host = (HostXML*) hp->objects[4];
+        HostXML* host = (HostXML*) hp->objects[5];
         CPPUNIT_ASSERT(host != 0);
 
         /*
@@ -205,7 +232,7 @@ public:
 
         CPPUNIT_ASSERT( hp->objects.size() == 4 );
 
-        HostXML* host = (HostXML*) hp->objects[4];
+        HostXML* host = (HostXML*) hp->objects[5];
         CPPUNIT_ASSERT(host != 0);
 
         // add cpu, mem, disk
@@ -232,68 +259,196 @@ public:
 
 int main(int argc, char ** argv)
 {
+    // We need to set the log file
+    NebulaLog::init_log_system(NebulaLog::FILE, Log::DEBUG, "test.log");
+    NebulaLog::log("Test", Log::INFO, "Test started");
+
     CppUnit::TextUi::TestRunner runner;
 
     runner.addTest(HostXMLTest::suite());
     runner.run();
+
+
+//    remove("test.log");
+    NebulaLog::finalize_log_system();
 
     return 0;
 }
 
 // ----------------------------------------------------------------------------
 
+const string FriendHostPool::xmls[] =
+{
+"",
+
+"<HOST>\
+  <ID>1</ID>\
+  <NAME>ursa12</NAME>\
+  <STATE>2</STATE>\
+  <IM_MAD>im_kvm</IM_MAD>\
+  <VM_MAD>vmm_kvm</VM_MAD>\
+  <TM_MAD>tm_nfs</TM_MAD>\
+  <LAST_MON_TIME>1274107238</LAST_MON_TIME>\
+  <HOST_SHARE>\
+    <HID>1</HID>\
+    <DISK_USAGE>0</DISK_USAGE>\
+    <MEM_USAGE>0</MEM_USAGE>\
+    <CPU_USAGE>0</CPU_USAGE>\
+    <MAX_DISK>0</MAX_DISK>\
+    <MAX_MEM>8194368</MAX_MEM>\
+    <MAX_CPU>800</MAX_CPU>\
+    <FREE_DISK>0</FREE_DISK>\
+    <FREE_MEM>7954812</FREE_MEM>\
+    <FREE_CPU>800</FREE_CPU>\
+    <USED_DISK>0</USED_DISK>\
+    <USED_MEM>536220</USED_MEM>\
+    <USED_CPU>0</USED_CPU>\
+    <RUNNING_VMS>0</RUNNING_VMS>\
+  </HOST_SHARE>\
+  <TEMPLATE>\
+    <ARCH>x86_64</ARCH>\
+    <CPUSPEED>2326</CPUSPEED>\
+    <FREECPU>800.0</FREECPU>\
+    <FREEMEMORY>7954812</FREEMEMORY>\
+    <HOSTNAME>ursa12</HOSTNAME>\
+    <HYPERVISOR>kvm</HYPERVISOR>\
+    <MODELNAME>Intel(R) Xeon(R) CPU           E5410  @ 2.33GHz</MODELNAME>\
+    <NETRX>18584233465</NETRX>\
+    <NETTX>117426</NETTX>\
+    <TOTALCPU>800</TOTALCPU>\
+    <TOTALMEMORY>8194368</TOTALMEMORY>\
+    <USEDCPU>0.0</USEDCPU>\
+    <USEDMEMORY>536220</USEDMEMORY>\
+  </TEMPLATE>\
+</HOST>",
+
+"<HOST>\
+  <ID>2</ID>\
+  <NAME>ursa</NAME>\
+  <STATE>2</STATE>\
+  <IM_MAD>im_kvm</IM_MAD>\
+  <VM_MAD>vmm_kvm</VM_MAD>\
+  <TM_MAD>tm_nfs</TM_MAD>\
+  <LAST_MON_TIME>1274107299</LAST_MON_TIME>\
+  <HOST_SHARE>\
+    <HID>2</HID>\
+    <DISK_USAGE>0</DISK_USAGE>\
+    <MEM_USAGE>4280320</MEM_USAGE>\
+    <CPU_USAGE>700</CPU_USAGE>\
+    <MAX_DISK>0</MAX_DISK>\
+    <MAX_MEM>16468252</MAX_MEM>\
+    <MAX_CPU>800</MAX_CPU>\
+    <FREE_DISK>0</FREE_DISK>\
+    <FREE_MEM>9104256</FREE_MEM>\
+    <FREE_CPU>613</FREE_CPU>\
+    <USED_DISK>0</USED_DISK>\
+    <USED_MEM>13897924</USED_MEM>\
+    <USED_CPU>186</USED_CPU>\
+    <RUNNING_VMS>7</RUNNING_VMS>\
+  </HOST_SHARE>\
+  <TEMPLATE>\
+    <ARCH>x86_64</ARCH>\
+    <CPUSPEED>2327</CPUSPEED>\
+    <FREECPU>613.6</FREECPU>\
+    <FREEMEMORY>9104256</FREEMEMORY>\
+    <HOSTNAME>ursa</HOSTNAME>\
+    <HYPERVISOR>kvm</HYPERVISOR>\
+    <MODELNAME>Intel(R) Xeon(R) CPU           E5410  @ 2.33GHz</MODELNAME>\
+    <NETRX>102804341287</NETRX>\
+    <NETTX>2186665283</NETTX>\
+    <TOTALCPU>800</TOTALCPU>\
+    <TOTALMEMORY>16468252</TOTALMEMORY>\
+    <USEDCPU>186.4</USEDCPU>\
+    <USEDMEMORY>13897924</USEDMEMORY>\
+  </TEMPLATE>\
+</HOST>",
+
+"",
+
+"<HOST>\
+  <ID>4</ID>\
+  <NAME>ursa11</NAME>\
+  <STATE>2</STATE>\
+  <IM_MAD>im_kvm</IM_MAD>\
+  <VM_MAD>vmm_kvm</VM_MAD>\
+  <TM_MAD>tm_nfs</TM_MAD>\
+  <LAST_MON_TIME>1274107328</LAST_MON_TIME>\
+  <HOST_SHARE>\
+    <HID>4</HID>\
+    <DISK_USAGE>0</DISK_USAGE>\
+    <MEM_USAGE>0</MEM_USAGE>\
+    <CPU_USAGE>0</CPU_USAGE>\
+    <MAX_DISK>0</MAX_DISK>\
+    <MAX_MEM>8194368</MAX_MEM>\
+    <MAX_CPU>800</MAX_CPU>\
+    <FREE_DISK>0</FREE_DISK>\
+    <FREE_MEM>7958684</FREE_MEM>\
+    <FREE_CPU>800</FREE_CPU>\
+    <USED_DISK>0</USED_DISK>\
+    <USED_MEM>621616</USED_MEM>\
+    <USED_CPU>0</USED_CPU>\
+    <RUNNING_VMS>0</RUNNING_VMS>\
+  </HOST_SHARE>\
+  <TEMPLATE>\
+    <ARCH>x86_64</ARCH>\
+    <CPUSPEED>2327</CPUSPEED>\
+    <FREECPU>800.0</FREECPU>\
+    <FREEMEMORY>7958684</FREEMEMORY>\
+    <HOSTNAME>ursa11</HOSTNAME>\
+    <HYPERVISOR>kvm</HYPERVISOR>\
+    <MODELNAME>Intel(R) Xeon(R) CPU           E5410  @ 2.33GHz</MODELNAME>\
+    <NETRX>26567502961</NETRX>\
+    <NETTX>4061</NETTX>\
+    <TOTALCPU>800</TOTALCPU>\
+    <TOTALMEMORY>8194368</TOTALMEMORY>\
+    <USEDCPU>0.0</USEDCPU>\
+    <USEDMEMORY>621616</USEDMEMORY>\
+  </TEMPLATE>\
+</HOST>",
+
+"<HOST>\
+  <ID>5</ID>\
+  <NAME>ursa10</NAME>\
+  <STATE>2</STATE>\
+  <IM_MAD>im_kvm</IM_MAD>\
+  <VM_MAD>vmm_kvm</VM_MAD>\
+  <TM_MAD>tm_nfs</TM_MAD>\
+  <LAST_MON_TIME>1274107300</LAST_MON_TIME>\
+  <HOST_SHARE>\
+    <HID>5</HID>\
+    <DISK_USAGE>256</DISK_USAGE>\
+    <MEM_USAGE>128</MEM_USAGE>\
+    <CPU_USAGE>20</CPU_USAGE>\
+    <MAX_DISK>512</MAX_DISK>\
+    <MAX_MEM>512</MAX_MEM>\
+    <MAX_CPU>200</MAX_CPU>\
+    <FREE_DISK>256</FREE_DISK>\
+    <FREE_MEM>384</FREE_MEM>\
+    <FREE_CPU>180</FREE_CPU>\
+    <USED_DISK>0</USED_DISK>\
+    <USED_MEM>2191756</USED_MEM>\
+    <USED_CPU>6</USED_CPU>\
+    <RUNNING_VMS>0</RUNNING_VMS>\
+  </HOST_SHARE>\
+  <TEMPLATE>\
+    <ARCH>x86_64</ARCH>\
+    <CPUSPEED>2327</CPUSPEED>\
+    <FREECPU>793.6</FREECPU>\
+    <FREEMEMORY>7871464</FREEMEMORY>\
+    <HOSTNAME>ursa10</HOSTNAME>\
+    <HYPERVISOR>kvm</HYPERVISOR>\
+    <MODELNAME>Intel(R) Xeon(R) CPU           E5410  @ 2.33GHz</MODELNAME>\
+    <NETRX>42525516540</NETRX>\
+    <NETTX>144088</NETTX>\
+    <TOTALCPU>800</TOTALCPU>\
+    <TOTALMEMORY>8194368</TOTALMEMORY>\
+    <USEDCPU>6.39999999999998</USEDCPU>\
+    <USEDMEMORY>2191756</USEDMEMORY>\
+  </TEMPLATE>\
+</HOST>"
+};
+
 const string FriendHostPool::host_dump =
-    "<HOST_POOL><HOST><ID>0</ID><NAME>a</NAME><STATE>0</STATE><IM_MAD>im_mad</I"
-    "M_MAD><VM_MAD>vmm_mad</VM_MAD><TM_MAD>tm_mad</TM_MAD><LAST_MON_TIME>000000"
-    "0000</LAST_MON_TIME><HOST_SHARE><HID>0</HID><DISK_USAGE>0</DISK_USAGE><MEM"
-    "_USAGE>0</MEM_USAGE><CPU_USAGE>0</CPU_USAGE><MAX_DISK>0</MAX_DISK><MAX_MEM"
-    ">0</MAX_MEM><MAX_CPU>0</MAX_CPU><FREE_DISK>0</FREE_DISK><FREE_MEM>0</FREE_"
-    "MEM><FREE_CPU>0</FREE_CPU><USED_DISK>0</USED_DISK><USED_MEM>0</USED_MEM><U"
-    "SED_CPU>0</USED_CPU><RUNNING_VMS>0</RUNNING_VMS></HOST_SHARE></HOST><HOST>"
-    "<ID>1</ID><NAME>a name</NAME><STATE>1</STATE><IM_MAD>im_mad</IM_MAD><VM_MA"
-    "D>vmm_mad</VM_MAD><TM_MAD>tm_mad</TM_MAD><LAST_MON_TIME>0000000000</LAST_M"
-    "ON_TIME><HOST_SHARE><HID>1</HID><DISK_USAGE>0</DISK_USAGE><MEM_USAGE>0</ME"
-    "M_USAGE><CPU_USAGE>0</CPU_USAGE><MAX_DISK>0</MAX_DISK><MAX_MEM>0</MAX_MEM>"
-    "<MAX_CPU>0</MAX_CPU><FREE_DISK>0</FREE_DISK><FREE_MEM>0</FREE_MEM><FREE_CP"
-    "U>0</FREE_CPU><USED_DISK>0</USED_DISK><USED_MEM>0</USED_MEM><USED_CPU>0</U"
-    "SED_CPU><RUNNING_VMS>0</RUNNING_VMS></HOST_SHARE></HOST><HOST><ID>2</ID><N"
-    "AME>a_name</NAME><STATE>3</STATE><IM_MAD>im_mad</IM_MAD><VM_MAD>vmm_mad</V"
-    "M_MAD><TM_MAD>tm_mad</TM_MAD><LAST_MON_TIME>0000000000</LAST_MON_TIME><HOS"
-    "T_SHARE><HID>2</HID><DISK_USAGE>0</DISK_USAGE><MEM_USAGE>0</MEM_USAGE><CPU"
-    "_USAGE>0</CPU_USAGE><MAX_DISK>0</MAX_DISK><MAX_MEM>0</MAX_MEM><MAX_CPU>0</"
-    "MAX_CPU><FREE_DISK>0</FREE_DISK><FREE_MEM>0</FREE_MEM><FREE_CPU>0</FREE_CP"
-    "U><USED_DISK>0</USED_DISK><USED_MEM>0</USED_MEM><USED_CPU>0</USED_CPU><RUN"
-    "NING_VMS>0</RUNNING_VMS></HOST_SHARE></HOST><HOST><ID>3</ID><NAME>another "
-    "name</NAME><STATE>2</STATE><IM_MAD>im_mad</IM_MAD><VM_MAD>vmm_mad</VM_MAD>"
-    "<TM_MAD>tm_mad</TM_MAD><LAST_MON_TIME>0000000000</LAST_MON_TIME><HOST_SHAR"
-    "E><HID>3</HID><DISK_USAGE>0</DISK_USAGE><MEM_USAGE>0</MEM_USAGE><CPU_USAGE"
-    ">0</CPU_USAGE><MAX_DISK>0</MAX_DISK><MAX_MEM>0</MAX_MEM><MAX_CPU>0</MAX_CP"
-    "U><FREE_DISK>0</FREE_DISK><FREE_MEM>0</FREE_MEM><FREE_CPU>0</FREE_CPU><USE"
-    "D_DISK>0</USED_DISK><USED_MEM>0</USED_MEM><USED_CPU>0</USED_CPU><RUNNING_V"
-    "MS>0</RUNNING_VMS></HOST_SHARE></HOST>"
-  "<HOST>"
-"    <ID>4</ID>"
-"    <NAME>host</NAME>"
-"    <STATE>0</STATE>"
-"    <IM_MAD>im_mad</IM_MAD>"
-"    <VM_MAD>vmm_mad</VM_MAD>"
-"    <TM_MAD>tm_mad</TM_MAD>"
-"    <LAST_MON_TIME>0000000000</LAST_MON_TIME>"
-"    <HOST_SHARE>"
-"      <HID>4</HID>"
-"      <DISK_USAGE>256</DISK_USAGE>"
-"      <MEM_USAGE>128</MEM_USAGE>"
-"      <CPU_USAGE>20</CPU_USAGE>"
-"      <MAX_DISK>512</MAX_DISK>"
-"      <MAX_MEM>512</MAX_MEM>"
-"      <MAX_CPU>200</MAX_CPU>"
-"      <FREE_DISK>256</FREE_DISK>"
-"      <FREE_MEM>384</FREE_MEM>"
-"      <FREE_CPU>180</FREE_CPU>"
-"      <USED_DISK>256</USED_DISK>"
-"      <USED_MEM>128</USED_MEM>"
-"      <USED_CPU>20</USED_CPU>"
-"      <RUNNING_VMS>0</RUNNING_VMS>"
-"    </HOST_SHARE>"
-"  </HOST>"
-"</HOST_POOL>";
+"<HOST_POOL> <HOST> <ID>1</ID> <NAME>ursa12</NAME> <STATE>2</STATE> <IM_MAD>im_kvm</IM_MAD> <VM_MAD>vmm_kvm</VM_MAD> <TM_MAD>tm_nfs</TM_MAD> <LAST_MON_TIME>1274107145</LAST_MON_TIME> <HOST_SHARE> <HID>1</HID> <DISK_USAGE>0</DISK_USAGE> <MEM_USAGE>0</MEM_USAGE> <CPU_USAGE>0</CPU_USAGE> <MAX_DISK>0</MAX_DISK> <MAX_MEM>8194368</MAX_MEM> <MAX_CPU>800</MAX_CPU> <FREE_DISK>0</FREE_DISK> <FREE_MEM>7955180</FREE_MEM> <FREE_CPU>800</FREE_CPU> <USED_DISK>0</USED_DISK> <USED_MEM>535848</USED_MEM> <USED_CPU>0</USED_CPU> <RUNNING_VMS>0</RUNNING_VMS> </HOST_SHARE> </HOST> <HOST> <ID>2</ID> <NAME>ursa</NAME> <STATE>2</STATE> <IM_MAD>im_kvm</IM_MAD> <VM_MAD>vmm_kvm</VM_MAD> <TM_MAD>tm_nfs</TM_MAD> <LAST_MON_TIME>1274107206</LAST_MON_TIME> <HOST_SHARE> <HID>2</HID> <DISK_USAGE>0</DISK_USAGE> <MEM_USAGE>4280320</MEM_USAGE> <CPU_USAGE>700</CPU_USAGE> <MAX_DISK>0</MAX_DISK> <MAX_MEM>16468252</MAX_MEM> <MAX_CPU>800</MAX_CPU> <FREE_DISK>0</FREE_DISK> <FREE_MEM>9109156</FREE_MEM> <FREE_CPU>628</FREE_CPU> <USED_DISK>0</USED_DISK> <USED_MEM>13891232</USED_MEM> <USED_CPU>172</USED_CPU> <RUNNING_VMS>7</RUNNING_VMS> </HOST_SHARE> </HOST> <HOST> <ID>4</ID> <NAME>ursa11</NAME> <STATE>2</STATE> <IM_MAD>im_kvm</IM_MAD> <VM_MAD>vmm_kvm</VM_MAD> <TM_MAD>tm_nfs</TM_MAD> <LAST_MON_TIME>1274107145</LAST_MON_TIME> <HOST_SHARE> <HID>4</HID> <DISK_USAGE>0</DISK_USAGE> <MEM_USAGE>0</MEM_USAGE> <CPU_USAGE>0</CPU_USAGE> <MAX_DISK>0</MAX_DISK> <MAX_MEM>8194368</MAX_MEM> <MAX_CPU>800</MAX_CPU> <FREE_DISK>0</FREE_DISK> <FREE_MEM>7958684</FREE_MEM> <FREE_CPU>800</FREE_CPU> <USED_DISK>0</USED_DISK> <USED_MEM>621616</USED_MEM> <USED_CPU>0</USED_CPU> <RUNNING_VMS>0</RUNNING_VMS> </HOST_SHARE> </HOST> <HOST> <ID>5</ID> <NAME>ursa10</NAME> <STATE>2</STATE> <IM_MAD>im_kvm</IM_MAD> <VM_MAD>vmm_kvm</VM_MAD> <TM_MAD>tm_nfs</TM_MAD> <LAST_MON_TIME>1274107207</LAST_MON_TIME> <HOST_SHARE> <HID>5</HID> <DISK_USAGE>0</DISK_USAGE> <MEM_USAGE>0</MEM_USAGE> <CPU_USAGE>0</CPU_USAGE> <MAX_DISK>0</MAX_DISK> <MAX_MEM>8194368</MAX_MEM> <MAX_CPU>800</MAX_CPU> <FREE_DISK>0</FREE_DISK> <FREE_MEM>7870776</FREE_MEM> <FREE_CPU>796</FREE_CPU> <USED_DISK>0</USED_DISK> <USED_MEM>2192424</USED_MEM> <USED_CPU>4</USED_CPU> <RUNNING_VMS>0</RUNNING_VMS> </HOST_SHARE> </HOST> <HOST> <ID>6</ID> <NAME>ursa08</NAME> <STATE>3</STATE> <IM_MAD>im_kvm</IM_MAD> <VM_MAD>vmm_kvm</VM_MAD> <TM_MAD>tm_nfs</TM_MAD> <LAST_MON_TIME>1274107176</LAST_MON_TIME> <HOST_SHARE> <HID>6</HID> <DISK_USAGE>0</DISK_USAGE> <MEM_USAGE>0</MEM_USAGE> <CPU_USAGE>0</CPU_USAGE> <MAX_DISK>0</MAX_DISK> <MAX_MEM>0</MAX_MEM> <MAX_CPU>0</MAX_CPU> <FREE_DISK>0</FREE_DISK> <FREE_MEM>6585200</FREE_MEM> <FREE_CPU>0</FREE_CPU> <USED_DISK>0</USED_DISK> <USED_MEM>1132608</USED_MEM> <USED_CPU>0</USED_CPU> <RUNNING_VMS>0</RUNNING_VMS> </HOST_SHARE> </HOST> <HOST> <ID>7</ID> <NAME>ursa09</NAME> <STATE>3</STATE> <IM_MAD>im_kvm</IM_MAD> <VM_MAD>vmm_kvm</VM_MAD> <TM_MAD>tm_nfs</TM_MAD> <LAST_MON_TIME>1274107146</LAST_MON_TIME> <HOST_SHARE> <HID>7</HID> <DISK_USAGE>0</DISK_USAGE> <MEM_USAGE>524288</MEM_USAGE> <CPU_USAGE>100</CPU_USAGE> <MAX_DISK>0</MAX_DISK> <MAX_MEM>8194368</MAX_MEM> <MAX_CPU>800</MAX_CPU> <FREE_DISK>0</FREE_DISK> <FREE_MEM>7286128</FREE_MEM> <FREE_CPU>798</FREE_CPU> <USED_DISK>0</USED_DISK> <USED_MEM>8011908</USED_MEM> <USED_CPU>1</USED_CPU> <RUNNING_VMS>1</RUNNING_VMS> </HOST_SHARE> </HOST> </HOST_POOL> ";
+
+
