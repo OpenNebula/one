a342d00d8d6cfb15f7723272ad60888d01ada706
Javi Fontan <jfontan@fdi.ucm.es>
2010-07-12 22:48:48 +0200

    feature #203: public flag is taken into account

--- src/authm_mad/simple_permissions.rb
+++ src/authm_mad/simple_permissions.rb
@@ -13,28 +13,28 @@ class SimplePermissions
         result=true
         
         tokens.each do |token|
-            object, id, action, owner=token.split(':')
-            result=auth_object(uid.to_s, object, id, action, owner)
+            object, id, action, owner, pub=token.split(':')
+            result=auth_object(uid.to_s, object, id, action, owner, pub)
             break result if result!=true
         end
         
         result
     end
     
-    def auth_object(uid, object, id, action, owner)
+    def auth_object(uid, object, id, action, owner, pub)
         return true if uid==0
         
         case object
         when 'VM'
-            return auth_vm(uid, object, id, action, owner)
+            return auth_vm(uid, object, id, action, owner, pub)
         when 'HOST'
-            return auth_host(uid, object, id, action, owner)
+            return auth_host(uid, object, id, action, owner, pub)
         else
-            return auth_generic(uid, object, id, action, owner)
+            return auth_generic(uid, object, id, action, owner, pub)
         end
     end
     
-    def auth_vm(uid, object, id, action, owner)
+    def auth_vm(uid, object, id, action, owner, pub)
         case action
         when 'CREATE'
             # add quota here
@@ -44,16 +44,20 @@ class SimplePermissions
         end
     end
     
-    def auth_host(uid, object, id, action, owner)
+    def auth_host(uid, object, id, action, owner, pub)
         auth_message(action=='USE', 'Only oneadmin can manage hosts')
     end
     
-    def auth_generic(uid, object, id, action, owner)
-        if action=='CREATE'
+    def auth_generic(uid, object, id, action, owner, pub)
+        case action
+        when 'CREATE'
             true
+        when 'USE'
+            auth_message(uid==owner || pub=='1', "You are not allowed to "<<
+                "#{action} #{object} #{id}")
         else
-            auth_message(uid==owner, "You are not allowed to #{action} "<<
-                "#{object} #{id}")
+            auth_message(uid==owner, "You are not allowed to "<<
+                "#{action} #{object} #{id}")
         end
     end
 end
