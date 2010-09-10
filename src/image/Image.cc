043f762950be519b4a75c95148bb556ce44be1ca
Ruben S. Montero <rubensm@dacya.ucm.es>
2010-09-10 23:23:47 +0200

    bug #345: Revert previous commit.

--- src/image/Image.cc
+++ src/image/Image.cc
@@ -554,14 +554,6 @@ bool Image::release_image()
         break;
 
         case DISABLED:
-            if (running_vms == 1 && persistent_img == true )
-            {
-                running_vms = 0;
-                state       = READY;
-
-                dirty = true;
-            }
-        break;
         case READY:
         default:
         break;
