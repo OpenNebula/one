0257c9c7da54d492c8834ebb1b73dac430b59f39
Ruben S. Montero <rubensm@dacya.ucm.es>
2010-10-15 17:43:10 +0200

    UserInfo class

---
+++ src/rm/RequestManagerUserInfo.cc
@@ -0,0 +1,122 @@
+/* -------------------------------------------------------------------------- */
+/* Copyright 2002-2010, OpenNebula Project Leads (OpenNebula.org)             */
+/*                                                                            */
+/* Licensed under the Apache License, Version 2.0 (the "License"); you may    */
+/* not use this file except in compliance with the License. You may obtain    */
+/* a copy of the License at                                                   */
+/*                                                                            */
+/* http://www.apache.org/licenses/LICENSE-2.0                                 */
+/*                                                                            */
+/* Unless required by applicable law or agreed to in writing, software        */
+/* distributed under the License is distributed on an "AS IS" BASIS,          */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   */
+/* See the License for the specific language governing permissions and        */
+/* limitations under the License.                                             */
+/* -------------------------------------------------------------------------- */
+
+#include "RequestManager.h"
+#include "NebulaLog.h"
+
+#include "AuthManager.h"
+
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+
+void RequestManager::UserInfo::execute(
+    xmlrpc_c::paramList const& paramList,
+    xmlrpc_c::value *   const  retval)
+{ 
+    string session;
+    int    the_uid, uid;
+    
+    User * user;
+    ostringstream oss;
+
+    const string method_name = "UserInfo";
+
+    /*   -- RPC specific vars --  */
+    vector<xmlrpc_c::value> arrayData;
+    xmlrpc_c::value_array * arrayresult;
+
+    NebulaLog::log("ReM",Log::DEBUG,"UserInfo method invoked");
+
+    // Get the parameters
+    session = xmlrpc_c::value_string(paramList.getString(0));
+    the_uid = xmlrpc_c::value_int(paramList.getInt(1));
+    
+    // Only oneadmin can retrieve user information
+    uid = UserInfo::upool->authenticate(session);
+    
+    if ( uid == -1 )                             
+    {                                            
+        goto error_authenticate;                     
+    }
+    
+    //Authorize the operation
+    if ( uid != 0 ) // uid == 0 means oneadmin
+    {
+        AuthRequest ar(uid);
+
+        ar.add_auth(AuthRequest::USER,
+                    the_uid,
+                    AuthRequest::INFO,
+                    0,
+                    false);
+
+        if (UserPool::authorize(ar) == -1)
+        {
+            goto error_authorize;
+        }
+    }    
+    // Now let's get the user 
+    user = UserInfo::upool->get(uid,true);
+    
+    if ( user == 0 )                             
+    {                                            
+        goto error_get_user;                     
+    }    
+   
+    oss << *user; 
+
+    user->unlock();
+    
+    // All nice, return the new uid to client  
+    arrayData.push_back(xmlrpc_c::value_boolean(true)); // SUCCESS
+    arrayData.push_back(xmlrpc_c::value_string(oss.str()));
+
+    // Copy arrayresult into retval mem space
+    arrayresult = new xmlrpc_c::value_array(arrayData);
+    *retval     = *arrayresult;
+
+    delete arrayresult; // and get rid of the original
+
+    return;
+
+error_authenticate:
+    oss.str(authenticate_error(method_name));
+    goto error_common;
+
+error_authorize:
+    oss.str(authorization_error(method_name, "INFO", "USER", uid, -1));
+    goto error_common;
+
+error_get_user:
+    oss.str(get_error(method_name, "USER", the_uid));
+    goto error_common;
+
+error_common:
+
+    arrayData.push_back(xmlrpc_c::value_boolean(false));  // FAILURE
+    arrayData.push_back(xmlrpc_c::value_string(oss.str()));
+    
+    NebulaLog::log("ReM",Log::ERROR,oss); 
+    
+    xmlrpc_c::value_array arrayresult_error(arrayData);
+
+    *retval = arrayresult_error;
+    
+    return;
+}
+
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
