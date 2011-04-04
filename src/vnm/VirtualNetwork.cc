OpenNebula/one

commit ff73aefc6f46a8713b9ad771bd40cd1fe898f3ee

Feature #523: Fix bug that prevented the usage of 'onevnet list m'

src/vnm/VirtualNetwork.cc
@@ -74,7 +74,7 @@ const char * VirtualNetwork::db_names     = "oid, name, body, uid";
 
 const char * VirtualNetwork::db_bootstrap = "CREATE TABLE IF NOT EXISTS"
     " network_pool (oid INTEGER PRIMARY KEY, name VARCHAR(256),"
-    " body TEXT, uid INTEGER, UNIQUE(name,uid))";
+    " body TEXT, uid INTEGER, public INTEGER, UNIQUE(name,uid))";
 
 /* -------------------------------------------------------------------------- */
 /* -------------------------------------------------------------------------- */
@@ -423,7 +423,8 @@ int VirtualNetwork::insert_replace(SqlDB *db, bool replace)
         <<          oid         << ","
         << "'" <<   sql_name    << "',"
         << "'" <<   sql_xml     << "',"
-        <<          uid         << ")";
+        <<          uid         << ","
+        <<          public_vnet << ")";
 
     rc = db->exec(oss);
 

commit 1cf8be6480fdf1bcb9cb81f57927d3218c8a49d2

Feature #523: Small bug introduced in ff73aefc6f46a8713b9ad771bd40cd1fe898f3ee

src/vnm/VirtualNetwork.cc
@@ -70,7 +70,7 @@ VirtualNetwork::~VirtualNetwork()
 
 const char * VirtualNetwork::table        = "network_pool";
 
-const char * VirtualNetwork::db_names     = "oid, name, body, uid";
+const char * VirtualNetwork::db_names     = "oid, name, body, uid, public";
 
 const char * VirtualNetwork::db_bootstrap = "CREATE TABLE IF NOT EXISTS"
     " network_pool (oid INTEGER PRIMARY KEY, name VARCHAR(256),"