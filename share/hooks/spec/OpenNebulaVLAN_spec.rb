9d38042dc74a41fe5c5e1a6fb2c3b52c7d8b886d
Jaime Melis <j.melis@fdi.ucm.es>
2011-05-17 12:45:53 +0200

    feature #476: adapt rspec to refactorization

--- share/hooks/spec/OpenNebulaVLAN_spec.rb
+++ share/hooks/spec/OpenNebulaVLAN_spec.rb
@@ -13,35 +13,46 @@ Dir[File.dirname(__FILE__) + "/output/**"].each do |f|
     OUTPUT[key] = File.read(f)
 end
 
-$collector = Hash.new
-
 def execute_cmd(cmd)
-    if cmd =~ /virsh.*dumpxml/
-        OUTPUT[:virsh_dumpxml]
-    else
-        Open3.popen3(cmd){|stdin, stdout, stderr| stdout.read}
+    if $capture_commands
+        $capture_commands.each do |regex, output|
+            if cmd.match(regex)
+                return output
+            end
+        end
     end
+    Open3.popen3(cmd){|stdin, stdout, stderr| stdout.read}
 end
 
 def `(cmd)
-    $collector[:backtick] << cmd
+    log_command(:backtick, cmd)
     execute_cmd(cmd)
 end
 
 def system(cmd)
-    $collector[:system] << cmd
-    #Kernel::system(cmd)
+    log_command(:system, cmd)
+    execute_cmd(cmd)
+    nil
+end
+
+def log_command(facility, msg)
+    $collector = Hash.new if !$collector
+    $collector[facility] = Array.new if !$collector[facility]
+    $collector[facility] << msg
 end
 
 RSpec.configure do |config|
     config.before(:all) do
-        $collector[:system]   = Array.new
-        $collector[:backtick] = Array.new
+        $capture_commands = Hash.new
+        $collector = Hash.new
     end
 end
 
 describe 'networking' do
     it "get all nics in kvm" do
+        $capture_commands = {
+            /virsh.*dumpxml/ => OUTPUT[:virsh_dumpxml]
+        }
         onevlan = OpenNebulaVLAN.new(OUTPUT[:onevm_show],"kvm")
         nics_expected = [{:bridge=>"br0",
                       :ip=>"172.16.0.100",
@@ -67,16 +78,19 @@ end
 
 describe 'ebtables' do
     it "generate ebtable rules in kvm" do
+        $capture_commands = {
+            /virsh.*dumpxml/ => OUTPUT[:virsh_dumpxml],
+            /ebtables/       => nil
+        }
         onevlan = EbtablesVLAN.new(OUTPUT[:onevm_show],"kvm")
         onevlan.activate
         ebtables_cmds = [
-        " -A FORWARD -s ! 02:00:ac:10:00:00/ff:ff:ff:ff:ff:00 -o vnet0 -j DROP",
-        " -A FORWARD -s ! 02:00:ac:10:00:64 -i vnet0 -j DROP",
-        " -A FORWARD -s ! 02:00:0a:01:01:00/ff:ff:ff:ff:ff:00 -o vnet1 -j DROP",
-        " -A FORWARD -s ! 02:00:0a:01:01:01 -i vnet1 -j DROP",
-        " -A FORWARD -s ! 02:00:0a:01:02:00/ff:ff:ff:ff:ff:00 -o vnet2 -j DROP",
-        " -A FORWARD -s ! 02:00:0a:01:02:01 -i vnet2 -j DROP"]
-
+        "sudo /sbin/ebtables -A FORWARD -s ! 02:00:ac:10:00:00/ff:ff:ff:ff:ff:00 -o vnet0 -j DROP",
+        "sudo /sbin/ebtables -A FORWARD -s ! 02:00:ac:10:00:64 -i vnet0 -j DROP",
+        "sudo /sbin/ebtables -A FORWARD -s ! 02:00:0a:01:01:00/ff:ff:ff:ff:ff:00 -o vnet1 -j DROP",
+        "sudo /sbin/ebtables -A FORWARD -s ! 02:00:0a:01:01:01 -i vnet1 -j DROP",
+        "sudo /sbin/ebtables -A FORWARD -s ! 02:00:0a:01:02:00/ff:ff:ff:ff:ff:00 -o vnet2 -j DROP",
+        "sudo /sbin/ebtables -A FORWARD -s ! 02:00:0a:01:02:01 -i vnet2 -j DROP"]
         $collector[:system].should == ebtables_cmds
     end
 end
