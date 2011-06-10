d9f96d343e49b7cac148db106f5f46cc733f6ff2
Daniel Molina <danmolin@fdi.ucm.es>
2011-06-10 18:25:35 +0200

    feature #661: Add onegroup command

---
+++ src/cli/one_helper/onegroup_helper.rb
@@ -0,0 +1,65 @@
+# -------------------------------------------------------------------------- #
+# Copyright 2002-2011, OpenNebula Project Leads (OpenNebula.org)             #
+#                                                                            #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
+# not use this file except in compliance with the License. You may obtain    #
+# a copy of the License at                                                   #
+#                                                                            #
+# http://www.apache.org/licenses/LICENSE-2.0                                 #
+#                                                                            #
+# Unless required by applicable law or agreed to in writing, software        #
+# distributed under the License is distributed on an "AS IS" BASIS,          #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+# See the License for the specific language governing permissions and        #
+# limitations under the License.                                             #
+#--------------------------------------------------------------------------- #
+
+require 'one_helper'
+
+class OneGroupHelper < OpenNebulaHelper::OneHelper
+    TABLE_CONF_FILE="#{OpenNebulaHelper::TABLE_CONF_PATH}/onegroup.yaml"
+
+    def self.rname
+        "GROUP"
+    end
+
+    private
+
+    def factory(id=nil)
+        if id
+            OpenNebula::Group.new_with_id(id, @client)
+        else
+            xml=OpenNebula::Group.build_xml
+            OpenNebula::Group.new(xml, @client)
+        end
+    end
+
+    def factory_pool(user_flag=-2)
+        #TBD OpenNebula::UserPool.new(@client, user_flag)
+        OpenNebula::GroupPool.new(@client)
+    end
+
+    def format_pool(pool, options, top=false)
+        table=CLIHelper::ShowTable.new(TABLE_CONF_FILE, self) do
+            column :ID, "ONE identifier for the Group", :size=>4 do |d|
+                d["ID"]
+            end
+
+            column :NAME, "Name of the Group", :left, :size=>15 do |d|
+                d["NAME"]
+            end
+            
+            column :USER, "Username of the Group owner", :left, :size=>8 do |d|
+                helper.uid_to_str(d["UID"], options)
+            end
+
+            default :ID, :USER, :NAME
+        end
+
+        if top
+            table.top(pool, options)
+        else
+            table.show(pool, options)
+        end
+    end
+end
