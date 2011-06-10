d6017d4ba49507971cab818067af275bb21e83ff
Ruben S. Montero <rubensm@dacya.ucm.es>
2011-06-10 16:58:28 +0200

    feature #662: Do not drop hosts with running vms

--- include/HostPool.h
+++ include/HostPool.h
@@ -139,6 +139,19 @@ public:
         }
     };
 
+    int drop(PoolObjectSQL * objsql, string& error_msg)
+    {
+        Host * host = static_cast<Host *>(objsql);
+
+        if ( host->get_share_running_vms() > 0 )
+        {
+            error_msg = "Can not remove a host with running VMs";
+            return -1;
+        }
+
+        return PoolSQL::drop(objsql, error_msg);
+    };
+
     /**
      *  Dumps the HOST pool in XML format. A filter can be also added to the
      *  query
