a5a143c232bf847a7c3f16fbc96c66716f73c7c6
Jaime Melis <j.melis@fdi.ucm.es>
2011-06-13 16:04:09 +0200

    feature #476: xen-firewall test

--- src/vnm_mad/spec/OpenNebulaVLAN_spec.rb
+++ src/vnm_mad/spec/OpenNebulaVLAN_spec.rb
@@ -16,11 +16,12 @@ end
 include SystemMock
 
 RSpec.configure do |config|
-    config.before(:all) do
+    config.before(:each) do
         $capture_commands = Hash.new
         $collector = Hash.new
     end
 end
+
 
 describe 'networking' do
     it "get all nics in kvm" do
@@ -78,11 +79,34 @@ describe 'openvswitch' do
         onevlan = OpenvSwitchVLAN.new(OUTPUT[:onevm_show],"kvm")
         onevlan.activate
         openvswitch_tags = [
-            "sudo /usr/local/bin/ovs-vsctl set Port vnet0 tap=2",
-            "sudo /usr/local/bin/ovs-vsctl set Port vnet1 tap=3",
-            "sudo /usr/local/bin/ovs-vsctl set Port vnet2 tap=4"
+            "sudo /usr/local/bin/ovs-vsctl set Port vnet0 tag=2",
+            "sudo /usr/local/bin/ovs-vsctl set Port vnet1 tag=3",
+            "sudo /usr/local/bin/ovs-vsctl set Port vnet2 tag=4"
             ]
 
         $collector[:system].should == openvswitch_tags
+    end
+end
+
+describe 'firewall' do
+    it "should activate FW rules in xen" do
+        $capture_commands = {
+            /uname/ => OUTPUT[:xen_uname_a],
+            /lsmod/ => OUTPUT[:xen_lsmod],
+            /network-list/ => OUTPUT[:xm_network_list],
+            /domid/ => OUTPUT[:xm_domid]
+        }
+        fw = OpenNebulaFirewall.new(OUTPUT[:onevm_show_xen])
+        fw.activate
+
+        fw_activate_rules = ["sudo /sbin/iptables -N one-36-3",
+                 "sudo /sbin/iptables -A FORWARD -m physdev --physdev-out vif4.0 -j one-36-3",
+                 "sudo /sbin/iptables -A one-36-3 -p tcp -m state --state ESTABLISHED -j ACCEPT",
+                 "sudo /sbin/iptables -A one-36-3 -p tcp -m multiport --dports 22,80 -j ACCEPT",
+                 "sudo /sbin/iptables -A one-36-3 -p tcp -j DROP",
+                 "sudo /sbin/iptables -A one-36-3 -p icmp -m state --state ESTABLISHED -j ACCEPT",
+                 "sudo /sbin/iptables -A one-36-3 -p icmp -j DROP"]
+
+        $collector[:system].should == fw_activate_rules
     end
 end
