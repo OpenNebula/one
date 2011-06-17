5f0dccd0f438478715f8c31ede3b56b57cac613c
Daniel Molina <danmolin@fdi.ucm.es>
2011-06-17 12:56:23 +0200

    Add License headers

--- src/onedb/onedb.rb
+++ src/onedb/onedb.rb
@@ -1,18 +1,18 @@
-ONE_LOCATION = ENV["ONE_LOCATION"]
-
-if !ONE_LOCATION
-    LIB_LOCATION      = "/usr/lib/one"
-    RUBY_LIB_LOCATION = LIB_LOCATION + "/ruby"
-    VAR_LOCATION      = "/var/lib/one"
-    ETC_LOCATION      = "/etc/one"
-    LOCK_FILE         = "/var/lock/one/one"
-else
-    LIB_LOCATION      = ONE_LOCATION + "/lib"
-    RUBY_LIB_LOCATION = LIB_LOCATION + "/ruby"
-    VAR_LOCATION      = ONE_LOCATION + "/var"
-    ETC_LOCATION      = ONE_LOCATION + "/etc"
-    LOCK_FILE         = VAR_LOCATION + "/.lock"
-end
+# -------------------------------------------------------------------------- #
+# Copyright 2002-2011, OpenNebula Project Leads (OpenNebula.org)             #
+#                                                                            #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
+# not use this file except in compliance with the License. You may obtain    #
+# a copy of the License at                                                   #
+#                                                                            #
+# http://www.apache.org/licenses/LICENSE-2.0                                 #
+#                                                                            #
+# Unless required by applicable law or agreed to in writing, software        #
+# distributed under the License is distributed on an "AS IS" BASIS,          #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+# See the License for the specific language governing permissions and        #
+# limitations under the License.                                             #
+#--------------------------------------------------------------------------- #
 
 require 'cloud/Configuration'
 require 'onedb_backend'
