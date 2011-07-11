7d266cc63e920b1c5248d50c66785d5e136b3024
Carlos Martín <cmartins@fdi.ucm.es>
2011-07-11 17:52:51 +0200

    onedb: more thorough tests

--- src/onedb/test/create.sh
+++ src/onedb/test/create.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
+# This script populates a DB with different resources
+# Set the TWO_SERIES flag if the target OpenNebula is 2.0 or 2.2
+
+# The oneadmin's auth must be in ~/.one/one_auth
+
 TWO_SERIES="no"
 #TWO_SERIES="yes"
 
+ONEADMIN_AUTH=$ONE_AUTH
 
 TMP_FILE="tmp_file"
 
+# 5 Users
 
-# 5 Hosts and Clusters
+for i in 0 1 2 3 4; do
+    echo "user_$i:pass_$i" > one_auth$i
+    oneuser create user_$i pass_$i
+done
+
+
+# 5 Hosts
 
 for i in 0 1 2 3 4; do
     onehost create host_$i im_dummy vmm_dummy tm_dummy
@@ -18,13 +31,18 @@ done
 for i in 0 1 2; do
     (   echo "NAME = vnet_fixed_$i"
         echo "TYPE = FIXED"
+        echo "PUBLIC = FIXED"
         echo "BRIDGE = vbr1"
+        echo "EXTRA_ATT = \"EXTRA_VALUE FOR VNET $i\""
         echo "LEASES = [IP=192.168.$i.1]"
         echo "LEASES = [IP=192.168.$i.2]"
         echo "LEASES = [IP=192.168.$i.3]"
         echo "LEASES = [IP=192.168.$i.4]"
         echo "LEASES = [IP=192.168.$i.5]"
     ) > $TMP_FILE
+
+    # Change auth
+    export ONE_AUTH="`pwd`/one_auth$i"
 
     onevnet create $TMP_FILE
 done
@@ -37,9 +55,14 @@ for i in 3 4; do
         echo "BRIDGE = vbr0"
         echo "NETWORK_SIZE    = C"
         echo "NETWORK_ADDRESS = 192.168.$i.0"
+        echo "EXTRA_ATT       = \"EXTRA_VALUE FOR VNET $i\""
     ) > $TMP_FILE
 
+    # Change auth
+    export ONE_AUTH="`pwd`/one_auth$i"
+
     onevnet create $TMP_FILE
+    onevnet publish $i
 done
 
 # 5 Images
@@ -48,15 +71,12 @@ for i in 0 1 2 3 4; do
     (   echo "NAME          = image_$i"
         echo "TYPE          = DATABLOCK"
         echo "PATH          = /dev/null"
+        echo "EXTRA_ATT     = \"EXTRA_VALUE FOR IMG $i\""
     ) > $TMP_FILE
 
+    export ONE_AUTH="`pwd`/one_auth$i"
+
     oneimage create $TMP_FILE
-done
-
-# 5 Users
-
-for i in 0 1 2 3 4; do
-    oneuser create user_$i pass_$i
 done
 
 # 5 VMs
@@ -82,7 +102,11 @@ for i in 0 1 2 3 4; do
         echo "REQUIREMENTS = \"CPUSPEED > 1000\"   "
         echo "RANK         = FREECPU               "
         echo "CONTEXT = [ files = \"/dev/null\"  ] "
+
+        echo "EXTRA_ATT = \"EXTRA_VALUE FOR VM $i\""
     ) > $TMP_FILE
+
+    export ONE_AUTH="`pwd`/one_auth$i"
 
     if [ $TWO_SERIES == "yes" ]; then
         onevm create $TMP_FILE
@@ -90,15 +114,26 @@ for i in 0 1 2 3 4; do
         onetemplate create $TMP_FILE
         onetemplate instantiate $i
     fi
+done
 
+
+if [ -n "$ONEADMIN_AUTH" ] ; then
+    export ONE_AUTH=$ONEADMIN_AUTH
+else
+    # Unset the one_auth file, cli will look for it in ~/.one/one_auth
+    unset ONE_AUTH
+fi
+
+for i in 0 1 2 3; do
     onevm deploy $i $i
 done
 
 rm $TMP_FILE
 
+
 echo -n "Waiting until all VMs are running "
 
-while [ $(onevm list | grep -c runn) -ne 5 ]; do
+while [ $(onevm list | grep -c runn) -ne 4 ]; do
     echo -n "."
     sleep 0.5s
 done
@@ -108,17 +143,34 @@ echo " ok"
 # Wait for some monitorization data
 sleep 3s
 
+
 onevm migrate 0 1
-while [ $(onevm list | grep -c runn) -ne 5 ]; do sleep 0.5s; done
+while [ $(onevm list a | grep -c runn) -ne 4 ]; do sleep 0.5s; done
+
 
 onevm livemigrate 1 2
-while [ $(onevm list | grep -c runn) -ne 5 ]; do sleep 0.5s; done
+while [ $(onevm list a | grep -c runn) -ne 4 ]; do sleep 0.5s; done
+
+onevm stop 1
+while [ $(onevm list a | grep -c stop) -ne 1 ]; do sleep 0.5s; done
+
+onevm resume 1
+while [ $(onevm list a | grep -c pend) -ne 2 ]; do sleep 0.5s; done
+
+onevm deploy 1 0
+while [ $(onevm list a | grep -c runn) -ne 4 ]; do sleep 0.5s; done
+
+onevm migrate 1 3
+while [ $(onevm list a | grep -c runn) -ne 4 ]; do sleep 0.5s; done
+
+onevm shutdown 1
+while [ $(onevm list a | grep -c runn) -ne 3 ]; do sleep 0.5s; done
 
 onevm shutdown 2
-while [ $(onevm list | grep -c runn) -ne 4 ]; do sleep 0.5s; done
+while [ $(onevm list a | grep -c runn) -ne 2 ]; do sleep 0.5s; done
 
 onevm delete 3
-while [ $(onevm list | grep -c runn) -ne 3 ]; do sleep 0.5s; done
+while [ $(onevm list a | grep -c runn) -ne 1 ]; do sleep 0.5s; done
 
 onehost disable 3
 oneimage persistent 3
@@ -130,8 +182,20 @@ sleep 3s
 
 mkdir -p results/xml_files
 
-for obj in host vnet image vm; do
+for obj in host vnet image vm user; do
+    one$obj list -x > results/xml_files/$obj-pool.xml
+
     for i in 0 1 2 3 4; do
         one$obj show -x $i > results/xml_files/$obj-$i.xml
     done
+done
+
+if [ $TWO_SERIES == "no" ]; then
+    oneacl list -x > results/xml_files/acl-pool.xml
+    onegroup list -x > results/xml_files/group-pool.xml
+fi
+
+# Clean one auth files
+for i in 0 1 2 3 4; do
+    rm one_auth$i
 done
