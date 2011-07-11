OpenNebula/one

commit 5bf12e0557ddbe0d891a11176f47b29fbd62d2c5

bug #707: tentative fix for xpath problem in onevm show (2.2)

src/cli/client_utilities.rb
@@ -314,11 +314,11 @@ def get_cluster_id(name)
 end
 
 def str_running_time(data)
-    stime=Time.at(data["STIME"].to_i)
-    if data["ETIME"]=="0"
+    stime=Time.at(data["/VM/STIME"].to_i)
+    if data["/VM/ETIME"]=="0"
         etime=Time.now
     else
-        etime=Time.at(data["ETIME"].to_i)
+        etime=Time.at(data["/VM/ETIME"].to_i)
     end
     dtime=Time.at(etime-stime).getgm
 

src/cli/onevm
@@ -859,7 +859,7 @@ true)
                 puts str % ["STATE", vm.state_str]
                 puts str % ["LCM_STATE", vm.lcm_state_str]
 
-                value=vm['STIME'].to_i
+                value=vm['/VM/STIME'].to_i
                 if value==0
                     value='-'
                 else
@@ -867,7 +867,7 @@ true)
                 end
                 puts str % ["START TIME", value]
 
-                value=vm['ETIME'].to_i
+                value=vm['/VM/ETIME'].to_i
                 if value==0
                     value='-'
                 else

commit d07323d2940acd9195dcad690d5dea9b66aff37a

bug #707: fix bug introduced in 5bf12e0, time not shown in onevm list

src/cli/client_utilities.rb
@@ -314,11 +314,11 @@ def get_cluster_id(name)
 end
 
 def str_running_time(data)
-    stime=Time.at(data["/VM/STIME"].to_i)
-    if data["/VM/ETIME"]=="0"
+    stime=Time.at(data["STIME"].to_i)
+    if data["ETIME"]=="0"
         etime=Time.now
     else
-        etime=Time.at(data["/VM/ETIME"].to_i)
+        etime=Time.at(data["ETIME"].to_i)
     end
     dtime=Time.at(etime-stime).getgm
 