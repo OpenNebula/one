8db03b2bfa039a848683467c1d374d69cf942c57
Carlos Martín <cmartins@fdi.ucm.es>
2011-09-28 03:39:23 -0700

    Revert "Allow user passwords with ':' characters"
    This reverts commit 6c5e90eb844e70df23e5cb0f37782e8f4b272299.

--- src/um/User.cc
+++ src/um/User.cc
@@ -27,7 +27,7 @@
 #include "Group.h"
 
 
-const string User::INVALID_CHARS = " \t\n\v\f\r";
+const string User::INVALID_CHARS = " :\t\n\v\f\r";
 
 /* ************************************************************************** */
 /* User :: Database Access Functions                                          */
