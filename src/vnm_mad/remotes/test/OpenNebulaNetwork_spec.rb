bae7cc9b9cd2e28a588d3e762f509a7beee63114
Ruben S. Montero <rubensm@dacya.ucm.es>
2011-12-02 00:19:09 +0100

    feature #863: Get unit tests for VNET drivers run again. (Still needs fix)

--- src/vnm_mad/remotes/test/OpenNebulaNetwork_spec.rb
+++ src/vnm_mad/remotes/test/OpenNebulaNetwork_spec.rb
@@ -1,7 +1,11 @@
 #!/usr/bin/env ruby
 
-$: << File.dirname(__FILE__) + '/..' \
-   << './'
+$: << File.dirname(__FILE__) + '/..' 
+$: << File.dirname(__FILE__) + '/../ebtables' 
+$: << File.dirname(__FILE__) + '/../802.1Q' 
+$: << File.dirname(__FILE__) + '/../ovswitch' 
+$: << File.dirname(__FILE__) + '/../../../mad/ruby' 
+$: << './'
 
 require 'rubygems'
 require 'rspec'
