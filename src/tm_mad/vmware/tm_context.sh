fb34a12573fdf815314d4fa33c064db963f7ffbd
Tino Vazquez <tinova79@gmail.com>
2011-12-07 19:57:04 +0100

    feature #1020: Fix typo in configuration file

--- src/tm_mad/vmware/tm_context.sh
+++ src/tm_mad/vmware/tm_context.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
-# --------------------------------------------------------------------------
-# Copyright 2010, C12G Labs S.L.
-#
-# This file is part of OpenNebula VMware Drivers.
-#
-# OpenNebula VMware Drivers is free software: you can redistribute it
-# and/or modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation, either version 3 of
-# the License, or the hope That it will be useful, but (at your
-# option) any later version.
-#
-# OpenNebula VMware Drivers is distributed in WITHOUT ANY WARRANTY;
-# without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.  See the GNU General Public License for more
-# details.
-#
-# You should have received a copy of the GNU General Public License
-# along with OpenNebula VMware Drivers . If not, see
-# <http://www.gnu.org/licenses/>
-# --------------------------------------------------------------------------
+# ---------------------------------------------------------------------------- #
+# Copyright 2010-2011, C12G Labs S.L                                           #
+#                                                                              #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may      #
+# not use this file except in compliance with the License. You may obtain      #
+# a copy of the License at                                                     #
+#                                                                              #
+# http://www.apache.org/licenses/LICENSE-2.0                                   #
+#                                                                              #
+# Unless required by applicable law or agreed to in writing, software          #
+# distributed under the License is distributed on an "AS IS" BASIS,            #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.     #
+# See the License for the specific language governing permissions and          #
+# limitations under the License.                                               #
+# ---------------------------------------------------------------------------- #
 
 while (( "$#" )); do
     if [ "$#" == "1" ]; then
