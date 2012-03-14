OpenNebula/one

commit 92dd8d4c5ed0af8099b7fc0547deee748fe36dfe

feature #1112: Save_as functionality

include/VirtualMachine.h
@@ -653,15 +653,27 @@ public:
     int  generate_context(string &files);
 
     // ------------------------------------------------------------------------
-    // Image repository related functions
+    // Datastore related functions
     // ------------------------------------------------------------------------
     /**
      *  Set the SAVE_AS attribute for the "disk_id"th disk.
      *    @param  disk_id Index of the disk to save
-     *    @param  img_id ID of the image this disk will be saved to.
+     *    @param  source to save the disk (SAVE_AS_SOURCE)
+     *    @param  img_id ID of the image this disk will be saved to (SAVE_AS).
      *    @return 0 if success
      */
-    int  save_disk(int disk_id, int img_id, string& error_str);
+    int save_disk(const string& disk_id, 
+                  const string& source,
+                  int img_id);
+
+    /**
+     * Get the original image id of the disk. It also checks that the disk can
+     * be saved_as.
+     *    @param  disk_id Index of the disk to save
+     *    @param  error_str describes the error
+     *    @return -1 if failure
+     */
+    int get_image_from_disk(int disk_id, string& error_str);
 
     // ------------------------------------------------------------------------
     // Authorization related functions

include/Image.h
@@ -40,6 +40,22 @@ public:
     };
 
     /**
+     *  Return the string representation of an ImageType
+     *    @param ob the type
+     *    @return the string
+     */ 
+    static string type_to_str(ImageType ob)
+    {
+        switch (ob)
+        {
+            case OS:        return "OS" ; break;
+            case CDROM:     return "CDROM" ; break;
+            case DATABLOCK: return "DATABLOCK" ; break;
+            default:        return "";
+        }
+    };
+
+    /**
      *  Image State
      */
     enum ImageState
@@ -200,6 +216,17 @@ public:
     }
 
     /**
+     *  Check if the image is used for saving_as a current one
+     *  @return true if the image will be used to save an existing image.
+     */
+    bool isSaving()
+    {
+        ImageTemplate * it = static_cast<ImageTemplate *>(obj_template);
+
+        return it->is_saving();
+    }
+
+    /**
      *  Set permissions for the Image. Extends the PoolSQLObject method
      *  by checking the persistent state of the image.
      */
@@ -291,7 +318,7 @@ public:
     /**
      *  Factory method for image templates
      */
-    Template * get_new_template()
+    Template * get_new_template() const
     {
         return new ImageTemplate;
     }
@@ -299,11 +326,19 @@ public:
     /**
      * Returns the Datastore ID
      */
-    int get_ds_id()
+    int get_ds_id() const
     {
         return ds_id;
     };
 
+    /**
+     * Returns the Datastore ID
+     */
+    const string& get_ds_name() const
+    {
+        return ds_name;
+    };
+    
 private:
 
     // -------------------------------------------------------------------------

include/ImageManager.h
@@ -99,14 +99,8 @@ public:
     /**
      *  Releases an image and triggers any needed operations in the repo
      *    @param iid image id of the image to be released
-     *    @param disk_path base path for disk location
-     *    @param disk number for this image in the VM
-     *    @param saveid id of image to save the current image
      */
-    void release_image(const string& iid,
-                       const string& disk_path, 
-                       int           disk_num, 
-                       const string& saveid)
+    void release_image(const string& iid)
     {
         int           image_id;
         istringstream iss;
@@ -114,30 +108,14 @@ public:
         iss.str(iid);
         iss >> image_id;
 
-        release_image(image_id, disk_path, disk_num, saveid);
+        release_image(image_id);
     };
 
     /**
      *  Releases an image and triggers any needed operations in the repo
      *    @param iid image id of the image to be released
-     *    @param disk_path base path for disk location
-     *    @param disk number for this image in the VM
-     *    @param saveid id of image to save the current image
      */
-    void release_image(int           iid,
-                       const string& disk_path,
-                       int           disk_num,
-                       const string& saveid);
-
-    /**
-     *  Moves a VM disk to the Image Repository
-     *    @param disk_path base path for disk location
-     *    @param disk number for this image in the VM
-     *    @param saveid id of image to save the current image
-     */
-    void disk_to_image(const string& disk_path, 
-                       int           disk_num, 
-                       const string& save_id);
+    void release_image(int iid);
 
     /**
      *  Enables the image
@@ -205,9 +183,7 @@ private:
      *    @param action the name of the action
      *    @param arg arguments for the action function
      */
-    void do_action(
-        const string &  action,
-        void *          arg);
+    void do_action(const string& action, void * arg);
 
     /**
      *  Acquires an image updating its state.
@@ -230,9 +206,7 @@ private:
      *    @param ds_data Datastore XML representation
      *    @return the XML message
      */
-    string * format_message(
-        const string& img_data,
-        const string& ds_data);
+    string * format_message(const string& img_data, const string& ds_data);
 };
 
 #endif /*IMAGE_MANAGER_H*/

include/ImageTemplate.h
@@ -41,10 +41,35 @@ public:
         return Template::check(rs_attr, restricted_attributes);
     };
 
+    bool is_saving()
+    {
+        string saving;
+
+        get(saving_attribute, saving);
+
+        return (saving.empty() == false);
+    }
+
+    void set_saving()
+    {
+        SingleAttribute * attr= new SingleAttribute(saving_attribute, "YES");
+
+        erase(saving_attribute);
+
+        set(attr);
+    }
+
+    void unset_saving()
+    {
+        erase(saving_attribute);
+    }
+
 private:
     friend class ImagePool;
 
     static vector<string> restricted_attributes;
+
+    static string saving_attribute;
 
     /**
      * Stores the attributes as restricted, these attributes will be used in

include/RequestManagerVirtualMachine.h
@@ -48,8 +48,11 @@ protected:
     virtual void request_execute(xmlrpc_c::paramList const& _paramList,
             RequestAttributes& att) = 0;
 
-    bool vm_authorization(int id, ImageTemplate *tmpl,
-            RequestAttributes& att, PoolObjectAuth* host_perms);
+    bool vm_authorization(int id, 
+                          ImageTemplate *    tmpl,
+                          RequestAttributes& att, 
+                          PoolObjectAuth *   host_perms, 
+                          PoolObjectAuth *   ds_perm);
 
     int get_host_information(int hid, string& name, string& vmm, string& vnm,
             RequestAttributes& att, PoolObjectAuth& host_perms);

src/image/Image.cc
@@ -96,6 +96,7 @@ int Image::insert(SqlDB *db, string& error_str)
     string dev_prefix;
     string source_attr;
     string aname;
+    string saved_id;
 
     ostringstream oss;
 
@@ -159,33 +160,35 @@ int Image::insert(SqlDB *db, string& error_str)
     erase_template_attribute("PATH", path);
     erase_template_attribute("SOURCE", source);
 
-    // The template should contain PATH or SOURCE
-    if ( source.empty() && path.empty() )
+    if (!isSaving()) //Not a saving image
     {
-        string        size_attr;
-        istringstream iss;
-
-        erase_template_attribute("SIZE",   size_attr);
-        erase_template_attribute("FSTYPE", fs_type);
-
-        // DATABLOCK image needs SIZE and FSTYPE
-        if (type != DATABLOCK || size_attr.empty() || fs_type.empty())
+        if ( source.empty() && path.empty() )
         {
-            goto error_no_path;
+            string        size_attr;
+            istringstream iss;
+
+            erase_template_attribute("SIZE",   size_attr);
+            erase_template_attribute("FSTYPE", fs_type);
+
+            // DATABLOCK image needs SIZE and FSTYPE
+            if (type != DATABLOCK || size_attr.empty() || fs_type.empty())
+            {
+                goto error_no_path;
+            }
+
+            iss.str(size_attr);
+
+            iss >> size_mb;
+
+            if (iss.fail() == true)
+            {
+                goto error_size_format;
+            }
         }
-
-        iss.str(size_attr);
-
-        iss >> size_mb;
-
-        if (iss.fail() == true)
+        else if ( !source.empty() && !path.empty() )
         {
-            goto error_size_format;
+            goto error_path_and_source;
         }
-    }
-    else if ( !source.empty() && !path.empty() )
-    {
-        goto error_path_and_source;
     }
 
     state = LOCKED; //LOCKED till the ImageManager copies it to the Repository

src/image/ImageManagerActions.cc
@@ -128,83 +128,13 @@ int ImageManager::acquire_image(Image *img, string& error)
 /* -------------------------------------------------------------------------- */
 /* -------------------------------------------------------------------------- */
 
-void ImageManager::move_image(Image *img, const string& source)
-{
-    const ImageManagerDriver* imd = get();
-    ostringstream oss;
-
-    if ( imd == 0 )
-    {
-        NebulaLog::log("ImM",Log::ERROR,
-                "Could not get driver to update repository");
-        return;
-    }
-
-    oss << "Moving disk " << source << " to repository image " 
-        << img->get_oid();
-
-    imd->mv(img->get_oid(),source,img->get_source());
-
-    NebulaLog::log("ImM",Log::INFO,oss);
-}
-
-/* -------------------------------------------------------------------------- */
-/* -------------------------------------------------------------------------- */
-
-void ImageManager::disk_to_image(const string& disk_path, 
-                                 int           disk_num, 
-                                 const string& save_id)
-{
-    int sid;
-
-    istringstream iss;
-    Image *       img;
-
-    ostringstream disk_file;
-
-    iss.str(save_id);
-
-    iss >> sid;
-
-    img = ipool->get(sid,true);
-
-    if ( img == 0 )
-    {
-        NebulaLog::log("ImM",Log::ERROR,"Could not get image to saveas disk.");
-    }
-    else
-    {
-        disk_file << disk_path << "/disk." << disk_num;
-
-        move_image(img,disk_file.str());
-    }
-
-    img->unlock();
-}
-
-/* -------------------------------------------------------------------------- */
-/* -------------------------------------------------------------------------- */
-
-void ImageManager::release_image(int           iid,
-                                 const string& disk_path, 
-                                 int           disk_num, 
-                                 const string& save_id)
+void ImageManager::release_image(int iid)
 {
     int rvms;
 
-    int sid = -1;
-
-    istringstream iss;
-    Image *       img;
+    Image * img;
 
     ostringstream disk_file;
-
-    if ( save_id.empty() == false )
-    {
-        iss.str(save_id);
-
-        iss >> sid;
-    }
 
     img = ipool->get(iid,true);
 
@@ -218,47 +148,13 @@ void ImageManager::release_image(int           iid,
         case Image::USED:
             rvms = img->dec_running();
 
-            if ( img->isPersistent() && !disk_path.empty() )
+            if ( img->isPersistent() || rvms == 0 )
             {
-                disk_file << disk_path << "/disk." << disk_num;
-
-                img->set_state(Image::LOCKED);
-
-                move_image(img,disk_file.str());
+                img->set_state(Image::READY);
 
                 ipool->update(img);
 
                 img->unlock();
-            }
-            else 
-            {
-                if ( rvms == 0)
-                {
-                    img->set_state(Image::READY);
-                }
-
-                ipool->update(img);
-
-                img->unlock();
-
-                if ( sid != -1 )
-                {
-                    img = ipool->get(sid,true);
-
-                    if ( img == 0 )
-                    {
-                        NebulaLog::log("ImM",Log::ERROR,
-                            "Could not get image to saveas disk.");
-                    }
-                    else
-                    {
-                        disk_file << disk_path << "/disk." << disk_num;
-
-                        move_image(img,disk_file.str());
-                    }
-
-                    img->unlock();
-                }
             }
         break;
 
@@ -372,8 +268,7 @@ int ImageManager::delete_image(int iid, const string& ds_data)
     }
 
     drv_msg = format_message(img->to_xml(img_tmpl), ds_data);
-
-    source = img->get_source();
+    source  = img->get_source();
 
     if (source.empty())
     {
@@ -431,30 +326,25 @@ int ImageManager::register_image(int iid, const string& ds_data)
     drv_msg = format_message(img->to_xml(img_tmpl), ds_data);
     path    = img->get_path();
 
-    if ( path.empty() == true ) //NO PATH -> USE SOURCE OR MKFS FOR DATABLOCK
+    if ( path.empty() == true ) //NO PATH
     {
-        if ( img->get_type() == Image::DATABLOCK)
-        {
-            string fs   = img->get_fstype();
-            int    size = img->get_size();
+        string source = img->get_source();
 
+        if ( img->isSaving() || img->get_type() == Image::DATABLOCK )
+        {
             imd->mkfs(img->get_oid(), *drv_msg);
          
-            oss << "Creating disk at " << img->get_source() << " of " 
-                << size << "Mb with format " << fs;
+            oss << "Creating disk at " << source 
+                << " of "<<  img->get_size()
+                << "Mb (type: " <<  img->get_fstype() << ")";
         }
-        else
+        else if ( !source.empty() ) //Source in Template
         {
-            string source = img->get_source();
+            img->set_state(Image::READY);
+            ipool->update(img);
 
-            if (source != "-") //SAVE_AS IMAGE DO NOT ENABLE THE IMAGE
-            {
-                img->set_state(Image::READY);
-                ipool->update(img);
-
-                oss << "Using source " << img->get_source() 
-                    << " from template for image " << img->get_name();
-            }
+            oss << "Using source " << source
+                << " from template for image " << img->get_name();
         }
     }
     else //PATH -> COPY TO REPOSITORY AS SOURCE

src/image/ImageManagerDriver.cc
@@ -179,40 +179,69 @@ void ImageManagerDriver::protocol(
             goto error_cp;
         }
     }
-    else if ( action == "MV" )
-    {
-        if ( result == "SUCCESS" )
-        {
-            if (image->get_source() == "-")
-            {
-                image->set_source(source);
-            }
-
-            image->set_size(size_mb);
-
-            image->set_state(Image::READY);
-
-            ipool->update(image);
-
-            NebulaLog::log("ImM", Log::INFO, "Image saved and ready to use.");
-        }
-        else
-        {
-            goto error_mv;
-        }
-    }
     else if ( action == "MKFS" )
     {
         if ( result == "SUCCESS" )
         {
-            image->set_source(source);
-            image->set_size(size_mb);
+            bool   is_saving = image->isSaving();
 
-            image->set_state(Image::READY);
+            string disk_id;
+            string vm_id;
+            int    rc;
+
+            image->set_source(source);
+
+            if (is_saving)
+            {
+                image->get_template_attribute("SAVED_DISK_ID",disk_id);
+                image->get_template_attribute("SAVED_VM_ID",  vm_id);
+            }
+            else
+            {
+                image->set_size(size_mb);
+
+                image->set_state(Image::READY);
+
+                NebulaLog::log("ImM", Log::INFO, 
+                               "Image created and ready to use");
+            }
 
             ipool->update(image);
 
-            NebulaLog::log("ImM", Log::INFO, "Image created and ready to use");
+            image->unlock();
+
+            if (is_saving)
+            {
+                Nebula& nd = Nebula::instance();
+
+                VirtualMachinePool * vmpool = nd.get_vmpool();
+
+                VirtualMachine * vm;
+                istringstream    iss(vm_id);
+
+                int vm_id_i;
+
+                iss >> vm_id_i;
+
+                vm = vmpool->get(vm_id_i, true);
+
+                if ( vm == 0 )
+                {
+                    goto error_save_no_vm;
+                }
+
+                rc = vm->save_disk(disk_id, source, id);
+
+                if ( rc == -1 )
+                {
+                    vm->unlock();
+                    goto error_save_state_vm;
+                }
+
+                vm->unlock();
+            }
+
+            return;
         }
         else
         {
@@ -255,11 +284,6 @@ error_cp:
     os << "Error copying image in the repository";
     goto error_common;
 
-error_mv:
-    os.str("");
-    os << "Error saving image to the repository";
-    goto error_common;
-
 error_mkfs:
     os.str("");
     os << "Error creating datablock";
@@ -280,8 +304,27 @@ error_rm:
     }
 
     NebulaLog::log("ImM", Log::ERROR, os);
-
     return;
+
+error_save_no_vm:
+    os.str("");
+    os << "Image created for SAVE_AS, but the associated VM does not exist.";
+
+    goto error_save_common;
+
+error_save_state_vm:
+    os.str("");
+    os << "Image created for SAVE_AS, but VM is no longer running";
+
+    goto error_save_common;
+
+error_save_common:
+    image = ipool->get(id, true);
+
+    if (image == 0 )
+    {
+        return;
+    }
 
 error_common:
     getline(is,info);

commit 3c1cf1f1b3c91c87e3a932d615d3ac8ebed60a4c

Feature #1112: Fix oneimage update (broken in commit:92dd8d4c)

include/Datastore.h
@@ -191,7 +191,7 @@ private:
     /**
      *  Factory method for virtual network templates
      */
-    Template * get_new_template()
+    Template * get_new_template() const
     {
         return new DatastoreTemplate;
     }

include/Host.h
@@ -281,7 +281,7 @@ public:
     /**
      *  Factory method for host templates
      */
-    Template * get_new_template()
+    Template * get_new_template() const
     {
         return new HostTemplate;
     }

include/PoolObjectSQL.h
@@ -363,7 +363,7 @@ public:
      *  by classes that uses templates
      *    @return a new template
      */
-    virtual Template * get_new_template()
+    virtual Template * get_new_template() const
     {
         return 0;
     }

include/User.h
@@ -162,7 +162,7 @@ public:
     /**
      *  Factory method for image templates
      */
-    Template * get_new_template()
+    Template * get_new_template() const
     {
         return new UserTemplate;
     }

include/VMTemplate.h
@@ -44,7 +44,7 @@ public:
     /**
      *  Factory method for virtual machine templates
      */
-    Template * get_new_template()
+    Template * get_new_template() const
     {
         return new VirtualMachineTemplate;
     }

include/VirtualMachine.h
@@ -542,7 +542,7 @@ public:
     /**
      *  Factory method for virtual machine templates
      */
-    Template * get_new_template()
+    Template * get_new_template() const
     {
         return new VirtualMachineTemplate;
     }

include/VirtualNetwork.h
@@ -62,7 +62,7 @@ public:
     /**
      *  Factory method for virtual network templates
      */
-    Template * get_new_template()
+    Template * get_new_template() const
     {
         return new VirtualNetworkTemplate;
     }