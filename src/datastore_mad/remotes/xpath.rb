ec7c9106d6296b5fdb48df3a00b9f8811554fdab
Jaime Melis <jmelis@opennebula.org>
2012-04-18 02:01:31 +0200

    Feature #1240: Add LVM drivers adapted to the new datastore subsystem (cherry picked from commit 8379ac09aa56971f69e8f2986d6a61723ec23c43)
    Conflicts:
    
    	src/datastore_mad/remotes/xpath.rb

--- src/datastore_mad/remotes/xpath.rb
+++ src/datastore_mad/remotes/xpath.rb
@@ -22,18 +22,24 @@
 require 'base64'
 require 'rexml/document'
 require 'getoptlong'
+require 'pp'
 
 opts = opts = GetoptLong.new(
+    [ '--stdin',   '-s', GetoptLong::NO_ARGUMENT ],
     [ '--base64',  '-b', GetoptLong::REQUIRED_ARGUMENT ]
 )
 
-tmp64 = ""
+source = :stdin
+tmp64  = ""
 
 begin
     opts.each do |opt, arg|
         case opt
+            when '--stdin'
+                source = :stdin
             when '--base64'
-                tmp64 = arg
+                source = :b64
+                tmp64  = arg
         end
     end
 rescue Exception => e
@@ -42,7 +48,13 @@ end
 
 values = ""
 
-tmp = Base64::decode64(tmp64)
+case source
+when :stdin
+    tmp = STDIN.read
+when :b64
+    tmp = Base64::decode64(tmp64)
+end
+
 xml = REXML::Document.new(tmp).root
 
 ARGV.each do |xpath|
