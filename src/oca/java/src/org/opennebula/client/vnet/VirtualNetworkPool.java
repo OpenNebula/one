c73248643d442abc9939a1ad3a4775dc8b0d189f
Carlos Martín <cmartins@fdi.ucm.es>
2012-04-20 12:20:26 +0300

    Bug #1127: Add comments to java oca getById methods

--- src/oca/java/src/org/opennebula/client/vnet/VirtualNetworkPool.java
+++ src/oca/java/src/org/opennebula/client/vnet/VirtualNetworkPool.java
@@ -249,7 +249,7 @@ public class VirtualNetworkPool extends Pool implements Iterable<VirtualNetwork>
 
     /**
      * Returns the Virtual Network with the given Id from the pool. If it is not found,
-     * then returns null.
+     * then returns null. The method {@link #info()} must be called before.
      *
      * @param id of the Virtual Network to retrieve
      * @return The Image with the given Id, or null if it was not found.
