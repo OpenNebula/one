3ad4956f6fdbafac089995994574b82294803f44
Daniel Molina <dmolina@opennebula.org>
2012-06-11 13:02:17 +0200

    feature #1303: add onlyOneCheckBox listener

--- src/sunstone/public/js/plugins/marketplace-tab.js
+++ src/sunstone/public/js/plugins/marketplace-tab.js
@@ -1,6 +1,25 @@
+/* -------------------------------------------------------------------------- */
+/* Copyright 2002-2012, OpenNebula Project Leads (OpenNebula.org)             */
+/*                                                                            */
+/* Licensed under the Apache License, Version 2.0 (the "License"); you may    */
+/* not use this file except in compliance with the License. You may obtain    */
+/* a copy of the License at                                                   */
+/*                                                                            */
+/* http://www.apache.org/licenses/LICENSE-2.0                                 */
+/*                                                                            */
+/* Unless required by applicable law or agreed to in writing, software        */
+/* distributed under the License is distributed on an "AS IS" BASIS,          */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   */
+/* See the License for the specific language governing permissions and        */
+/* limitations under the License.                                             */
+/* -------------------------------------------------------------------------- */
+
 /*Marketpplace tab plugin*/
 var dataTable_marketplace;
 
+/*
+ * fnReloadAjax: re-read the Ajax source and update the table
+ */
 
 $.fn.dataTableExt.oApi.fnReloadAjax = function ( oSettings, sNewSource, fnCallback, bStandingRedraw )
 {
@@ -47,6 +66,156 @@ $.fn.dataTableExt.oApi.fnReloadAjax = function ( oSettings, sNewSource, fnCallba
     }, oSettings );
 }
 
+
+/*
+ * MAIN TAB
+ */
+
+var market_actions = {
+    "Marketplace.refresh" : {
+        type: "custom",
+        call: function () {
+            dataTable_marketplace.fnReloadAjax();
+        }
+    },
+    "Marketplace.import" : {
+        type: "single",
+        call: function () {
+            var app_id = getSelectedNodes(dataTable_marketplace)[0];
+
+            $.ajax({
+                url: "/marketplace/" + app_id,
+                type: "GET",
+                dataType: "json",
+                success: function(response){
+                    document.getElementById("img_name").value = response['name'];
+                    document.getElementById("img_path").value = response['links']['download']['href'];
+                    popUpCreateImageDialog();
+                },
+                error: function(response)
+                {
+                    return onError(null, OpenNebula.Error(response));
+                }
+            });
+        }
+    }
+}
+
+var market_buttons = {
+    "Marketplace.refresh" : {
+        type: "image",
+        text: tr("Refresh list"),
+        img: "images/Refresh-icon.png"
+    },
+    "Marketplace.import" : {
+        type: "action",
+        text: tr('Import to local infrastructure')
+    }
+};
+
+var marketplace_tab_content = '\
+<h2>'+tr("OpenNebula Marketplace")+'</h2>\
+<form id="marketplace_form" action="" action="javascript:alert(\'js error!\');">\
+  <div class="action_blocks">\
+  </div>\
+<table id="datatable_marketplace" class="display">\
+  <thead>\
+    <tr>\
+      <th class="check"></th>\
+      <th>'+tr("ID")+'</th>\
+      <th>'+tr("Name")+'</th>\
+      <th>'+tr("Publisher")+'</th>\
+      <th>'+tr("Hypervisor")+'</th>\
+      <th>'+tr("Arch")+'</th>\
+      <th>'+tr("Format")+'</th>\
+      <th>'+tr("Tags")+'</th>\
+    </tr>\
+  </thead>\
+  <tbody id="tbodymarketplace">\
+  </tbody>\
+</table>\
+</div>';
+
+
+var marketplace_tab = {
+    title: '<i class="icon-shopping-cart"></i>' + tr("Marketplace"),
+    content: marketplace_tab_content,
+    buttons: market_buttons,
+};
+
+Sunstone.addMainTab('marketplace_tab', marketplace_tab);
+Sunstone.addActions(market_actions);
+
+
+/*
+ * INFO PANEL
+ */
+
+var marketplace_info_panel = {
+    "marketplace_info_tab" : {
+        title: tr("Appliance information"),
+        content:""
+    },
+};
+
+Sunstone.addInfoPanel("marketplace_info_panel", marketplace_info_panel);
+
+
+function updateMarketInfo(request,app){
+    var info_tab = {
+        title : tr("Appliance information"),
+        content :
+        '<table id="info_marketplace_table" class="info_table">\
+            <thead>\
+              <tr><th colspan="2">'+tr("Appliance information")+'</th></tr>\
+            </thead>\
+            <tbody>\
+              <tr>\
+                <td class="key_td">' + tr("ID") + '</td>\
+                <td class="value_td">'+app['_id']["$oid"]+'</td>\
+              </tr>\
+              <tr>\
+                <td class="key_td">' + tr("Publisher") + '</td>\
+                <td class="value_td">'+app['publisher']+'</td>\
+              </tr>\
+              <tr>\
+                <td class="key_td">' + tr("Downloads") + '</td>\
+                <td class="value_td">'+app['downloads']+'</td>\
+              </tr>\
+              <tr>\
+                <td class="key_td">' + tr("OS") + '</td>\
+                <td class="value_td">'+app['files'][0]['os-id']+' '+app['files'][0]['os-release']+'</td>\
+              </tr>\
+              <tr>\
+                <td class="key_td">' + tr("Arch") + '</td>\
+                <td class="value_td">'+app['files'][0]['os-arch']+'</td>\
+              </tr>\
+              <tr>\
+                <td class="key_td">' + tr("Size") + '</td>\
+                <td class="value_td">'+app['files'][0]['size']+'</td>\
+              </tr>\
+              <tr>\
+                <td class="key_td">' + tr("Hypervisor") + '</td>\
+                <td class="value_td">'+app['files'][0]['hypervisor']+'</td>\
+              </tr>\
+            </tbody>\
+        </table>\
+        <table id="info_marketplace_table2" class="info_table">\
+           <thead>\
+             <tr><th colspan="2">'+tr("Description")+'</th></tr>\
+           </thead>\
+           <tbody>\
+              <tr>\
+                <td class="value_td">'+app['description'].replace(/\n/g, "<br />")+'</td>\
+              </tr>\
+            </tbody>\
+        </table>'
+    };
+
+    Sunstone.updateInfoPanelTab("marketplace_info_panel", "marketplace_info_tab", info_tab);
+    Sunstone.popUpInfoPanel("marketplace_info_panel");
+};
+
 function infoListenerMarket(dataTable){
     $('tbody tr',dataTable).live("click",function(e){
         if ($(e.target).is('input')) {return true;}
@@ -74,133 +243,24 @@ function infoListenerMarket(dataTable){
     });
 }
 
-var market_actions = {
-    "Marketplace.refresh" : {
-        type: "custom",
-        call: function () {
-            dataTable_marketplace.fnReloadAjax();
-        }
-    },
-    "Marketplace.import" : {
-        type: "single",
-        call: function () {
-            var app_id = getSelectedNodes(dataTable_marketplace)[0];
 
-            $.ajax({
-                url: "/marketplace/" + app_id,
-                type: "GET",
-                dataType: "json",
-                success: function(response){
-                    document.getElementById("img_name").value = response['name'];
-                    document.getElementById("img_path").value = 'http://marketplace.c12g.com/appliance/' + response['_id']['$oid'] + '/download';
-                    popUpCreateImageDialog();
-                },
-                error: function(response)
-                {
-                    return onError(null, OpenNebula.Error(response));
-                }
-            });
-        }
-    }
+/*
+ * onlyOneCheckboxListener: Only one box can be checked
+ */
+
+function onlyOneCheckboxListener(dataTable) {
+    $('tbody input.check_item', dataTable).live("change", function(){
+        var checked = $('input.check_item:checked', $('tr', dataTable));
+        var self = this;
+        checked.each(function(){
+          if(this!=self) this.checked = ''
+        })
+    });
 }
 
-var market_buttons = {
-    "Marketplace.refresh" : {
-        type: "image",
-        text: tr("Refresh list"),
-        img: "images/Refresh-icon.png"
-    },
-    "Marketplace.import" : {
-        type: "action",
-        text: tr('Import to local infrastructure')
-    }
-};
-
-Sunstone.addActions(market_actions);
-
-var marketplace_tab_content = '\
-<h2>'+tr("OpenNebula Marketplace")+'</h2>\
-<form id="marketplace_form" action="" action="javascript:alert(\'js error!\');">\
-  <div class="action_blocks">\
-  </div>\
-<table id="datatable_marketplace" class="display">\
-  <thead>\
-    <tr>\
-      <th class="check"></th>\
-      <th>'+tr("ID")+'</th>\
-      <th>'+tr("Name")+'</th>\
-      <th>'+tr("Publisher")+'</th>\
-      <th>'+tr("Hypervisor")+'</th>\
-      <th>'+tr("Arch")+'</th>\
-      <th>'+tr("Format")+'</th>\
-    </tr>\
-  </thead>\
-  <tbody id="tbodymarketplace">\
-  </tbody>\
-</table>\
-</div>';
-
-
-var marketplace_tab = {
-    title: '<i class="icon-shopping-cart"></i>' + tr("Marketplace"),
-    content: marketplace_tab_content,
-    buttons: market_buttons,
-};
-
-Sunstone.addMainTab('marketplace_tab', marketplace_tab);
-
-
-
-var marketplace_info_panel = {
-    "marketplace_info_tab" : {
-        title: tr("Appliance information"),
-        content:""
-    },
-};
-
-Sunstone.addInfoPanel("marketplace_info_panel", marketplace_info_panel);
-
-
-
-function updateMarketInfo(request,app){
-    var info_tab = {
-        title : tr("Appliance information"),
-        content :
-        '<table id="info_marketplace_table" class="info_table">\
-            <thead>\
-              <tr><th colspan="2">'+tr("Appliance information")+'</th></tr>\
-            </thead>\
-            <tbody>\
-              <tr>\
-                <td class="key_td">' + tr("ID") + '</td>\
-                <td class="value_td">'+app['_id']["$oid"]+'</td>\
-              </tr>\
-              <tr>\
-                <td class="key_td">' + tr("Publisher") + '</td>\
-                <td class="value_td">'+app['publisher']+'</td>\
-              </tr>\
-              <tr>\
-                <td class="key_td">' + tr("Downloads") + '</td>\
-                <td class="value_td">'+app['downloads']+'</td>\
-              </tr>\
-            </tbody>\
-        </table>\
-        <table id="info_marketplace_table2" class="info_table">\
-           <thead>\
-             <tr><th colspan="2">'+tr("Description")+'</th></tr>\
-           </thead>\
-           <tbody>\
-              <tr>\
-                <td class="value_td">'+app['description'].replace(/\n/g, "<br />")+'</td>\
-              </tr>\
-            </tbody>\
-        </table>'
-    };
-
-    Sunstone.updateInfoPanelTab("marketplace_info_panel", "marketplace_info_tab", info_tab);
-    Sunstone.popUpInfoPanel("marketplace_info_panel");
-};
-
+/*
+ * Document
+ */
 
 $(document).ready(function(){
     dataTable_marketplace = $("#datatable_marketplace", main_tabs_context).dataTable({
@@ -221,7 +281,8 @@ $(document).ready(function(){
             { "mDataProp": "publisher" },
             { "mDataProp": "files.0.hypervisor"},
             { "mDataProp": "files.0.os-arch"},
-            { "mDataProp": "files.0.format"}
+            { "mDataProp": "files.0.format"},
+            { "mDataProp": "tags", "bVisible": false}
           ],
         "oLanguage": (datatable_lang != "") ?
             {
@@ -232,6 +293,7 @@ $(document).ready(function(){
 
     initCheckAllBoxes(dataTable_marketplace);
     tableCheckboxesListener(dataTable_marketplace);
+    onlyOneCheckboxListener(dataTable_marketplace);
 
     infoListenerMarket(dataTable_marketplace);
 });
