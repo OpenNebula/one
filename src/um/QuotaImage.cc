OpenNebula/one

commit 77452c86e46a7ea6d254f3e5d0402aeff4d8f209

Feature #1288: Do not try to check quotas when image or network ID is not present

src/um/QuotaImage.cc
@@ -51,9 +51,9 @@ bool QuotaImage::check(Template * tmpl,  string& error)
 
         image_id = disk->vector_value("IMAGE_ID");
         
-        if ( !check_quota(image_id, image_request, error) )
+        if ( !image_id.empty() )
         {
-            return false;
+            return check_quota(image_id, image_request, error);
         }
     }
 

src/um/QuotaNetwork.cc
@@ -50,10 +50,10 @@ bool QuotaNetwork::check(Template * tmpl,  string& error)
         }
 
         net_id = nic->vector_value("NETWORK_ID");
-        
-        if ( !check_quota(net_id, net_request, error) )
+
+        if ( !net_id.empty() )
         {
-            return false;
+            return check_quota(net_id, net_request, error);
         }
     }
 

commit 4d8967deafcdd29f23dd50c49b2115efb920dd58

Feature #1288: Fix bug introduced in commit:77452c86, quotas were only checked for the first image or vnet found (cherry picked from commit 564502fe6a3d85877be59c36fa6980f5d3da5e0c)

src/um/QuotaImage.cc
@@ -53,7 +53,10 @@ bool QuotaImage::check(Template * tmpl,  string& error)
         
         if ( !image_id.empty() )
         {
-            return check_quota(image_id, image_request, error);
+            if ( !check_quota(image_id, image_request, error) )
+            {
+                return false;
+            }
         }
     }
 

src/um/QuotaNetwork.cc
@@ -53,7 +53,10 @@ bool QuotaNetwork::check(Template * tmpl,  string& error)
 
         if ( !net_id.empty() )
         {
-            return check_quota(net_id, net_request, error);
+            if ( !check_quota(net_id, net_request, error) )
+            {
+                return false;
+            }
         }
     }
 