ba96b413b9c8d7a6f440ede9a0ce6099fc4b54e5
Carlos Martín <cmartins@fdi.ucm.es>
2012-07-17 17:21:52 +0200

    Bug #1347, #1363: onedb migrator re-calculates the VM quotas

--- src/onedb/3.6.0_to_3.7.80.rb
+++ src/onedb/3.6.0_to_3.7.80.rb
@@ -27,6 +27,9 @@ module Migrator
     end
 
     def up
+        ########################################################################
+        # Bug #1335
+        ########################################################################
         @db.fetch("SELECT * FROM vm_pool WHERE state = 5") do |row|
 
             vm_doc = Document.new(row[:body])
@@ -83,6 +86,145 @@ module Migrator
                 :other_u        => host_row[:other_u])
         end
 
+        ########################################################################
+        # Bugs #1347 & #1363: re-calculate CPU & MEM quotas
+        ########################################################################
+
+        @db.run "ALTER TABLE user_pool RENAME TO old_user_pool;"
+        @db.run "CREATE TABLE user_pool (oid INTEGER PRIMARY KEY, name VARCHAR(128), body TEXT, uid INTEGER, gid INTEGER, owner_u INTEGER, group_u INTEGER, other_u INTEGER, UNIQUE(name));"
+
+        # oneadmin does not have quotas
+        @db.fetch("SELECT * FROM old_user_pool WHERE oid=0") do |row|
+            @db[:user_pool].insert(
+                :oid        => row[:oid],
+                :name       => row[:name],
+                :body       => row[:body],
+                :uid        => row[:oid],
+                :gid        => row[:gid],
+                :owner_u    => row[:owner_u],
+                :group_u    => row[:group_u],
+                :other_u    => row[:other_u])
+        end
+
+        @db.fetch("SELECT * FROM old_user_pool WHERE oid>0") do |row|
+            doc = Document.new(row[:body])
+
+            redo_vm_quotas(doc, "uid=#{row[:oid]}")
+
+            @db[:user_pool].insert(
+                :oid        => row[:oid],
+                :name       => row[:name],
+                :body       => doc.root.to_s,
+                :uid        => row[:oid],
+                :gid        => row[:gid],
+                :owner_u    => row[:owner_u],
+                :group_u    => row[:group_u],
+                :other_u    => row[:other_u])
+        end
+
+        @db.run "DROP TABLE old_user_pool;"
+
+
+        @db.run "ALTER TABLE group_pool RENAME TO old_group_pool;"
+        @db.run "CREATE TABLE group_pool (oid INTEGER PRIMARY KEY, name VARCHAR(128), body TEXT, uid INTEGER, gid INTEGER, owner_u INTEGER, group_u INTEGER, other_u INTEGER, UNIQUE(name));"
+
+
+        # oneadmin group does not have quotas
+        @db.fetch("SELECT * FROM old_group_pool WHERE oid=0") do |row|
+            @db[:group_pool].insert(
+                :oid        => row[:oid],
+                :name       => row[:name],
+                :body       => row[:body],
+                :uid        => row[:oid],
+                :gid        => row[:gid],
+                :owner_u    => row[:owner_u],
+                :group_u    => row[:group_u],
+                :other_u    => row[:other_u])
+        end
+
+        @db.fetch("SELECT * FROM old_group_pool WHERE oid>0") do |row|
+            doc = Document.new(row[:body])
+
+            redo_vm_quotas(doc, "gid=#{row[:oid]}")
+
+            @db[:group_pool].insert(
+                :oid        => row[:oid],
+                :name       => row[:name],
+                :body       => doc.root.to_s,
+                :uid        => row[:oid],
+                :gid        => row[:gid],
+                :owner_u    => row[:owner_u],
+                :group_u    => row[:group_u],
+                :other_u    => row[:other_u])
+        end
+
+        @db.run "DROP TABLE old_group_pool;"
+
         return true
+    end
+
+    def redo_vm_quotas(doc, where_filter)
+        cpu_limit = "0"
+        mem_limit = "0"
+        vms_limit = "0"
+
+        doc.root.each_element("VM_QUOTA/VM/CPU") { |e|
+            cpu_limit = e.text
+        }
+
+        doc.root.each_element("VM_QUOTA/VM/MEMORY") { |e|
+            mem_limit = e.text
+        }
+
+        doc.root.each_element("VM_QUOTA/VM/VMS") { |e|
+            vms_limit = e.text
+        }
+
+        doc.root.delete_element("VM_QUOTA")
+        vm_quota  = doc.root.add_element("VM_QUOTA")
+
+        # VM quotas
+        cpu_used = 0
+        mem_used = 0
+        vms_used = 0
+
+        @db.fetch("SELECT body FROM vm_pool WHERE #{where_filter} AND state!=6") do |vm_row|
+            vmdoc = Document.new(vm_row[:body])
+
+            # VM quotas
+            vmdoc.root.each_element("TEMPLATE/CPU") { |e|
+                cpu_used += e.text.to_f
+            }
+
+            vmdoc.root.each_element("TEMPLATE/MEMORY") { |e|
+                mem_used += e.text.to_i
+            }
+
+            vms_used += 1
+        end
+
+##
+puts "where : #{where_filter}"
+puts "CPU           #{cpu_limit}"
+puts "CPU_USED      #{cpu_used.to_s}"
+puts "MEMORY        #{mem_limit}"
+puts "MEMORY_USED   #{mem_used.to_s}"
+puts "VMS           #{vms_limit}"
+puts "VMS_USED      #{vms_used.to_s}"
+puts
+puts
+##
+
+        # VM quotas
+        vm_elem = vm_quota.add_element("VM")
+
+        vm_elem.add_element("CPU").text = cpu_limit
+        vm_elem.add_element("CPU_USED").text = cpu_used.to_s
+
+        vm_elem.add_element("MEMORY").text = mem_limit
+        vm_elem.add_element("MEMORY_USED").text = mem_used.to_s
+
+        vm_elem.add_element("VMS").text = vms_limit
+        vm_elem.add_element("VMS_USED").text = vms_used.to_s
     end
 end
