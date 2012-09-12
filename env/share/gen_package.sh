d8c27f1eec7ecf65c47a09c40af1385147003643
Javi Fontan <jfontan@opennebula.org>
2012-09-12 12:40:30 +0200

    appenv: changed context package info

--- env/share/gen_package.sh
+++ env/share/gen_package.sh
@@ -9,6 +9,20 @@ EXTRA="$PWD/guest/*"
 PACKAGER_DIR="$ONE_SOURCE/share/scripts/context-packages"
 
 export PACKAGE_NAME=one-chef
+export VERSION=3.6.0
+export LICENSE="C12G Commercial Open-source License"
+export URL="http://c12g.com"
+export DESCRIPTION="
+This package prepares a VM image for OpenNebula:
+  * Disables udev net and cd persistent rules
+  * Deletes udev net and cd persistent rules
+  * Unconfigures the network
+  * Adds OpenNebula contextualization scripts to startup
+  * Prepares the image to be used with AppEnv
+
+To get support use the OpenNebula.pro portal:
+  http://opennebula.pro
+"
 
 cd $PACKAGER_DIR
 ./generate.sh $EXTRA
