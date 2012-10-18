e5c0d20b96eae893089f884f59ce3ca4696c885d
Jaime Melis <jmelis@opennebula.org>
2012-10-18 13:02:43 +0200

    Bump version 3.8.0

--- src/oca/ruby/OpenNebula.rb
+++ src/oca/ruby/OpenNebula.rb
@@ -52,7 +52,7 @@ require 'OpenNebula/DocumentPool'
 module OpenNebula
 
     # OpenNebula version
-    VERSION = '3.7.80'
+    VERSION = '3.8.0'
 
     # The Error Class represents a generic error in the OpenNebula
     # library. It contains a readable representation of the error.
