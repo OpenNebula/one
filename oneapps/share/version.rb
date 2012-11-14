50e170ee44da73ab27af417b9b93348438278644
Daniel Molina <dmolina@opennebula.org>
2012-11-14 12:09:39 +0100

    Fix column size to 80 in VERSION_TEXT

--- oneapps/share/version.rb
+++ oneapps/share/version.rb
@@ -7,6 +7,8 @@ module OneApps
 OpenNebulaApps #{OneApps::VERSION}
 Copyright 2010-2012, C12G Labs S.L
 
-Licensed under the C12G Commercial Open-source License (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License as part of the software distribution.
+Licensed under the C12G Commercial Open-source License (the "License"); you may 
+not use this file except in compliance with the License. You may obtain a copy 
+of the License as part of the software distribution.
 EOT
 end
