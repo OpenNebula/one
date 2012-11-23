a3b30e7bf37555c70a482a1222b21248548bfd74
Carlos Martín <cmartins@fdi.ucm.es>
2012-11-23 18:28:09 +0100

    Feature #1611: Show the default quotas limit in oneuser/group list

--- src/cli/one_helper/onegroup_helper.rb
+++ src/cli/one_helper/onegroup_helper.rb
@@ -48,6 +48,16 @@ class OneGroupHelper < OpenNebulaHelper::OneHelper
     def format_pool(options)
         config_file = self.class.table_conf
 
+        gpool = GroupPool.new(@client)
+        default_xml_str = gpool.get_quota()
+
+        # TODO: check error
+#        if OpenNebula::is_error?(default_xml_str)
+#        end
+
+        default_user_quotas = XMLElement.new
+        default_user_quotas.initialize_xml(default_xml_str, 'DEFAULT_GROUP_QUOTAS')
+
         table = CLIHelper::ShowTable.new(config_file, self) do
             column :ID, "ONE identifier for the Group", :size=>4 do |d|
                 d["ID"]
@@ -67,7 +77,14 @@ class OneGroupHelper < OpenNebulaHelper::OneHelper
 
             column :VMS , "Number of VMS", :size=>9 do |d|             
                 if d.has_key?('VM_QUOTA') and d['VM_QUOTA'].has_key?('VM')
-                    "%3d / %3d" % [d['VM_QUOTA']['VM']["VMS_USED"], d['VM_QUOTA']['VM']["VMS"]]
+                    limit = d['VM_QUOTA']['VM']["VMS"]
+
+                    if limit == "-1"
+                        limit = default_user_quotas['VM_QUOTA/VM/VMS']
+                        limit = "0" if limit.nil? || limit == ""
+                    end
+
+                    "%3d / %3d" % [d['VM_QUOTA']['VM']["VMS_USED"], limit]
                 else
                     "-"
                 end
@@ -75,8 +92,16 @@ class OneGroupHelper < OpenNebulaHelper::OneHelper
 
             column :MEMORY, "Total memory allocated to user VMs", :size=>17 do |d|
                 if d.has_key?('VM_QUOTA') and d['VM_QUOTA'].has_key?('VM')
+                    limit = d['VM_QUOTA']['VM']["MEMORY"]
+
+                    if limit == "-1"
+                        limit = default_user_quotas['VM_QUOTA/VM/MEMORY']
+                        limit = "0" if limit.nil? || limit == ""
+                    end
+
+                    d['VM_QUOTA']['VM']['MEMORY_USED']
                     "%7s / %7s" % [OpenNebulaHelper.unit_to_str(d['VM_QUOTA']['VM']["MEMORY_USED"].to_i,{},"M"),
-                    OpenNebulaHelper.unit_to_str(d['VM_QUOTA']['VM']["MEMORY"].to_i,{},"M")]
+                    OpenNebulaHelper.unit_to_str(limit.to_i,{},"M")]
                 else
                     "-"
                 end
@@ -84,7 +109,14 @@ class OneGroupHelper < OpenNebulaHelper::OneHelper
 
             column :CPU, "Total CPU allocated to user VMs", :size=>11 do |d|
                 if d.has_key?('VM_QUOTA') and d['VM_QUOTA'].has_key?('VM')
-                    "%4.0f / %4.0f" % [d['VM_QUOTA']['VM']["CPU_USED"], d['VM_QUOTA']['VM']["CPU"]]
+                    limit = d['VM_QUOTA']['VM']["CPU"]
+
+                    if limit == "-1"
+                        limit = default_user_quotas['VM_QUOTA/VM/CPU']
+                        limit = "0" if limit.nil? || limit == ""
+                    end
+
+                    "%4.0f / %4.0f" % [d['VM_QUOTA']['VM']["CPU_USED"], limit]
                 else
                     "-"
                 end
