cb30f922ff59eb4408dc5db503c6d8ed88635e41
Carlos Martín <cmartins@fdi.ucm.es>
2012-12-07 15:08:43 +0100

    Feature #1617: Add changes to DB migrator

--- src/onedb/3.8.1_to_3.9.80.rb
+++ src/onedb/3.8.1_to_3.9.80.rb
@@ -112,6 +112,72 @@ module Migrator
         @db.run("DROP TABLE image_pool")
         @db.run("ALTER TABLE image_pool_new RENAME TO image_pool")
 
+        ########################################################################
+        # Feature #1617
+        # New datastore, 2 "files"
+        # DATASTORE/SYSTEM is now DATASTORE/TYPE
+        ########################################################################
+
+        @db.run "ALTER TABLE datastore_pool RENAME TO old_datastore_pool;"
+        @db.run "CREATE TABLE datastore_pool (oid INTEGER PRIMARY KEY, name VARCHAR(128), body TEXT, uid INTEGER, gid INTEGER, owner_u INTEGER, group_u INTEGER, other_u INTEGER, UNIQUE(name));"
+
+        @db.fetch("SELECT * FROM old_datastore_pool") do |row|
+            doc = Document.new(row[:body])
+
+            type = "0"  # IMAGE_DS
+
+            system_elem = doc.root.delete_element("SYSTEM")
+
+            if ( !system_elem.nil? && system_elem.text == "1" )
+                type = "1"  # SYSTEM_DS
+            end
+
+            doc.root.add_element("TYPE").text = type
+
+            doc.root.each_element("TEMPLATE") do |e|
+                e.delete_element("SYSTEM")
+                e.add_element("TYPE").text = type == "0" ? "IMAGE_DS" : "SYSTEM_DS"
+            end
+
+            @db[:datastore_pool].insert(
+                :oid        => row[:oid],
+                :name       => row[:name],
+                :body       => doc.root.to_s,
+                :uid        => row[:uid],
+                :gid        => row[:gid],
+                :owner_u    => row[:owner_u],
+                :group_u    => row[:group_u],
+                :other_u    => row[:other_u])
+        end
+
+        @db.run "DROP TABLE old_datastore_pool;"
+
+
+        user_0_name = "oneadmin"
+
+        @db.fetch("SELECT name FROM user_pool WHERE oid=0") do |row|
+            user_0_name = row[:name]
+        end
+
+        group_0_name = "oneadmin"
+
+        @db.fetch("SELECT name FROM group_pool WHERE oid=0") do |row|
+            group_0_name = row[:name]
+        end
+
+        base_path = "/var/lib/one/datastores/2"
+
+        @db.fetch("SELECT body FROM datastore_pool WHERE oid=0") do |row|
+            doc = Document.new(row[:body])
+
+            doc.root.each_element("BASE_PATH") do |e|
+                base_path = e.text
+                base_path[-1] = "2"
+            end
+        end
+
+        @db.run "INSERT INTO datastore_pool VALUES(2,'files','<DATASTORE><ID>2</ID><UID>0</UID><GID>0</GID><UNAME>#{user_0_name}</UNAME><GNAME>#{group_0_name}</GNAME><NAME>files</NAME><PERMISSIONS><OWNER_U>1</OWNER_U><OWNER_M>1</OWNER_M><OWNER_A>0</OWNER_A><GROUP_U>1</GROUP_U><GROUP_M>0</GROUP_M><GROUP_A>0</GROUP_A><OTHER_U>1</OTHER_U><OTHER_M>0</OTHER_M><OTHER_A>0</OTHER_A></PERMISSIONS><DS_MAD>fs</DS_MAD><TM_MAD>ssh</TM_MAD><BASE_PATH>#{base_path}</BASE_PATH><TYPE>2</TYPE><DISK_TYPE>0</DISK_TYPE><CLUSTER_ID>-1</CLUSTER_ID><CLUSTER></CLUSTER><IMAGES></IMAGES><TEMPLATE><DS_MAD><![CDATA[fs]]></DS_MAD><TM_MAD><![CDATA[ssh]]></TM_MAD><TYPE><![CDATA[FILE_DS]]></TYPE></TEMPLATE></DATASTORE>',0,0,1,1,1);"
+
 
         ########################################################################
         # Feature #1611: Default quotas
