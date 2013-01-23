851689c32ecfa17cdd3c234495553901f2537e04
Javi Fontan <jfontan@opennebula.org>
2013-01-23 12:44:19 +0100

    Change license

--- oneapps/share/version.rb
+++ oneapps/share/version.rb
@@ -5,10 +5,10 @@ module OneApps
 
     VERSION_TEXT = <<EOT
 OpenNebulaApps #{OneApps::VERSION}
-Copyright 2010-2012, C12G Labs S.L
+Copyright 2010-2013, C12G Labs S.L
 
-Licensed under the C12G Commercial Open-source License (the "License"); you may 
-not use this file except in compliance with the License. You may obtain a copy 
-of the License as part of the software distribution.
+Licensed under the Apache License, Version 2.0 (the "License"); you may
+not use this file except in compliance with the License. You may obtain
+a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 EOT
 end
