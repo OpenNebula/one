7357bffe3451489599f4057cdad4bc9b168036f1
Jaime Melis <jmelis@opennebula.org>
2013-01-24 16:18:30 +0100

    Update copyright notice

--- src/template/template_parser.c
+++ src/template/template_parser.c
@@ -522,7 +522,7 @@ int template__flex_debug = 0;
 char *template_text;
 #line 1 "template_parser.l"
 /* -------------------------------------------------------------------------- */
-/* Copyright 2002-2013, OpenNebula Project Leads (OpenNebula.org)             */
+/* Copyright 2002-2013, OpenNebula Project (OpenNebula.org), C12G Labs        */
 /*                                                                            */
 /* Licensed under the Apache License, Version 2.0 (the "License"); you may    */
 /* not use this file except in compliance with the License. You may obtain    */
