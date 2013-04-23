61fc5c575280fc11d3133873961d74e48fd08f47
Ruben S. Montero <rsmontero@opennebula.org>
2013-04-24 01:14:22 +0200

    bug #1949: Add OCA state

--- src/oca/ruby/opennebula/host.rb
+++ src/oca/ruby/opennebula/host.rb
@@ -42,6 +42,8 @@ module OpenNebula
             "ERROR"                => "err",
             "DISABLED"             => "off",
             "MONITORING_ERROR"     => "retry",
+            "MONITORING_INIT"      => "init",
+            "MONITORING_DISABLED"  => "off"
         }
 
         # Creates a Host description with just its identifier
