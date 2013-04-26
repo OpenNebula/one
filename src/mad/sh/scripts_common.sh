be7b5e47280ef15d45313d0398f67d95f4b66b5d
Ruben S. Montero <rsmontero@opennebula.org>
2013-04-26 21:45:56 +0200

    Revert "Bug #1948: adds  umask awareness to qcow2 and shared MVDS scripts"
    This reverts commit 190b4668acc7a3fce190adba44571249b7513e8f.

--- src/mad/sh/scripts_common.sh
+++ src/mad/sh/scripts_common.sh
@@ -35,7 +35,6 @@ MD5SUM=md5sum
 MKFS=mkfs
 MKISOFS=genisoimage
 MKSWAP=mkswap
-MKTEMP=mktemp
 QEMU_IMG=qemu-img
 RBD=rbd
 READLINK=readlink
@@ -417,7 +416,7 @@ function is_iscsi {
     fi
 }
 
-# Checks wether $IMAGE_TYPE is CDROM
+# Checks wether $IMAGE_TYPE is CDROM 
 function is_cdrom {
     [ "$IMAGE_TYPE" = "1" ]
 }
