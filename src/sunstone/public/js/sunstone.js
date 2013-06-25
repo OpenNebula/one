8086e2e902c9adec1a5579f12c8bed504b0d19d7
Daniel Molina <dmolina@opennebula.org>
2013-06-25 13:01:01 +0200

    Fix panel resize button

--- src/sunstone/public/js/sunstone.js
+++ src/sunstone/public/js/sunstone.js
@@ -25,6 +25,7 @@ var dialogs_context;
 var plots_context;
 var info_panels_context;
 
+panel_extended = false;
 
 //Sunstone configuration is formed by predifined "actions", main tabs
 //and "info_panels". Each tab has "content" and "buttons". Each
@@ -132,8 +133,8 @@ var Sunstone = {
                         <button type="button" style="float:left; margin-right:5px" class="button secondary tiny radius" id="'+panel_name+'_refresh"><span class="icon-refresh"></span></button>\
                     </div>\
                     <div id="aa" class="right">\
-                        <a href="#" id="'+panel_name+'_resize_50" hidden><span class="icon-resize-small"></span>&emsp;</a>\
-                        <a href="#" id="'+panel_name+'_resize_75"><span class="icon-resize-full"></span>&emsp;</a>\
+                        <a href="#" id="'+panel_name+'_resize_50" '+ (!panel_extended ? "hidden" : "") +'><span class="icon-resize-small"></span>&emsp;</a>\
+                        <a href="#" id="'+panel_name+'_resize_75" '+ (panel_extended ? "hidden" : "") +'><span class="icon-resize-full"></span>&emsp;</a>\
                     </div>\
                 </dl>\
             </div>\
@@ -141,14 +142,14 @@ var Sunstone = {
         </div>');
 
         $('#'+panel_name+'_resize_75').live('click', function(){
-            $(".ui-layout-resizer-south").css('bottom', Math.floor($(window).height()*0.75));
-            $(".ui-layout-pane-south").css('height', Math.floor($(window).height()*0.75));
+            panel_extended = true;
+            changeInnerLayout(0.75);
             $('#'+panel_name+'_resize_75').hide();
             $('#'+panel_name+'_resize_50').show();
         })
         $('#'+panel_name+'_resize_50').live('click', function(){
-            $(".ui-layout-resizer-south").css('bottom', Math.floor($(window).height()*0.50));
-            $(".ui-layout-pane-south").css('height', Math.floor($(window).height()*0.50));
+            panel_extended = false;
+            changeInnerLayout(0.5);
             $('#'+panel_name+'_resize_50').hide();
             $('#'+panel_name+'_resize_75').show();
         })
