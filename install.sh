a6770e02b18c984f92871c35720090f244a06ab0
Tino Vazquez <tinova@opennebula.org>
2013-07-23 17:55:35 +0200

    Fix for dashboard in vdcadmin view (cherry picked from commit de61f5fcf7efddab5ebe134f08bb449568936e0b)

--- install.sh
+++ install.sh
@@ -1480,7 +1480,8 @@ SUNSTONE_ETC_FILES="src/sunstone/etc/sunstone-server.conf \
 
 SUNSTONE_ETC_VIEW_FILES="src/sunstone/etc/sunstone-views/admin.yaml \
                     src/sunstone/etc/sunstone-views/user.yaml \
-                    src/sunstone/etc/sunstone-views/cloud.yaml"
+                    src/sunstone/etc/sunstone-views/cloud.yaml \
+                     src/sunstone/etc/sunstone-views/vdcadmin.yaml"
 
 SUNSTONE_MODELS_FILES="src/sunstone/models/OpenNebulaJSON.rb \
                        src/sunstone/models/SunstoneServer.rb \
