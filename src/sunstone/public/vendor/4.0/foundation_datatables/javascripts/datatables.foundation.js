a424e928861485b0b92a0a689c09c62612c1bc35
Daniel Molina <dmolina@opennebula.org>
2013-07-24 18:37:11 +0200

    Remove oLanguage from default data table conf, since some tables are not working correctly(cherry picked from commit 5025c8f7dace0f1f91d7f34554929aabbd07a4d0)

--- src/sunstone/public/vendor/4.0/foundation_datatables/javascripts/datatables.foundation.js
+++ src/sunstone/public/vendor/4.0/foundation_datatables/javascripts/datatables.foundation.js
@@ -2,11 +2,9 @@
 $.extend( true, $.fn.dataTable.defaults, {
     "sDom": "<'H'>t<'row'<'eight columns'li><'four columns'p>>",
     "sPaginationType": "foundation",
-        "oLanguage": (datatable_lang != "") ?
-            {
-                sUrl: "locale/"+lang+"/"+datatable_lang,
-
-            } : "",
+    "oLanguage": {
+        "sLengthMenu": '<select><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select>&emsp;&emsp;'
+    }
 
 } );
 
