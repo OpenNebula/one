c2e4558040ab6461057b6841ebbb04359f272e4f
Daniel Molina <dmolina@opennebula.org>
2013-08-15 19:06:37 +0200

    feature #2259: Add DeleteTags

--- src/cloud/ec2/lib/econe_application.rb
+++ src/cloud/ec2/lib/econe_application.rb
@@ -142,6 +142,8 @@ class EC2Application
             result,rc = econe_server.describe_snapshots(params)
         when 'CreateTags'
             result,rc = econe_server.create_tags(params)
+        when 'DeleteTags'
+            result,rc = econe_server.delete_tags(params)
         when 'CreateImage'
             result,rc = econe_server.create_image(params)
         when 'CreateVolume'
