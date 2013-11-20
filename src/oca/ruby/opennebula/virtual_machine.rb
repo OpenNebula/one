b50189024c80925c0966626039def9bca4c35253
Daniel Molina <dmolina@opennebula.org>
2013-11-20 12:47:41 +0100

    bug #2478: ds_id must be an integer in the deploy action
    (cherry picked from commit b8d516d2b2f406d2f2e426aecff4e26c1f86027e)

--- src/oca/ruby/opennebula/virtual_machine.rb
+++ src/oca/ruby/opennebula/virtual_machine.rb
@@ -222,7 +222,7 @@ module OpenNebula
         # @return [nil, OpenNebula::Error] nil in case of success, Error
         #   otherwise
         def deploy(host_id, enforce=false, ds_id=-1)
-            return call(VM_METHODS[:deploy], @pe_id, host_id.to_i, enforce, ds_id)
+            return call(VM_METHODS[:deploy], @pe_id, host_id.to_i, enforce, ds_id.to_i)
         end
 
         # Shutdowns an already deployed VM
