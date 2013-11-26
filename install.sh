1078fea78826814ad45b2961c11e21f08a3b3160
Javi Fontan <jfontan@opennebula.org>
2013-11-26 10:49:20 +0100

    feature #2468: add probe to get remotes version

--- install.sh
+++ install.sh
@@ -886,7 +886,8 @@ IM_PROBES_KVM_PROBES_FILES="src/im_mad/remotes/kvm-probes.d/kvm.rb \
                      src/im_mad/remotes/kvm-probes.d/cpu.sh \
                      src/im_mad/remotes/kvm-probes.d/poll.sh \
                      src/im_mad/remotes/kvm-probes.d/name.sh \
-                     src/im_mad/remotes/common.d/monitor_ds.sh"
+                     src/im_mad/remotes/common.d/monitor_ds.sh \
+                     src/im_mad/remotes/common.d/version.sh"
 
 IM_PROBES_XEN3_FILES="src/im_mad/remotes/xen.d/collectd-client_control.sh \
                       src/im_mad/remotes/xen.d/collectd-client.rb"
@@ -896,7 +897,8 @@ IM_PROBES_XEN3_PROBES_FILES="src/im_mad/remotes/xen-probes.d/xen.rb \
                       src/im_mad/remotes/xen-probes.d/cpu.sh \
                       src/im_mad/remotes/xen-probes.d/poll3.sh \
                       src/im_mad/remotes/xen-probes.d/name.sh
-                      src/im_mad/remotes/common.d/monitor_ds.sh"
+                      src/im_mad/remotes/common.d/monitor_ds.sh \
+                      src/im_mad/remotes/common.d/version.sh"
 
 IM_PROBES_XEN4_FILES="src/im_mad/remotes/xen.d/collectd-client_control.sh \
                       src/im_mad/remotes/xen.d/collectd-client.rb"
@@ -906,7 +908,8 @@ IM_PROBES_XEN4_PROBES_FILES="src/im_mad/remotes/xen-probes.d/xen.rb \
                       src/im_mad/remotes/xen-probes.d/cpu.sh \
                       src/im_mad/remotes/xen-probes.d/poll4.sh \
                       src/im_mad/remotes/xen-probes.d/name.sh \
-                      src/im_mad/remotes/common.d/monitor_ds.sh"
+                      src/im_mad/remotes/common.d/monitor_ds.sh \
+                      src/im_mad/remotes/common.d/version.sh"
 
 IM_PROBES_VMWARE_FILES="src/im_mad/remotes/vmware.d/vmware.rb"
 
