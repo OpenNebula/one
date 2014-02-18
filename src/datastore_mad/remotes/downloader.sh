e18be2d51775fe3c3de143707ad3e3eeeedbccc3
Jaime Melis <jmelis@opennebula.org>
2014-02-18 12:43:38 +0100

    Bug #2729: Race condition in hash calculation in downloader.sh

--- src/datastore_mad/remotes/downloader.sh
+++ src/datastore_mad/remotes/downloader.sh
@@ -188,8 +188,10 @@ esac
 file_type=$(get_type "$command")
 decompressor=$(get_decompressor "$file_type")
 
+# Note: the 'cat' at the end of this pipeline forces the pipe to wait until
+# all the 'tee' processes are finished
 $command | tee >( decompress "$decompressor" "$TO" ) \
-    >( hasher $HASH_TYPE ) >/dev/null
+    >( hasher $HASH_TYPE ) | cat >/dev/null
 
 if [ "$?" != "0" ]; then
     echo "Error copying" >&2
