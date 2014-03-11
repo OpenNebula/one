2595e11e07b13ec54f6d26d39ea0c9da18b082a8
Daniel Molina <dmolina@opennebula.org>
2014-03-11 17:58:58 +0100

    feature #2746: Fix sign out link

--- src/sunstone/public/js/sunstone.js
+++ src/sunstone/public/js/sunstone.js
@@ -14,6 +14,18 @@
 /* limitations under the License.                                             */
 /* -------------------------------------------------------------------------- */
 
+/*!
+ * jQuery Cookie Plugin v1.4.0
+ * https://github.com/carhartl/jquery-cookie
+ *
+ * Copyright 2013 Klaus Hartl
+ * Released under the MIT license
+ */
+!function(a){"function"==typeof define&&define.amd?define(["jquery"],a):a(jQuery)}(function(a){function b(a){return h.raw?a:encodeURIComponent(a)}function c(a){return h.raw?a:decodeURIComponent(a)}function d(a){return b(h.json?JSON.stringify(a):String(a))}function e(a){0===a.indexOf('"')&&(a=a.slice(1,-1).replace(/\\"/g,'"').replace(/\\\\/g,"\\"));try{a=decodeURIComponent(a.replace(g," "))}catch(b){return}try{return h.json?JSON.parse(a):a}catch(b){}}function f(b,c){var d=h.raw?b:e(b);return a.isFunction(c)?c(d):d}var g=/\+/g,h=a.cookie=function(e,g,i){if(void 0!==g&&!a.isFunction(g)){if(i=a.extend({},h.defaults,i),"number"==typeof i.expires){var j=i.expires,k=i.expires=new Date;k.setDate(k.getDate()+j)}return document.cookie=[b(e),"=",d(g),i.expires?"; expires="+i.expires.toUTCString():"",i.path?"; path="+i.path:"",i.domain?"; domain="+i.domain:"",i.secure?"; secure":""].join("")}for(var l=e?void 0:{},m=document.cookie?document.cookie.split("; "):[],n=0,o=m.length;o>n;n++){var p=m[n].split("="),q=c(p.shift()),r=p.join("=");if(e&&e===q){l=f(r,g);break}e||void 0===(r=f(r))||(l[q]=r)}return l};h.defaults={},a.removeCookie=function(b,c){return void 0!==a.cookie(b)?(a.cookie(b,"",a.extend({},c,{expires:-1})),!0):!1}});
+/*!
+ * end jQuery Cookie Plugin v1.4.0
+ */
+
 var cookie = {};
 var username = '';
 var uid = '';
@@ -391,16 +403,13 @@ function setLogin(){
     // TODOO only call dropdown initialization
     $(document).foundation();
 
-    $("span.user-login a.logout").click(function(){
-        redirect = function(){window.location.href = "login";};
-        switch(whichUI()){
-        case "sunstone":
-            OpenNebula.Auth.logout({success:redirect});
-            break;
-        case "selfservice":
-            OCCI.Auth.logout({success:function(){window.location.href = "ui";}});
-            break;
-        }
+    $("a.logout", $(".user-zone-info ")).click(function(){
+        OpenNebula.Auth.logout({
+          success: function(){
+            window.location.href = "login";
+          }
+        });
+
         return false;
     });
 }
