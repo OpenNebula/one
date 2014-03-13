423b79d5082d6bc1f216dc4923125cc34649d5a4
Daniel Molina <dmolina@opennebula.org>
2014-03-13 15:40:50 +0100

    feature #2746: Fix image logo

--- src/sunstone/public/vendor/4.0/foundation_datatables/javascripts/datatables.foundation.js
+++ src/sunstone/public/vendor/4.0/foundation_datatables/javascripts/datatables.foundation.js
@@ -2,7 +2,7 @@
 $.extend( true, $.fn.dataTable.defaults, {
     "sDom":
         "t"+
-        "<'row'<'small-6 columns'i><'small-6 columns'lp>>",
+        "<'row collapse'<'small-6 columns'i><'small-6 columns'lp>>",
     "oLanguage": {
         "sLengthMenu": "_MENU_"
     }
