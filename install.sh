fe7265df8988ebcd98b63bbcc1c42aad2926665b
Ruben S. Montero <rsmontero@opennebula.org>
2014-05-12 18:58:26 +0200

    feature #1607: Remove unused js files in install

--- install.sh
+++ install.sh
@@ -1498,8 +1498,6 @@ src/sunstone/public/vendor/flot/jquery.flot.pie.min.js \
 src/sunstone/public/vendor/flot/jquery.flot.resize.min.js \
 src/sunstone/public/vendor/flot/jquery.flot.stack.min.js \
 src/sunstone/public/vendor/flot/jquery.flot.tooltip.min.js \
-src/sunstone/public/vendor/flot/jquery.flot.JUMlib.min.js \
-src/sunstone/public/vendor/flot/jquery.flot.gantt.min.js \
 src/sunstone/public/vendor/flot/LICENSE.txt \
 src/sunstone/public/vendor/flot/NOTICE"
 
