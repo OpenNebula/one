f686446eb9855baad404681572e2ae85f50adb13
Carlos Martín <cmartin@opennebula.org>
2014-07-09 12:39:26 +0200

    Feature #2858: Rename NIC/IP6_SITE to _ULA in onedb upgrade

--- src/onedb/local/4.5.80_to_4.7.80.rb
+++ src/onedb/local/4.5.80_to_4.7.80.rb
@@ -257,6 +257,39 @@ module Migrator
 
         log_time()
 
+        @db.run "ALTER TABLE vm_pool RENAME TO old_vm_pool;"
+        @db.run "CREATE TABLE vm_pool (oid INTEGER PRIMARY KEY, name VARCHAR(128), body MEDIUMTEXT, uid INTEGER, gid INTEGER, last_poll INTEGER, state INTEGER, lcm_state INTEGER, owner_u INTEGER, group_u INTEGER, other_u INTEGER);"
+
+        @db.run "INSERT INTO vm_pool SELECT * FROM old_vm_pool WHERE state = 6;"
+
+        log_time()
+
+        @db.fetch("SELECT * FROM old_vm_pool WHERE state<>6") do |row|
+            doc = Nokogiri::XML(row[:body]){|c| c.default_xml.noblanks}
+
+            doc.root.xpath("TEMPLATE/NIC/IP6_SITE").each {|e|
+                e.name = "IP6_ULA"
+            }
+
+            @db[:vm_pool].insert(
+                :oid        => row[:oid],
+                :name       => row[:name],
+                :body       => doc.root.to_s,
+                :uid        => row[:uid],
+                :gid        => row[:gid],
+                :last_poll  => row[:last_poll],
+                :state      => row[:state],
+                :lcm_state  => row[:lcm_state],
+                :owner_u    => row[:owner_u],
+                :group_u    => row[:group_u],
+                :other_u    => row[:other_u])
+
+        end
+
+        @db.run "DROP TABLE old_vm_pool;"
+
+        log_time()
+
         return true
     end
 
