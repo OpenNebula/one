7484ed900855e9af5736b00f197d8f24bf96c347
Carlos Martín <cmartin@opennebula.org>
2014-09-04 18:11:38 +0200

    Feature #3167, #3169: Fix replace_template for Zone

--- include/Zone.h
+++ include/Zone.h
@@ -47,9 +47,13 @@ public:
     /**
      *  Replace template for this object. Object should be updated
      *  after calling this method
-     *    @param tmpl string representation of the template
+     *    @param tmpl_str new contents
+     *    @param keep_restricted If true, the restricted attributes of the
+     *    current template will override the new template
+     *    @param error string describing the error if any
+     *    @return 0 on success
      */
-    int replace_template(const string& tmpl_str, string& error);
+    int replace_template(const string& tmpl_str, bool keep_restricted, string& error);
 
 private:
 
