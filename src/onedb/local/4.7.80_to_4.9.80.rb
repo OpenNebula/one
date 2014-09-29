f61d896a9aa310c3d72a0ad998f4f01c2bcc18f9
Carlos Martín <cmartin@opennebula.org>
2014-09-29 12:25:08 +0200

    Feature #3183: New migrator to add vnet column

---
+++ src/onedb/local/4.7.80_to_4.9.80.rb
@@ -0,0 +1,68 @@
+# -------------------------------------------------------------------------- #
+# Copyright 2002-2014, OpenNebula Project (OpenNebula.org), C12G Labs        #
+#                                                                            #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
+# not use this file except in compliance with the License. You may obtain    #
+# a copy of the License at                                                   #
+#                                                                            #
+# http://www.apache.org/licenses/LICENSE-2.0                                 #
+#                                                                            #
+# Unless required by applicable law or agreed to in writing, software        #
+# distributed under the License is distributed on an "AS IS" BASIS,          #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+# See the License for the specific language governing permissions and        #
+# limitations under the License.                                             #
+#--------------------------------------------------------------------------- #
+
+require 'nokogiri'
+
+module Migrator
+    def db_version
+        "4.9.80"
+    end
+
+    def one_version
+        "OpenNebula 4.9.80"
+    end
+
+    def up
+
+        init_log_time()
+
+        ########################################################################
+        # Networks
+        ########################################################################
+
+        @db.run "ALTER TABLE network_pool RENAME TO old_network_pool;"
+        @db.run "CREATE TABLE network_pool (oid INTEGER PRIMARY KEY, name VARCHAR(128), body MEDIUMTEXT, uid INTEGER, gid INTEGER, owner_u INTEGER, group_u INTEGER, other_u INTEGER, cid INTEGER, pid INTEGER, UNIQUE(name,uid));"
+
+        @db.fetch("SELECT * FROM old_network_pool") do |row|
+            doc = Nokogiri::XML(row[:body]){|c| c.default_xml.noblanks}
+
+            parent_st = doc.root.at_xpath("PARENT_NETWORK_ID").text
+            parent_i = -1
+
+            if parent_st != ""
+                parent_i = parent_st.to_i
+            end
+
+            @db[:network_pool].insert(
+                :oid        => row[:oid],
+                :name       => row[:name],
+                :body       => row[:body],
+                :uid        => row[:uid],
+                :gid        => row[:gid],
+                :owner_u    => row[:owner_u],
+                :group_u    => row[:group_u],
+                :other_u    => row[:other_u],
+                :cid        => row[:cid],
+                :pid        => parent_i)
+        end
+
+        @db.run "DROP TABLE old_network_pool;"
+
+        log_time()
+
+        return true
+    end
+end
