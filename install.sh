cb1f6cbba97b20c1909270dba41876d068969e78
Tino Vazquez <tinova@opennebula.org>
2014-09-30 19:33:07 +0200

    feature #2911: Add vCenter view for Sunstone admin users

--- install.sh
+++ install.sh
@@ -1489,7 +1489,8 @@ SUNSTONE_ETC_FILES="src/sunstone/etc/sunstone-server.conf \
 SUNSTONE_ETC_VIEW_FILES="src/sunstone/etc/sunstone-views/admin.yaml \
                     src/sunstone/etc/sunstone-views/user.yaml \
                     src/sunstone/etc/sunstone-views/cloud.yaml \
-                    src/sunstone/etc/sunstone-views/vdcadmin.yaml"
+                    src/sunstone/etc/sunstone-views/vdcadmin.yaml \
+		    src/sunstone/etc/sunstone-views/vcenter.yaml"
 
 SUNSTONE_MODELS_FILES="src/sunstone/models/OpenNebulaJSON.rb \
                        src/sunstone/models/SunstoneServer.rb \
