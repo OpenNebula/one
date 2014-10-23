d4b528f8ad284ed817b167305a8b0317d11004e9
Daniel Molina <dmolina@opennebula.org>
2014-10-23 17:59:10 +0200

    feature #3197: Add browser check for uploader functionality

--- src/sunstone/public/js/sunstone.js
+++ src/sunstone/public/js/sunstone.js
@@ -6614,3 +6614,18 @@ function generateAdvancedSection(opts){
         '</div>'+
     '</div>';
 }
+
+
+function getInternetExplorerVersion(){
+// Returns the version of Internet Explorer or a -1
+// (indicating the use of another browser).
+    var rv = -1; // Return value assumes failure.
+    if (navigator.appName == 'Microsoft Internet Explorer')
+    {
+        var ua = navigator.userAgent;
+        var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+        if (re.exec(ua) != null)
+            rv = parseFloat( RegExp.$1 );
+    }
+    return rv;
+}
