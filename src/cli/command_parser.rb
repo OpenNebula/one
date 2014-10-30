5550ccf3e4d5ba9e59ad91a409b55eb0096fef3e
Javi Fontan <jfontan@opennebula.org>
2014-10-30 16:00:05 +0100

    Revert "bug #3037: parse US time in ruby > 1.8.7"
    This reverts commit f944559c51389a107d4be6e8f6b8e560a4faad70.

--- src/cli/command_parser.rb
+++ src/cli/command_parser.rb
@@ -15,7 +15,6 @@
 #--------------------------------------------------------------------------- #
 
 require 'optparse'
-require 'optparse/time'
 require 'pp'
 
 class String
@@ -28,17 +27,6 @@ class String
         self.gsub!(/^ {#{spaces}}/, '')
     end
 end
-
-if RUBY_VERSION > '1.8.7'
-    OptionParser.accept(Time) do |s,|
-        begin
-            (Time.strptime(s, "%m/%d/%Y %H:%M:%S")) if s
-        rescue
-            raise OptionParser::InvalidArgument, s
-        end
-    end
-end
-
 
 module CommandParser
     OPTIONS = [
