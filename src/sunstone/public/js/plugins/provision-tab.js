7cc45141f6e478c1d37a8c5861201b7f5ee4364e
Daniel Molina <dmolina@opennebula.org>
2015-01-08 13:19:55 +0100

    bug #3481: Use regex to filter user resources

--- src/sunstone/public/js/plugins/provision-tab.js
+++ src/sunstone/public/js/plugins/provision-tab.js
@@ -4178,7 +4178,7 @@ function setup_provision_vms_list(context, opts) {
       filter = $(this).val();
     }
 
-    provision_vms_datatable.fnFilter( filter, 2 );
+    provision_vms_datatable.fnFilter("^" + filter + "$", 2, true, false);
   })
 
   insertSelectOptions(
@@ -4335,7 +4335,7 @@ function setup_provision_templates_list(context, opts) {
       filter = $(this).val();
     }
 
-    provision_templates_datatable.fnFilter( filter, 3 );
+    provision_templates_datatable.fnFilter("^" + filter + "$", 3, true, false);
   })
 
   insertSelectOptions(
@@ -5054,7 +5054,7 @@ function setup_provision_flows_list(context, opts){
       filter = $(this).val();
     }
 
-    provision_flows_datatable.fnFilter( filter, 2 );
+    provision_flows_datatable.fnFilter("^" + filter + "$", 2, true, false);
   })
 
   insertSelectOptions(
