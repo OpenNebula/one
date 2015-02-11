f50883a0a832c96ec6b635ddf1155515c2a73f51
Ruben S. Montero <rsmontero@opennebula.org>
2015-02-11 11:59:45 +0100

    feature #3383: Use VM_NAME from monitor tro display ZOMBIE and WILD VMs. Mark VMs as imported
    (cherry picked from commit 6371877897528d37f6f7794e014578c0a77209fe)

--- src/vm/VirtualMachine.cc
+++ src/vm/VirtualMachine.cc
@@ -448,6 +448,7 @@ int VirtualMachine::insert(SqlDB * db, string& error_str)
     if (!value.empty())
     {
         deploy_id = value;
+        obj_template->add("IMPORTED", "YES");
     }
 
     // ------------------------------------------------------------------------
