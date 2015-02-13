3617fc91628dd78b361246f7bbf0727d96828e6b
Carlos Martín <cmartin@opennebula.org>
2015-02-13 11:58:47 +0100

    Feature #3383: Add migrator for 4.10.3
    (cherry picked from commit d8f2b448c6fe03b7170e18b6b19cc920561b088b)
    
    Conflicts:
    	include/Nebula.h
    	install.sh

--- install.sh
+++ install.sh
@@ -1203,6 +1203,7 @@ ONEDB_SHARED_MIGRATOR_FILES="src/onedb/shared/2.0_to_2.9.80.rb \
 
 ONEDB_LOCAL_MIGRATOR_FILES="src/onedb/local/4.5.80_to_4.7.80.rb \
                             src/onedb/local/4.7.80_to_4.9.80.rb \
+                            src/onedb/local/4.9.80_to_4.10.3.rb \
                             src/onedb/local/4.9.80_to_4.11.80.rb"
 
 #-------------------------------------------------------------------------------
