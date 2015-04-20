df192a0ab01da6f462aed985b0b147fdfd3112b7
Daniel Molina <dmolina@opennebula.org>
2015-04-20 18:13:53 +0200

    feature #3748: Use extended template table in zones info

--- src/sunstone/public/app/tabs/zones-tab/panels/info.js
+++ src/sunstone/public/app/tabs/zones-tab/panels/info.js
@@ -1,17 +1,24 @@
 define(function(require) {
   var TemplateInfo = require('hbs!./info/content');
   var Locale = require('utils/locale');
-  var PanelRenameTr = require('utils/panel-rename-tr');
+  var RenameTr = require('utils/panel/rename-tr');
+  var ExtendedTemplateTable = require('utils/panel/extended-template-table');
 
   var _html = function(info) {
+    var renameTrHTML = RenameTr.html("Zone", info.ZONE.NAME);
+    var extendedTemplateTableHTML = ExtendedTemplateTable.html(info.ZONE.TEMPLATE, "Zone", Locale.tr("Attributes"));
+
     return TemplateInfo({
       'info': info,
-      'renameTrHTML': PanelRenameTr.html("Zone", info.ZONE.NAME)
+      'renameTrHTML': renameTrHTML,
+      'extendedTemplateTableHTML': extendedTemplateTableHTML
     });
   }
 
   var _setup = function(info, context) {
-    PanelRenameTr.setup("Zone", info.ZONE.ID, context);
+    RenameTr.setup("Zone", info.ZONE.ID, context);
+    ExtendedTemplateTable.setup(info.ZONE.TEMPLATE, "Zone", info.ZONE.ID, context);
+    return false;
   }
 
   var infoPanel = {
