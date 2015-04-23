d5c9140eff16ed4f05f69e19b030ee768a573214
Daniel Molina <dmolina@opennebula.org>
2015-04-23 18:43:09 +0200

    feature #3748: Add datastore actions

---
+++ src/sunstone/public/app/tabs/datastores-tab/actions.js
@@ -0,0 +1,216 @@
+define(function(require) {
+  var Sunstone = require('sunstone');
+  var Notifier = require('utils/notifier');
+  var Locale = require('utils/locale');
+  var DatastoresDataTable = require('./datatable');
+  var OpenNebulaDatastore = require('opennebula/datastore');
+  var OpenNebulaCluster = require('opennebula/cluster');
+  var OpenNebulaAction = require('opennebula/action');
+
+  var TAB_ID = require('./tabId');
+  var CREATE_DIALOG_ID = require('./dialogs/create/dialogId');
+
+  var _actions = {
+    "Datastore.create" : {
+      type: "create",
+      call : OpenNebulaDatastore.create,
+      callback : function(request, response) {
+        Sunstone.hideDialog(TAB_ID, CREATE_DIALOG_ID);
+        Sunstone.resetDialog(TAB_ID, CREATE_DIALOG_ID);
+        DatastoresDataTable.addElement(request, response);
+        Notifier.notifyCustom(Locale.tr("Datastore created"), " ID: " + response.DATASTORE.ID, false);
+      },
+      error : Notifier.onError
+    },
+
+    "Datastore.create_dialog" : {
+      type: "custom",
+      call: function() {
+        Sunstone.showDialog(TAB_ID, CREATE_DIALOG_ID);
+      }
+    },
+
+    "Datastore.list" : {
+      type: "list",
+      call: OpenNebulaDatastore.list,
+      callback: DatastoresDataTable.updateView,
+      error: Notifier.onError
+    },
+
+    "Datastore.show" : {
+      type: "single",
+      call: OpenNebulaDatastore.show,
+      callback: function(request, response) {
+        DatastoresDataTable.updateElement(request, response);
+        if (Sunstone.rightInfoVisible($('#' + TAB_ID))) {
+          Sunstone.insertPanels(TAB_ID, response);
+        }
+      },
+      error: Notifier.onError
+    },
+
+    "Datastore.refresh" : {
+      type: "custom",
+      call: function() {
+          var tab = $('#' + TAB_ID);
+          if (Sunstone.rightInfoVisible(tab)) {
+            Sunstone.runAction("Datastore.show", Sunstone.rightInfoResourceId(tab))
+          } else {
+            DatastoresDataTable.waitingNodes();
+            Sunstone.runAction("Datastore.list", {force: true});
+          }
+        },
+      error: Notifier.onError
+    },
+
+    "Datastore.fetch_permissions" : {
+      type: "single",
+      call: OpenNebulaDatastore.show,
+      callback: function(request, element_json) {
+        //var ds = element_json.DATASTORE;
+        //setPermissionsTable(ds, $(".datastore_permissions_table"));
+      },
+      error: Notifier.onError
+    },
+
+    "Datastore.update_template" : {
+      type: "single",
+      call: OpenNebulaDatastore.update,
+      callback: function(request) {
+        Sunstone.runAction('Datastore.show', request.request.data[0][0]);
+      },
+      error: Notifier.onError
+    },
+
+    "Datastore.update" : {
+      type: "single",
+      call: OpenNebulaDatastore.update,
+      callback: function() {
+        Sunstone.runAction('Datastore.show', request.request.data[0][0]);
+      },
+      error: Notifier.onError
+    },
+
+    "Datastore.delete" : {
+      type: "multiple",
+      call : OpenNebulaDatastore.del,
+      callback : DatastoresDataTable.deleteElement,
+      elements: DatastoresDataTable.elements,
+      error : Notifier.onError,
+      notify: true
+    },
+
+    "Datastore.chown" : {
+      type: "multiple",
+      call: OpenNebulaDatastore.chown,
+      callback:  function (req) {
+        Sunstone.runAction("Datastore.show", req.request.data[0][0]);
+      },
+      elements: DatastoresDataTable.elements,
+      error: Notifier.onError
+    },
+
+    "Datastore.chgrp" : {
+      type: "multiple",
+      call: OpenNebulaDatastore.chgrp,
+      callback: function (req) {
+        Sunstone.runAction("Datastore.show", req.request.data[0][0]);
+      },
+      elements: DatastoresDataTable.elements,
+      error: Notifier.onError
+    },
+
+    "Datastore.chmod" : {
+      type: "single",
+      call: OpenNebulaDatastore.chmod,
+      callback: function (req) {
+        Sunstone.runAction("Datastore.show", req.request.data[0]);
+      },
+      error: Notifier.onError
+    },
+
+    "Datastore.addtocluster" : {
+      type: "multiple",
+      call: function(params, success) {
+        var cluster = params.data.extra_param;
+        var ds = params.data.id;
+
+        if (cluster == -1) {
+          OpenNebulaDatastore.show({
+            data : {
+              id: ds
+            },
+            success: function (request, ds_info) {
+              var current_cluster = ds_info.DATASTORE.CLUSTER_ID;
+
+              if (current_cluster != -1) {
+                OpenNebulaCluster.deldatastore({
+                  data: {
+                    id: current_cluster,
+                    extra_param: ds
+                  },
+                  success: function() {
+                    OpenNebulaAction.clear_cache("DATASTORE");
+                    Sunstone.runAction('Datastore.show', ds);
+                  },
+                  error: Notifier.onError
+                });
+              } else {
+                OpenNebulaAction.clear_cache("DATASTORE");
+                Sunstone.runAction('Datastore.show', ds);
+              }
+            },
+            error: Notifier.onError
+          });
+        } else {
+          OpenNebulaCluster.adddatastore({
+            data: {
+              id: cluster,
+              extra_param: ds
+            },
+            success: function() {
+              OpenNebulaActionr.clear_cache("DATASTORE");
+              Sunstone.runAction('Datastore.show', ds);
+            },
+            error: Notifier.onError
+          });
+        }
+      },
+      elements: DatastoresDataTable.elements
+    },
+
+    "Datastore.rename" : {
+      type: "single",
+      call: OpenNebulaDatastore.rename,
+      callback: function(request) {
+        Sunstone.runAction('Datastore.show', request.request.data[0][0]);
+      },
+      error: Notifier.onError,
+      notify: true
+    },
+
+    "Datastore.enable" : {
+      type: "multiple",
+      call: OpenNebulaDatastore.enable,
+      callback: function (req) {
+        Sunstone.runAction("Datastore.show", req.request.data[0]);
+      },
+      elements: DatastoresDataTable.elements,
+      error: Notifier.onError,
+      notify: true
+    },
+
+    "Datastore.disable" : {
+      type: "multiple",
+      call: OpenNebulaDatastore.disable,
+      callback: function (req) {
+        Sunstone.runAction("Datastore.show", req.request.data[0]);
+      },
+      elements: DatastoresDataTable.elements,
+      error: Notifier.onError,
+      notify: true
+    }
+  };
+
+  return _actions;
+});
