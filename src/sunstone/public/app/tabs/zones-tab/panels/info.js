bce9cc374d471a0557951839e7d20e802f840000
Daniel Molina <dmolina@opennebula.org>
2015-04-24 11:16:06 +0200

    feature #3748: Use generic vars for zone info panel

--- src/sunstone/public/app/tabs/zones-tab/panels/info.js
+++ src/sunstone/public/app/tabs/zones-tab/panels/info.js
@@ -5,10 +5,14 @@ define(function(require) {
   var ExtendedTemplateTable = require('utils/panel/extended-template-table');
 
   var PANEL_ID = require('./info/panelId');
+  var RESOURCE = "Zone"
 
   var _html = function(info) {
-    var renameTrHTML = RenameTr.html("Zone", info.ZONE.NAME);
-    var extendedTemplateTableHTML = ExtendedTemplateTable.html(info.ZONE.TEMPLATE, "Zone", Locale.tr("Attributes"));
+    var element = _element(info);
+    var renameTrHTML = RenameTr.html(RESOURCE, element.NAME);
+    var extendedTemplateTableHTML = ExtendedTemplateTable.html(
+                                      element.TEMPLATE, RESOURCE, 
+                                      Locale.tr("Attributes"));
 
     return TemplateInfo({
       'info': info,
@@ -18,9 +22,17 @@ define(function(require) {
   }
 
   var _setup = function(info, context) {
-    RenameTr.setup("Zone", info.ZONE.ID, context);
-    ExtendedTemplateTable.setup(info.ZONE.TEMPLATE, "Zone", info.ZONE.ID, context);
+    var element = _element(info);
+    RenameTr.setup(RESOURCE, element.ID, context);
+    ExtendedTemplateTable.setup(element.TEMPLATE, RESOURCE, element.ID, context);
     return false;
+  }
+
+  /*
+    Returns the object representing the resource, access the root element
+   */
+  var _element = function(info) {
+    return info[RESOURCE.toUpperCase()]
   }
 
   var InfoPanel = {
