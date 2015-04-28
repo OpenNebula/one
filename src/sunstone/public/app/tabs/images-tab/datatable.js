8dbf17fc519eeb059e21c2b2f89310c7d1be87cf
Daniel Molina <dmolina@opennebula.org>
2015-04-28 18:17:32 +0200

    feature #3748: Add image datable

---
+++ src/sunstone/public/app/tabs/images-tab/datatable.js
@@ -0,0 +1,127 @@
+define(function(require) {
+  require('foundation-datatables');
+
+  var SunstoneConfig = require('sunstone-config');
+  var TabDataTable = require('utils/tab-datatable');
+  var Humanize = require('utils/humanize');
+  var OpenNebulaImage = require('opennebula/image');
+
+  var _dataTable;
+  var DATATABLE_ID = "dataTableImages";
+  var DATATABLE_SEARCH_ID = "imagesSearch";
+  var TAB_NAME = require('./tabId');
+
+  function _initialize() {
+    _dataTable = $('#' + DATATABLE_ID).dataTable({
+      "bSortClasses": false,
+      "bDeferRender": true,
+      "aoColumnDefs": [
+        {"bSortable": false, "aTargets": ["check"]},
+        {"sWidth": "35px", "aTargets": [0]},
+        {"bVisible": true, "aTargets": SunstoneConfig.tabTableColumns(TAB_NAME)},
+        {"bVisible": false, "aTargets": ['_all']}
+      ]
+    });
+ 
+    $('#' + DATATABLE_SEARCH_ID).keyup(function() {
+      _dataTable.fnFilter($(this).val());
+    })
+
+    _dataTable.on('draw', function() {
+      TabDataTable.recountCheckboxes(_dataTable);
+    })
+
+    TabDataTable.initCheckAllBoxes(_dataTable);
+    TabDataTable.tableCheckboxesListener(_dataTable);
+    TabDataTable.infoListener(_dataTable, "Image.show");
+    _dataTable.fnSort([[1, SunstoneConfig.tableOrder]]);
+  }
+
+  var _dataTableHTML = function() {
+    var TemplateDataTableHTML = require('hbs!./datatable/table');
+    return TemplateDataTableHTML({'dataTableId': DATATABLE_ID});
+  }
+
+  var _searchInputHTML = function() {
+    var TemplateSearchInputHTML = require('hbs!./datatable/search-input');
+    return TemplateSearchInputHTML({'dataTableSearchId': DATATABLE_SEARCH_ID});
+  }
+
+  var _elements = function() {
+    return TabDataTable.getSelectedNodes(_dataTable);
+  }
+
+  var _elementArray = function(element_json) {
+    var element = element_json.IMAGE;
+
+    // KERNEL || RAMDISK || CONTEXT
+    if (element.TYPE == "3" ||  element.TYPE == "4" || element.TYPE == "5") {
+      return false;
+    }
+
+    return [
+      '<input class="check_item" type="checkbox" id="image_' + element.ID + '" name="selected_items" value="' + element.ID + '"/>',
+      element.ID,
+      element.UNAME,
+      element.GNAME,
+      element.NAME,
+      element.DATASTORE,
+      element.SIZE,
+      OpenNebulaImage.typeStr(element.TYPE),
+      Humanize.prettyTime(element.REGTIME),
+      parseInt(element.PERSISTENT) ? "yes" : "no",
+      OpenNebulaImage.stateStr(element.STATE),
+      element.RUNNING_VMS,
+      element.TEMPLATE.TARGET ? element.TEMPLATE.TARGET : '--'
+    ];
+  }
+
+  //callback for an action affecting a zone element
+  var _updateElement = function(request, element_json) {
+    var id = element_json.IMAGE.ID;
+    var element = _elementArray(element_json);
+    TabDataTable.updateSingleElement(element, _dataTable, '#image_' + id);
+  }
+
+  //callback for actions deleting a zone element
+  var _deleteElement = function(req) {
+    TabDataTable.deleteElement(_dataTable, '#image_' + req.request.data);
+  }
+
+  //call back for actions creating a zone element
+  var _addElement = function(request, element_json) {
+    var element = _elementArray(element_json);
+    TabDataTable.addElement(element, _dataTable);
+  }
+
+  //callback to update the list of zones.
+  var _updateView = function(request, list) {
+    var list_array = [];
+
+    $.each(list, function() {
+      list_array.push(_elementArray(this));
+    });
+
+    TabDataTable.updateView(list_array, _dataTable);
+
+    /* TODO
+    var size = humanize_size_from_mb(size_images)
+
+    $(".total_images").text(image_list_array.length);
+    $(".size_images").text(size);
+    */
+  };
+
+  return {
+    'initialize': _initialize,
+    'dataTableHTML': _dataTableHTML,
+    'searchInputHTML': _searchInputHTML,
+    'elements': _elements,
+    'elementArray': _elementArray,
+    'updateElement': _updateElement,
+    'deleteElement': _deleteElement,
+    'addElement': _addElement,
+    'updateView': _updateView,
+    'waitingNodes': TabDataTable.waitingNodes
+  }
+});
