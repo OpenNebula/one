a1cdbea44a118f889ca011144850c97d86dcbec1
Daniel Molina <dmolina@opennebula.org>
2015-04-30 14:33:23 +0200

    feature #3748: Use panel instances in datastore tab

--- src/sunstone/public/app/tabs/datastores-tab/panels/images.js
+++ src/sunstone/public/app/tabs/datastores-tab/panels/images.js
@@ -1,21 +1,53 @@
 define(function(require){
+  /*
+    DEPENDENCIES
+   */
+  
   var Locale = require('utils/locale');
+  var ImagesTable = require('tabs/images-tab/datatable');
+  
+  /*
+    CONSTANTS
+   */
+  
+  var IAMGES_TAB_ID = require('tabs/images-tab/tabId');
   var PANEL_ID = require('./images/panelId');
+  var IMAGES_TABLE_ID = PANEL_ID + "ImagesTable"
+  var RESOURCE = "Datastore"
 
-  var _html = function(info) {
-    return ''
+  /*
+    CONSTRUCTOR
+   */
+
+  function Panel(info) {
+    this.title = Locale.tr("Images");
+    this.icon = "fa-upload";
+
+    this.element = info[RESOURCE.toUpperCase()];
+    this.imagesDataTable = new ImagesTable(IMAGES_TABLE_ID, {info: true}, IAMGES_TAB_ID);
+
+    return this;
+  };
+
+  Panel.PANEL_ID = PANEL_ID;
+  Panel.prototype.html = _html;
+  Panel.prototype.setup = _setup;
+
+  return Panel;
+
+  /*
+    FUNCTION DEFINITIONS
+   */
+
+  function _html() {
+    return this.imagesDataTable.dataTableHTML;
   }
 
-  var _setup = function(info, context) {
+  function _setup(context) {
+    this.imagesDataTable.initialize();
+    this.imagesDataTable.filter(this.element.NAME, ImagesTable.COLUMN_IDS.DATASTORE);
+    this.imagesDataTable.list();
+
     return false;
   }
-  var ImagesPanel = {
-    title : Locale.tr("Images"),
-    icon: "fa-upload",
-    panelId: PANEL_ID,
-    html : _html,
-    setup: _setup
-  }
-
-  return ImagesPanel;
 })
--- src/sunstone/public/app/tabs/datastores-tab/panels/info.js
+++ src/sunstone/public/app/tabs/datastores-tab/panels/info.js
@@ -1,5 +1,8 @@
 define(function(require) {
-  var TemplateInfo = require('hbs!./info/html');
+  /*
+    DEPENDENCIES
+   */
+  
   var Locale = require('utils/locale');
   var Humanize = require('utils/humanize');
   var RenameTr = require('utils/panel/rename-tr');
@@ -9,28 +12,61 @@ define(function(require) {
   var OpenNebulaDatastore = require('opennebula/datastore');
   var DatastoreCapacityBar = require('../utils/datastore-capacity-bar');
 
+  /*
+    TEMPLATES
+   */
+  
+  var TemplateInfo = require('hbs!./info/html');
+
+  /*
+    CONSTANTS
+   */
+  
   var TAB_ID = require('../tabId');
   var PANEL_ID = require('./info/panelId');
   var RESOURCE = "Datastore"
 
-  var _html = function(info) {
-    var element = _element(info);
-    var renameTrHTML = RenameTr.html(RESOURCE, element.NAME);
-    var clusterTrHTML = ClusterTr.html(element.CLUSTER);
+  /*
+    CONSTRUCTOR
+   */
+
+  function Panel(info) {
+    this.title = Locale.tr("Info");
+    this.icon = "fa-info-circle";
+
+    this.element = info[RESOURCE.toUpperCase()];
+
+    return this;
+  };
+
+  Panel.PANEL_ID = PANEL_ID;
+  Panel.prototype.html = _html;
+  Panel.prototype.setup = _setup;
+
+  return Panel;
+
+  /*
+    FUNCTION DEFINITIONS
+   */
+
+
+  function _html() {
+    var renameTrHTML = RenameTr.html(RESOURCE, this.element.NAME);
+    var clusterTrHTML = ClusterTr.html(this.element.CLUSTER);
     var templateTableHTML = TemplateTable.html(
-                                      element.TEMPLATE, RESOURCE, 
+                                      this.element.TEMPLATE, RESOURCE, 
                                       Locale.tr("Attributes"));
-    var permissionsTableHTML = PermissionsTable.html(TAB_ID, RESOURCE, element);
-    var capacityBar = DatastoreCapacityBar.html(element);
-    var stateStr = Locale.tr(OpenNebulaDatastore.stateStr(element.STATE));
+    var permissionsTableHTML = PermissionsTable.html(TAB_ID, RESOURCE, this.element);
+    var capacityBar = DatastoreCapacityBar.html(this.element);
+    var stateStr = Locale.tr(OpenNebulaDatastore.stateStr(this.element.STATE));
 
     var limitStr = '-';
-    if (element.TEMPLATE.SHARED == "NO" || element.TEMPLATE.LIMIT_MB != undefined) {
-      limitStr = Humanize.sizeFromMB(element.TEMPLATE.LIMIT_MB)
+    if (this.element.TEMPLATE.SHARED == "NO" || this.element.TEMPLATE.LIMIT_MB != undefined) {
+      limitStr = Humanize.sizeFromMB(this.element.TEMPLATE.LIMIT_MB)
     }
 
     return TemplateInfo({
-      'element': element,
+      'element': this.element,
       'renameTrHTML': renameTrHTML,
       'clusterTrHTML': clusterTrHTML,
       'templateTableHTML': templateTableHTML,
@@ -41,29 +77,11 @@ define(function(require) {
     });
   }
 
-  var _setup = function(info, context) {
-    var element = _element(info);
-    RenameTr.setup(RESOURCE, element.ID, context);
-    ClusterTr.setup(RESOURCE, element.ID, element.CLUSTER_ID, context);
-    TemplateTable.setup(element.TEMPLATE, RESOURCE, element.ID, context);
-    PermissionsTable.setup(TAB_ID, RESOURCE, element, context);
+  function _setup(context) {
+    RenameTr.setup(RESOURCE, this.element.ID, context);
+    ClusterTr.setup(RESOURCE, this.element.ID, this.element.CLUSTER_ID, context);
+    TemplateTable.setup(this.element.TEMPLATE, RESOURCE, this.element.ID, context);
+    PermissionsTable.setup(TAB_ID, RESOURCE, this.element, context);
     return false;
   }
-
-  /*
-    Returns the object representing the resource, access the root element
-   */
-  var _element = function(info) {
-    return info[RESOURCE.toUpperCase()]
-  }
-
-  var InfoPanel = {
-    title : Locale.tr("Info"),
-    icon: "fa-info-circle",
-    panelId: PANEL_ID,
-    html : _html,
-    setup: _setup
-  }
-
-  return InfoPanel;
 });
