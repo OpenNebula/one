c025cd02e4082f2e56eb7f7a7736b114c16acf21
Daniel Molina <dmolina@opennebula.org>
2015-06-17 16:38:51 +0200

    feature #3748: Add provision-tab templates

---
+++ src/sunstone/public/app/templates/helpers/isTabPanelEnabled.js
@@ -0,0 +1,16 @@
+define(function(require) {
+  var Handlebars = require('hbs/handlebars');
+  var Config = require('sunstone-config');
+  
+  var isTabPanelEnabled = function(tabName, panel, options) {
+    if (Config.isTabPanelEnabled(tabName, panel)) {
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+  };
+
+  Handlebars.registerHelper('isTabPanelEnabled', isTabPanelEnabled);
+
+  return isTabPanelEnabled;
+})
