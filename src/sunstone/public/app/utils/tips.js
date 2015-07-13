41984ed6d227ce3c9db867d30a55da94f7f53638
Daniel Molina <dmolina@opennebula.org>
2015-07-13 12:00:57 +0200

    Uncomment setupTips function

--- src/sunstone/public/app/utils/tips.js
+++ src/sunstone/public/app/utils/tips.js
@@ -4,7 +4,7 @@ define(function(require) {
   //displays the tip information on mouseover.
   var _setup = function(context, position) {
     //For each tip in this context
-    /*$('.tip', context).each(function() {
+    $('.tip', context).each(function() {
       var obj = $(this);
       obj.removeClass('tip');
       var tip = obj.html();
@@ -15,8 +15,8 @@ define(function(require) {
       }
       //replace the text with an icon and spans
       obj.html('<span data-tooltip class="' + tip_classes.join(' ') + '" data-width="210" title="' + tip + '"><i class="fa fa-question-circle"></i></span>');
-    });*/
-    //$(document).foundation('reflow', 'tooltip');
+    });
+    context.foundation('reflow', 'tooltip');
   }
 
   var _html = function(str) {
