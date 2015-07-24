e20441b4aa5d0e062d89318df0ece9bb0ed251ed
Daniel Molina <dmolina@opennebula.org>
2015-07-24 18:31:19 +0200

    feature #1727: Add resize disk to admin view

--- src/sunstone/public/app/tabs/provision-tab.js
+++ src/sunstone/public/app/tabs/provision-tab.js
@@ -16,6 +16,7 @@ define(function(require) {
   var QuotaLimits = require('utils/quotas/quota-limits');
   var Graphs = require('utils/graphs');
   var RangeSlider = require('utils/range-slider');
+  var DisksResize = require('utils/disks-resize');
 
   var ProvisionQuotaWidget = require('./provision-tab/users/quota-widget');
 
@@ -1461,105 +1462,11 @@ define(function(require) {
               $(".provision_capacity_selector", create_vm_context),
               template_json.VMTEMPLATE.TEMPLATE);
 
-            var template_disk = template_json.VMTEMPLATE.TEMPLATE.DISK
-            var disks = []
-            if ($.isArray(template_disk)) {
-              disks = template_disk
-            } else if (!$.isEmptyObject(template_disk)) {
-              disks = [template_disk]
-            }
-
             var disksContext = $(".provision_disk_selector", create_vm_context);
-            if (disks.length > 0 && Config.provision.create_vm.isEnabled("disk_resize")) {
-              disksContext.html(
-                '<br>'+
-                '<div class="row">'+
-                  '<div class="large-12 columns">'+
-                    '<h3 class="subheader text-right">'+
-                      '<span class="left">'+
-                        '<i class="fa fa-tasks fa-lg"></i>&emsp;'+
-                        Locale.tr("Disks")+
-                      '</span>'+
-                      '<span class="provision_create_template_disk_cost_div hidden">' +
-                        '<span class="cost_value">0.00</span> '+
-                        '<small style="color: #999;">'+Locale.tr("COST")+' / ' + Locale.tr("HOUR") + '</small>'+
-                      '</span>'+
-                    '</h3>'+
-                    '<br>'+
-                  '</div>'+
-                '</div>'+
-                '<div class="row">'+
-                  '<div class="large-12 large-centered columns disksContainer">'+
-                    '<span class="text-center" style="font-size:80px">'+
-                      '<i class="fa fa-spinner fa-spin"></i>'+
-                    '</span>'+
-                  '</div>'+
-                '</div>'+
-                '<br>')
-
-              OpenNebula.Template.show({
-                data : {
-                    id: template_id,
-                    extended: true
-                },
-                success: function(request, extendedTemplateJSON) {
-                  var extendedTemplateDisk = extendedTemplateJSON.VMTEMPLATE.TEMPLATE.DISK;
-                  var extendedDisks = []
-                  if ($.isArray(extendedTemplateDisk)) {
-                    extendedDisks = extendedTemplateDisk
-                  } else if (!$.isEmptyObject(extendedTemplateDisk)) {
-                    extendedDisks = [extendedTemplateDisk]
-                  }
-
-
-                  var disk_cost = template_json.VMTEMPLATE.TEMPLATE.DISK_COST;
-                  if (disk_cost && Config.isFeatureEnabled("showback")) {
-                    $(".provision_create_template_disk_cost_div", disksContext).show();
-
-                    disksContext.on("change.fndtn.slider", '.range-slider', function(){
-                      /*if ($(this).attr('data-slider') <= 0) {
-                        var diskContainer = $(this).parent('.diskContainer');
-                        $("#SIZE", diskContainer).val(diskContainer.data('original_size'));
-                      }*/
-
-                      var cost = 0;
-                      $('.range-slider', disksContext).each(function(){
-                        if ($(this).attr('data-slider') > 0) {
-                          cost += $(this).attr('data-slider') * disk_cost
-                        }
-                      })
-                      $(".cost_value", disksContext).html(cost.toFixed(2));
-                    });
-                  } else {
-                    $(".provision_create_template_disk_cost_div", disksContext).hide();
-                  }
-
-                  var diskContext;
-                  $(".disksContainer", disksContext).html("");
-                  $.each(extendedDisks, function(disk_id, disk) {
-                    diskContext = $('<div class="row diskContainer">'+
-                        '<div class="large-12 columns diskSlider">' +
-                        '</div>' +
-                      '</div>').appendTo($(".disksContainer", disksContext));
-
-                    diskContext.data('template_disk', disks[disk_id]);
-
-                    var sizeGB = disk.SIZE / 1024;
-                    diskContext.data('original_size', sizeGB);
-                    RangeSlider.insert({
-                      'label': disk.IMAGE,
-                      'unitLabel': 'GBs',
-                      'name': 'SIZE',
-                      'start': sizeGB,
-                      'end': sizeGB + 500,
-                      'step': 10,
-                      'startValue': sizeGB
-                    }, $(".diskSlider", diskContext))
-                  })
-                }
-              })
+            if (Config.provision.create_vm.isEnabled("disk_resize")) {
+              DisksResize.insert(template_json, disksContext);
             } else {
-              $(".provision_disk_selector", create_vm_context).html("");
+              disksContext.html("");
             }
 
             if (Config.provision.create_vm.isEnabled("network_select") && (template_json.VMTEMPLATE.TEMPLATE.SUNSTONE_NETWORK_SELECT != "NO")) {
@@ -1618,22 +1525,7 @@ define(function(require) {
             }
           });
 
-          var disks = [];
-          var disk, size;
-          $(".diskContainer", context).each(function(){
-            if ($(this).data("template_disk")) {
-              disk = $(this).data("template_disk");
-              original_size = $(this).data("original_size");
-              size = $("#SIZE", this).val();
-              if (size && size > original_size && size > 0) {
-                disk['SIZE'] = Math.ceil(size * 1024);
-              }
-            }
-
-            if (disk) {
-              disks.push(disk);
-            }
-          });
+          var disks = DisksResize.retrieve($(".provision_disk_selector", context));
 
           var instance_type = $(".provision_instance_types_ul .selected", context);
 
