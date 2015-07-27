9af2e695f160ca108d3fa81637aaa8e0e7856b8b
Javi Fontan <jfontan@opennebula.org>
2015-07-27 09:56:07 -0400

    Bump version and man pages to 4.13.80

--- include/Nebula.h
+++ include/Nebula.h
@@ -384,7 +384,7 @@ public:
      */
     static string code_version()
     {
-        return "4.12.0"; // bump version
+        return "4.13.80"; // bump version
     }
 
     /**
--- src/oca/java/src/org/opennebula/client/OneSystem.java
+++ src/oca/java/src/org/opennebula/client/OneSystem.java
@@ -32,7 +32,7 @@ public class OneSystem
     private static final String GROUP_QUOTA_INFO    = "groupquota.info";
     private static final String GROUP_QUOTA_UPDATE  = "groupquota.update";
 
-    public static final String VERSION = "4.12.0";
+    public static final String VERSION = "4.13.80";
 
     public OneSystem(Client client)
     {
