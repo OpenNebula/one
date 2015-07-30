3c653b10596702d64159772de0fd5b37a989c062
Carlos Martín <cmartin@opennebula.org>
2015-07-30 15:58:09 +0200

    Bug #3882: Improve CustomTags UI

--- src/sunstone/public/app/utils/custom-tags-table.js
+++ src/sunstone/public/app/utils/custom-tags-table.js
@@ -2,6 +2,7 @@ define(function(require) {
 
   var Locale = require('utils/locale');
   var TemplateHTML = require('hbs!./custom-tags-table/html');
+  var RowTemplateHTML = require('hbs!./custom-tags-table/row');
   var TemplateUtils = require('utils/template-utils');
 
   function _html(){
@@ -9,31 +10,16 @@ define(function(require) {
   }
 
   function _setup(context){
-    $('#add_custom', context).click(function() {
-      var table = $('#custom_tags', context)[0];
-      var rowCount = table.rows.length;
-      var row = table.insertRow(rowCount);
-
-      var cell1 = row.insertCell(0);
-      var element1 = document.createElement("input");
-      element1.id = "KEY";
-      element1.type = "text";
-      element1.value = $('input#KEY', context).val();
-      cell1.appendChild(element1);
-
-      var cell2 = row.insertCell(1);
-      var element2 = document.createElement("input");
-      element2.id = "VALUE";
-      element2.type = "text";
-      element2.value = $('input#VALUE', context).val();
-      cell2.appendChild(element2);
-
-      var cell3 = row.insertCell(2);
-      cell3.innerHTML = "<i class='fa fa-times-circle fa fa-lg remove-tab'></i>";
+    context.off("click", ".add_custom_tag");
+    context.on("click", ".add_custom_tag", function(){
+      $(".custom_tags tbody", context).append(RowTemplateHTML());
     });
 
-    context.on("click", "i.remove-tab", function() {
-      $(this).closest("tr").remove();
+    $(".add_custom_tag", context).trigger("click");
+
+    context.on("click", ".custom_tags i.remove-tab", function(){
+      var tr = $(this).closest('tr');
+      tr.remove();
     });
   }
 
@@ -41,9 +27,9 @@ define(function(require) {
   function _retrieveCustomTags(context){
     var template_json = {};
 
-    $('#custom_tags tr', context).each(function(){
-      if ($('#KEY', $(this)).val()) {
-        template_json[$('#KEY', $(this)).val()] = $('#VALUE', $(this)).val();
+    $('.custom_tags tr', context).each(function(){
+      if ($('.custom_tag_key', $(this)).val()) {
+        template_json[$('.custom_tag_key', $(this)).val()] = $('.custom_tag_value', $(this)).val();
       }
     });
 
@@ -53,27 +39,11 @@ define(function(require) {
   // context is the container div of customTagsHtml()
   // template_json are the key:values that will be put into the table
   function _fillCustomTags(context, template_json){
+    $(".custom_tags i.remove-tab", context).trigger("click");
+
     $.each(template_json, function(key, value){
-      var table = $('#custom_tags', context)[0];
-      var rowCount = table.rows.length;
-      var row = table.insertRow(rowCount);
-
-      var cell1 = row.insertCell(0);
-      var element1 = document.createElement("input");
-      element1.id = "KEY";
-      element1.type = "text";
-      element1.value = TemplateUtils.htmlDecode(key);
-      cell1.appendChild(element1);
-
-      var cell2 = row.insertCell(1);
-      var element2 = document.createElement("input");
-      element2.id = "VALUE";
-      element2.type = "text";
-      element2.value = TemplateUtils.htmlDecode(value);
-      cell2.appendChild(element2);
-
-      var cell3 = row.insertCell(2);
-      cell3.innerHTML = "<i class='fa fa-times-circle fa fa-lg remove-tab'></i>";
+      $(".custom_tags tbody", context).append(
+                                    RowTemplateHTML({key: key, value: value}));
     });
   }
 
