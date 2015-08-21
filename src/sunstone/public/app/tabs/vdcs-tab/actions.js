452ca20f566cececdf6582dd3ecd72706c78ea05
Daniel Molina <dmolina@opennebula.org>
2015-08-21 13:04:51 +0200

    bug #3912: Use commonActions in vdcs tab

--- src/sunstone/public/app/tabs/vdcs-tab/actions.js
+++ src/sunstone/public/app/tabs/vdcs-tab/actions.js
@@ -4,12 +4,27 @@ define(function(require) {
   var Locale = require('utils/locale');
   var DataTable = require('./datatable');
   var OpenNebulaResource = require('opennebula/vdc');
+  var CommonActions = require('utils/common-actions');
 
-  var RESOURCE = "Vdc";
   var TAB_ID = require('./tabId');
   var CREATE_DIALOG_ID = require('./form-panels/create/formPanelId');
 
+  var RESOURCE = "Vdc";
+  var XML_ROOT = "VDC";
+
+  var _commonActions = new CommonActions(OpenNebulaResource, RESOURCE, TAB_ID, XML_ROOT);
+
   var _actions = {
+    "Vdc.create_dialog" : _commonActions.showCreate(CREATE_DIALOG_ID),
+    "Vdc.list" : _commonActions.list(),
+    "Vdc.show" : _commonActions.show(),
+    "Vdc.refresh" : _commonActions.refresh(),
+    "Vdc.delete" : _commonActions.del(),
+    "Vdc.update" : _commonActions.update(),
+    "Vdc.update_template" : _commonActions.updateTemplate(),
+    "Vdc.update_dialog" : _commonActions.checkAndShowUpdate(),
+    "Vdc.show_to_update" : _commonActions.showUpdate(CREATE_DIALOG_ID),
+    "Vdc.rename": _commonActions.singleAction('rename'),
 
     "Vdc.create" : {
       type: "create",
@@ -74,204 +89,63 @@ define(function(require) {
       error: Notifier.onError
     },
 
-    "Vdc.create_dialog" : {
-      type: "custom",
-      call: function(){
-        Sunstone.showFormPanel(TAB_ID, CREATE_DIALOG_ID, "create");
-      }
-    },
-
-    "Vdc.list" : {
-      type: "list",
-      call: OpenNebulaResource.list,
-      callback: function(request, response) {
-        Sunstone.getDataTable(TAB_ID).updateView(request, response);
-      },
-      error: Notifier.onError
-    },
-
-    "Vdc.show" : {
-      type: "single",
-      call: OpenNebulaResource.show,
-      callback: function(request, response) {
-        Sunstone.getDataTable(TAB_ID).updateElement(request, response);
-        if (Sunstone.rightInfoVisible($('#'+TAB_ID))) {
-          Sunstone.insertPanels(TAB_ID, response);
-        }
-      },
-      error: Notifier.onError
-    },
-
-    "Vdc.refresh" : {
-      type: "custom",
-      call: function() {
-        var tab = $('#' + TAB_ID);
-        if (Sunstone.rightInfoVisible(tab)) {
-          Sunstone.runAction(RESOURCE+".show", Sunstone.rightInfoResourceId(tab));
-        } else {
-          Sunstone.getDataTable(TAB_ID).waitingNodes();
-          Sunstone.runAction(RESOURCE+".list", {force: true});
-        }
-      },
-      error: Notifier.onError
-    },
-
-    "Vdc.delete" : {
-      type: "multiple",
-      call: OpenNebulaResource.del,
-      callback : function(request, response) {
-        Sunstone.getDataTable(TAB_ID).deleteElement(request, response);
-      },
-      elements: function() {
-        return Sunstone.getDataTable(TAB_ID).elements();
-      },
-      error: Notifier.onError
-    },
-
-    "Vdc.rename" : {
-      type: "single",
-      call: OpenNebulaResource.rename,
-      callback: function(request) {
-        Sunstone.runAction(RESOURCE+'.show',request.request.data[0][0]);
-      },
-      error: Notifier.onError
-    },
-
-    "Vdc.update_dialog" : {
-      type: "custom",
-      call: function(){
-        var selected_nodes = Sunstone.getDataTable(TAB_ID).elements();
-        if ( selected_nodes.length != 1 ) {
-          Notifier.notifyMessage("Please select one (and just one) VDC to update.");
-          return false;
-        }
-
-        var resource_id = ""+selected_nodes[0];
-        Sunstone.runAction(RESOURCE+".show_to_update", resource_id);
-      }
-    },
-
-    "Vdc.show_to_update" : {
-      type: "single",
-      call: OpenNebulaResource.show,
-      callback: function(request, response) {
-        Sunstone.showFormPanel(TAB_ID, CREATE_DIALOG_ID, "update",
-          function(formPanelInstance, context) {
-            formPanelInstance.fill(context, response.VDC);
-          });
-      },
-      error: Notifier.onError
-    },
-
-    "Vdc.update" : {
-      type: "single",
-      call: OpenNebulaResource.update,
-      callback: function(request, response){
-        Sunstone.hideFormPanel(TAB_ID);
-        Notifier.notifyMessage(Locale.tr("VDC updated correctly"));
-      },
-      error: function(request, response){
-        Sunstone.hideFormPanelLoading(TAB_ID);
-        Notifier.onError(request, response);
-      }
-    },
-
-    "Vdc.update_template" : {
-      type: "single",
-      call: OpenNebulaResource.update,
-      callback: function(request) {
-        Sunstone.runAction(RESOURCE+'.show',request.request.data[0][0]);
-      },
-      error: Notifier.onError
-    },
-
     "Vdc.add_group" : {
       type: "single",
       call : OpenNebulaResource.add_group,
-      callback : function (req) {
-        //Sunstone.runAction(RESOURCE+'.show',req.request.data[0][0]);
-      },
       error : Notifier.onError
     },
 
     "Vdc.del_group" : {
       type: "single",
       call : OpenNebulaResource.del_group,
-      callback : function (req) {
-        //Sunstone.runAction(RESOURCE+'.show',req.request.data[0][0]);
-      },
       error : Notifier.onError
     },
 
     "Vdc.add_cluster" : {
       type: "single",
       call : OpenNebulaResource.add_cluster,
-      callback : function (req) {
-        //Sunstone.runAction(RESOURCE+'.show',req.request.data[0][0]);
-      },
       error : Notifier.onError
     },
 
     "Vdc.del_cluster" : {
       type: "single",
       call : OpenNebulaResource.del_cluster,
-      callback : function (req) {
-        //Sunstone.runAction(RESOURCE+'.show',req.request.data[0][0]);
-      },
       error : Notifier.onError
     },
 
     "Vdc.add_host" : {
       type: "single",
       call : OpenNebulaResource.add_host,
-      callback : function (req) {
-        //Sunstone.runAction(RESOURCE+'.show',req.request.data[0][0]);
-      },
       error : Notifier.onError
     },
 
     "Vdc.del_host" : {
       type: "single",
       call : OpenNebulaResource.del_host,
-      callback : function (req) {
-        //Sunstone.runAction(RESOURCE+'.show',req.request.data[0][0]);
-      },
       error : Notifier.onError
     },
 
     "Vdc.add_vnet" : {
       type: "single",
       call : OpenNebulaResource.add_vnet,
-      callback : function (req) {
-        //Sunstone.runAction(RESOURCE+'.show',req.request.data[0][0]);
-      },
       error : Notifier.onError
     },
 
     "Vdc.del_vnet" : {
       type: "single",
       call : OpenNebulaResource.del_vnet,
-      callback : function (req) {
-        //Sunstone.runAction(RESOURCE+'.show',req.request.data[0][0]);
-      },
       error : Notifier.onError
     },
 
     "Vdc.add_datastore" : {
       type: "single",
       call : OpenNebulaResource.add_datastore,
-      callback : function (req) {
-        //Sunstone.runAction(RESOURCE+'.show',req.request.data[0][0]);
-      },
       error : Notifier.onError
     },
 
     "Vdc.del_datastore" : {
       type: "single",
       call : OpenNebulaResource.del_datastore,
-      callback : function (req) {
-        //Sunstone.runAction(RESOURCE+'.show',req.request.data[0][0]);
-      },
       error : Notifier.onError
     }
   };
