3e94dc2b03b1f5570dc39cbfcfc631cb4bf95dfc
Ruben S. Montero <rsmontero@opennebula.org>
2015-10-27 16:16:22 +0100

    bug-4088: Fix pagination parser in Ruby OX
    (cherry picked from commit f7f3b545438bfe31864b316856ecd4e8aa06b073)

--- src/oca/ruby/opennebula/xml_utils.rb
+++ src/oca/ruby/opennebula/xml_utils.rb
@@ -47,12 +47,16 @@ module OpenNebula
         attr_accessor :pool
 
         def initialize (pool_name, elem_name)
-            @current = 0
-            @levels  = [{}]
-            @pool    = Array.new
+            clear
 
             @pool_name = pool_name
             @elem_name = elem_name
+        end
+
+        def clear
+            @current = 0
+            @levels  = [{}]
+            @pool    = Array.new
         end
 
         def start_element(name, attrs = [])
@@ -98,7 +102,7 @@ module OpenNebula
         end
 
         def parse(str)
-            @pool_sax.pool.clear
+            @pool_sax.clear
             sax_parse(str)
             @pool_sax.pool
         end
