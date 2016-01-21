3ff13fbcdc4e5918a45fca558c09e4150a0afb77
Jaime Melis <jmelis@opennebula.org>
2016-01-21 17:56:02 +0100

    Feature #4305: Add onedb sqlite2mysql command
    (cherry picked from commit f9c950367be5cab8a464ba7460db58e22567bcaf)

---
+++ src/onedb/sqlite2mysql.rb
@@ -0,0 +1,33 @@
+module Sqlite2MySQL
+
+    PROGRESS = 1000
+
+    def convert(sqlite_db)
+        puts "Starting migration from SQLite to MySQL\n"
+
+        sqlite_db.tables.each do |table|
+            @db[table].delete
+
+            records = sqlite_db[table].count
+            puts "> #{table} (#{records} records)"
+
+            i=0
+            sqlite_db[table].each do |row|
+                @db[table].insert(row)
+
+                i+=1
+                if i % PROGRESS == 0
+                    if i < PROGRESS+1
+                        print "  #{i}"
+                    else
+                        print "...#{i}"
+                    end
+                end
+            end
+
+            puts if i > PROGRESS-1
+        end
+
+        puts "\nMigration successful."
+    end
+end
