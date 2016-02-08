b20b96e76e0d42548771bb8eb0d218f7ab435dd9
Daniel Molina <dmolina@opennebula.org>
2016-02-08 10:44:01 +0100

    Feature #4217: Add marketplaceapps-tab

---
+++ src/sunstone/public/app/tabs/marketplaceapps-tab.js
@@ -0,0 +1,58 @@
+/* -------------------------------------------------------------------------- */
+/* Copyright 2002-2015, OpenNebula Project, OpenNebula Systems                */
+/*                                                                            */
+/* Licensed under the Apache License, Version 2.0 (the "License"); you may    */
+/* not use this file except in compliance with the License. You may obtain    */
+/* a copy of the License at                                                   */
+/*                                                                            */
+/* http://www.apache.org/licenses/LICENSE-2.0                                 */
+/*                                                                            */
+/* Unless required by applicable law or agreed to in writing, software        */
+/* distributed under the License is distributed on an "AS IS" BASIS,          */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   */
+/* See the License for the specific language governing permissions and        */
+/* limitations under the License.                                             */
+/* -------------------------------------------------------------------------- */
+
+define(function(require) {
+  var Locale = require('utils/locale');
+  var Buttons = require('./marketplaceapps-tab/buttons');
+  var Actions = require('./marketplaceapps-tab/actions');
+  var Table = require('./marketplaceapps-tab/datatable');
+
+  var TAB_ID = require('./marketplaceapps-tab/tabId');
+  var DATATABLE_ID = "dataTableMarketplaceApps";
+
+  var _dialogs = [
+  ];
+
+  var _panels = [
+    //require('./marketplaceapps-tab/panels/info')
+  ];
+
+  var _panelsHooks = [
+    require('../utils/hooks/header')
+  ];
+
+  var _formPanels = [
+    //require('./datastores-tab/form-panels/create')
+  ];
+
+  var Tab = {
+    tabId: TAB_ID,
+    title: '<i class="fa fa-lg fa-fw fa-shopping-cart"></i>&emsp;' + Locale.tr("Apps"),
+    listHeader: '<i class="fa fa-fw fa-shopping-cart"></i>&emsp;'+Locale.tr("Apps"),
+    infoHeader: '<i class="fa fa-fw fa-shopping-cart"></i>&emsp;'+Locale.tr("App"),
+    subheader: '<span/> <small></small>&emsp;',
+    resource: 'MarketPlace',
+    buttons: Buttons,
+    actions: Actions,
+    dataTable: new Table(DATATABLE_ID, {actions: true, info: true, oneSelection: true}),
+    panels: _panels,
+    panelsHooks: _panelsHooks,
+    formPanels: _formPanels,
+    dialogs: _dialogs
+  };
+
+  return Tab;
+});
