bdba6c66a7769d48ae76ffe53653d1a05564b80a
Tino Vazquez <tinova@opennebula.org>
2016-03-03 17:29:03 +0100

    feature #4186: Add modified rbvmomi lib Datastore.rb

--- share/vendor/ruby/gems/rbvmomi/lib/rbvmomi/vim/Datastore.rb
+++ share/vendor/ruby/gems/rbvmomi/lib/rbvmomi/vim/Datastore.rb
@@ -26,6 +26,7 @@ class RbVmomi::VIM::Datastore
   # @return [void]
   def download remote_path, local_path
     url = "http#{_connection.http.use_ssl? ? 's' : ''}://#{_connection.http.address}:#{_connection.http.port}#{mkuripath(remote_path)}"
+puts url
     pid = spawn CURLBIN, "-k", '--noproxy', '*', '-f',
                 "-o", local_path,
                 "-b", _connection.cookie,
@@ -41,11 +42,53 @@ class RbVmomi::VIM::Datastore
   # @return [void]
   def download_to_stdout remote_path
     url = "http#{_connection.http.use_ssl? ? 's' : ''}://#{_connection.http.address}:#{_connection.http.port}#{mkuripath(remote_path)}"
+
     pid = spawn CURLBIN, "-k", '--noproxy', '*', '-f',
                 "-b", _connection.cookie,
                 url
+
+
     Process.waitpid(pid, 0)
     fail "download failed" unless $?.success?
+  end
+
+  def is_descriptor? remote_path
+    url = "http#{_connection.http.use_ssl? ? 's' : ''}://#{_connection.http.address}:#{_connection.http.port}#{mkuripath(remote_path)}"
+
+     rout, wout = IO.pipe
+    
+     pid = spawn CURLBIN, "-I", "-k", '--noproxy', '*', '-f',
+                "-b", _connection.cookie,
+                url,
+		:out => wout,
+                :err => '/dev/null'
+
+    Process.waitpid(pid, 0)
+    fail "read image header failed" unless $?.success?
+
+     wout.close
+     size = rout.readlines.select{|l| l.start_with?("Content-Length")}[0].sub("Content-Length: ","")
+     rout.close
+	size.chomp.to_i < 4096   # If <4k, then is a descriptor 
+  end
+
+  def get_text_file remote_path
+    url = "http#{_connection.http.use_ssl? ? 's' : ''}://#{_connection.http.address}:#{_connection.http.port}#{mkuripath(remote_path)}"
+
+     rout, wout = IO.pipe
+     pid = spawn CURLBIN, "-k", '--noproxy', '*', '-f',
+                "-b", _connection.cookie,
+                url,
+		:out => wout,
+                :err => '/dev/null'
+
+    Process.waitpid(pid, 0)
+    fail "get text file failed" unless $?.success?
+
+     wout.close
+     output = rout.readlines
+     rout.close
+     return output
   end
 
   # Upload a file to this datastore.
