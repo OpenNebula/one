34f331a187b8c8d6f91fcb5a8cd499ac670513e3
Carlos Martín <cmartin@opennebula.org>
2016-03-17 17:58:20 +0100

    Feature #4369: Add multiple clusters to onedb upgrade & fsck

--- src/onedb/local/4.13.85_to_4.90.0.rb
+++ src/onedb/local/4.13.85_to_4.90.0.rb
@@ -34,12 +34,180 @@ module Migrator
   def up
     init_log_time()
 
+    # 4369
+
+    @db.run "CREATE TABLE cluster_datastore_relation (cid INTEGER, oid INTEGER, PRIMARY KEY(cid, oid));"
+    @db.run "CREATE TABLE cluster_network_relation (cid INTEGER, oid INTEGER, PRIMARY KEY(cid, oid));"
+
+
+    @db.run "ALTER TABLE host_pool RENAME TO old_host_pool;"
+    @db.run "CREATE TABLE host_pool (oid INTEGER PRIMARY KEY, name VARCHAR(128), body MEDIUMTEXT, state INTEGER, last_mon_time INTEGER, uid INTEGER, gid INTEGER, owner_u INTEGER, group_u INTEGER, other_u INTEGER, cid INTEGER);"
+
+    host_ids = []
+
+    @db.transaction do
+      @db.fetch("SELECT * FROM old_host_pool") do |row|
+        doc = Nokogiri::XML(row[:body],nil,NOKOGIRI_ENCODING){|c| c.default_xml.noblanks}
+
+        cid_elem = doc.root.at_xpath("CLUSTER_ID")
+        cid = cid_elem.text.to_i
+
+        if (cid == -1)
+          cid = 0
+          cid_elem.content = "0"
+          doc.root.at_xpath("CLUSTER").content = "default"
+
+          host_ids << row[:oid]
+        end
+
+        @db[:host_pool].insert(
+          :oid            => row[:oid],
+          :name           => row[:name],
+          :body           => doc.root.to_s,
+          :state          => row[:state],
+          :last_mon_time  => row[:last_mon_time],
+          :uid            => row[:uid],
+          :gid            => row[:gid],
+          :owner_u        => row[:owner_u],
+          :group_u        => row[:group_u],
+          :other_u        => row[:other_u],
+          :cid            => cid)
+      end
+    end
+
+    @db.run "DROP TABLE old_host_pool;"
+
+    log_time()
+
+    @db.run "ALTER TABLE datastore_pool RENAME TO old_datastore_pool;"
+    @db.run "CREATE TABLE datastore_pool (oid INTEGER PRIMARY KEY, name VARCHAR(128), body MEDIUMTEXT, uid INTEGER, gid INTEGER, owner_u INTEGER, group_u INTEGER, other_u INTEGER);"
+
+    ds_ids = []
+
+    @db.transaction do
+      @db.fetch("SELECT * FROM old_datastore_pool") do |row|
+        doc = Nokogiri::XML(row[:body],nil,NOKOGIRI_ENCODING){|c| c.default_xml.noblanks}
+
+        doc.root.at_xpath("CLUSTER").remove
+
+        cid_elem = doc.root.at_xpath("CLUSTER_ID")
+        cid = cid_elem.text.to_i
+
+        cid_elem.remove
+
+        if (cid == -1)
+          cid = 0
+          ds_ids << row[:oid]
+        end
+
+        cluster_ids_elem = doc.create_element("CLUSTERS")
+        cluster_ids_elem.add_child(doc.create_element("ID")).content = cid.to_s
+
+        doc.root.add_child(cluster_ids_elem)
+
+        @db[:datastore_pool].insert(
+          :oid      => row[:oid],
+          :name     => row[:name],
+          :body     => doc.root.to_s,
+          :uid      => row[:uid],
+          :gid      => row[:gid],
+          :owner_u  => row[:owner_u],
+          :group_u  => row[:group_u],
+          :other_u  => row[:other_u])
+
+        @db[:cluster_datastore_relation].insert(
+          :cid => cid,
+          :oid => row[:oid])
+      end
+    end
+
+    @db.run "DROP TABLE old_datastore_pool;"
+
+    log_time()
+
+    @db.run "ALTER TABLE network_pool RENAME TO old_network_pool;"
+    @db.run "CREATE TABLE network_pool (oid INTEGER PRIMARY KEY, name VARCHAR(128), body MEDIUMTEXT, uid INTEGER, gid INTEGER, owner_u INTEGER, group_u INTEGER, other_u INTEGER, pid INTEGER, UNIQUE(name,uid));"
+
+    vnet_ids = []
+
+    @db.transaction do
+      @db.fetch("SELECT * FROM old_network_pool") do |row|
+        doc = Nokogiri::XML(row[:body],nil,NOKOGIRI_ENCODING){|c| c.default_xml.noblanks}
+
+        doc.root.at_xpath("CLUSTER").remove
+
+        cid_elem = doc.root.at_xpath("CLUSTER_ID")
+        cid = cid_elem.text.to_i
+
+        cid_elem.remove
+
+        if (cid == -1)
+          cid = 0
+          vnet_ids << row[:oid]
+        end
+
+        cluster_ids_elem = doc.create_element("CLUSTERS")
+        cluster_ids_elem.add_child(doc.create_element("ID")).content = cid.to_s
+
+        doc.root.add_child(cluster_ids_elem)
+
+        @db[:network_pool].insert(
+          :oid      => row[:oid],
+          :name     => row[:name],
+          :body     => doc.root.to_s,
+          :uid      => row[:uid],
+          :gid      => row[:gid],
+          :owner_u  => row[:owner_u],
+          :group_u  => row[:group_u],
+          :other_u  => row[:other_u],
+          :pid      => row[:pid])
+
+        @db[:cluster_network_relation].insert(
+          :cid => cid,
+          :oid => row[:oid])
+      end
+    end
+
+    @db.run "DROP TABLE old_network_pool;"
+
+    log_time()
+
+    default_cl_xml = '<CLUSTER><ID>0</ID><NAME>default</NAME><HOSTS></HOSTS><DATASTORES></DATASTORES><VNETS></VNETS><TEMPLATE><RESERVED_CPU><![CDATA[]]></RESERVED_CPU><RESERVED_MEM><![CDATA[]]></RESERVED_MEM></TEMPLATE></CLUSTER>'
+    doc = Nokogiri::XML(default_cl_xml,nil,NOKOGIRI_ENCODING){|c| c.default_xml.noblanks}
+
+    hosts_elem = doc.root.at_xpath("HOSTS")
+    host_ids.each { |id|
+      hosts_elem.add_child(doc.create_element("ID")).content = id.to_s
+    }
+
+    ds_elem = doc.root.at_xpath("DATASTORES")
+    ds_ids.each { |id|
+      ds_elem.add_child(doc.create_element("ID")).content = id.to_s
+    }
+
+    vnets_elem = doc.root.at_xpath("VNETS")
+    vnet_ids.each { |id|
+      vnets_elem.add_child(doc.create_element("ID")).content = id.to_s
+    }
+
+    @db[:cluster_pool].insert(
+      :oid      => 0,
+      :name     => 'default',
+      :body     => doc.root.to_s,
+      :uid      => 0,
+      :gid      => 0,
+      :owner_u  => 1,
+      :group_u  => 0,
+      :other_u  => 0)
+
+    log_time()
+
     # 4215
 
     @db.run "CREATE TABLE vrouter_pool (oid INTEGER PRIMARY KEY, name VARCHAR(128), body MEDIUMTEXT, uid INTEGER, gid INTEGER, owner_u INTEGER, group_u INTEGER, other_u INTEGER);"
 
     @db.run "ALTER TABLE network_pool RENAME TO old_network_pool;"
-    @db.run "CREATE TABLE network_pool (oid INTEGER PRIMARY KEY, name VARCHAR(128), body MEDIUMTEXT, uid INTEGER, gid INTEGER, owner_u INTEGER, group_u INTEGER, other_u INTEGER, cid INTEGER, pid INTEGER, UNIQUE(name,uid));"
+    @db.run "CREATE TABLE network_pool (oid INTEGER PRIMARY KEY, name VARCHAR(128), body MEDIUMTEXT, uid INTEGER, gid INTEGER, owner_u INTEGER, group_u INTEGER, other_u INTEGER, pid INTEGER, UNIQUE(name,uid));"
 
     @db.transaction do
       @db.fetch("SELECT * FROM old_network_pool") do |row|
@@ -56,7 +224,6 @@ module Migrator
           :owner_u  => row[:owner_u],
           :group_u  => row[:group_u],
           :other_u  => row[:other_u],
-          :cid      => row[:cid],
           :pid      => row[:pid])
       end
     end
