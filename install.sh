8177cfdb5c6087b484d38abd699191f28f41614f
Javi Fontan <jfontan@opennebula.org>
2016-04-07 09:12:57 +0000

    feature #4110: install fs_lvm context and monitor

--- install.sh
+++ install.sh
@@ -1022,7 +1022,9 @@ TM_SHARED_FILES="src/tm_mad/shared/clone \
                  src/tm_mad/shared/cpds"
 
 TM_FS_LVM_FILES="src/tm_mad/fs_lvm/clone \
+                 src/tm_mad/fs_lvm/context \
                  src/tm_mad/fs_lvm/ln \
+                 src/tm_mad/fs_lvm/monitor \
                  src/tm_mad/fs_lvm/mv \
                  src/tm_mad/fs_lvm/mvds \
                  src/tm_mad/fs_lvm/cpds \
