15d00795d52fabf46de87ffb0fc9883fad565e39
Jaime Melis <jmelis@opennebula.org>
2016-04-19 16:48:56 +0200

    Typo

--- src/onedb/shared/4.11.80_to_4.90.0.rb
+++ src/onedb/shared/4.11.80_to_4.90.0.rb
@@ -46,7 +46,6 @@ module Migrator
   def up
     init_log_time()
 
-=begin
     # Feature #3671
 
     @db.run "ALTER TABLE user_pool RENAME TO old_user_pool;"
@@ -197,7 +196,7 @@ module Migrator
     @db.run "REPLACE INTO pool_control VALUES('acl', #{last_oid+1});"
 
     log_time()
-=end
+
     return true
   end
 
