11ec5378e7a0569f30b756356d9428738d0babea
Tino Vazquez <tinova@opennebula.org>
2016-04-26 19:54:06 +0200

    Wrong shutdown signature fixed for vCenter

--- src/vmm_mad/remotes/vcenter/vcenter_driver.rb
+++ src/vmm_mad/remotes/vcenter/vcenter_driver.rb
@@ -1375,7 +1375,7 @@ class VCenterVm
     def self.cancel(deploy_id, hostname, lcm_state, keep_disks, disks)
         case lcm_state
             when "SHUTDOWN_POWEROFF", "SHUTDOWN_UNDEPLOY"
-                shutdown(deploy_id, hostname, lcm_state, keep_disks)
+                shutdown(deploy_id, hostname, lcm_state, keep_disks, disks)
             when "CANCEL", "LCM_INIT", "CLEANUP_RESUBMIT", "SHUTDOWN", "CLEANUP_DELETE"
                 hid         = VIClient::translate_hostname(hostname)
                 connection  = VIClient.new(hid)
