7fa0a59ab0ddf7175430592a75b37e44b4030845
Ruben S. Montero <rsmontero@gmail.com>
2016-05-23 17:41:48 +0200

    Revert "Fix VM ID parsing in `associate_address()`"

--- src/cloud/ec2/lib/elastic_ip.rb
+++ src/cloud/ec2/lib/elastic_ip.rb
@@ -119,7 +119,8 @@ module ElasticIP
 
         user_id = retrieve_uid
         eip     = params["PublicIp"]
-        vmid    = params['InstanceId'].sub(/^i\-0*/, '')
+        vmid    = params['InstanceId']
+        vmid    = vmid.split('-')[1] if vmid[0]==?i
 
         unless vnet["TEMPLATE/EC2_ADDRESSES[IP=\"#{eip}\" and UID=\"#{retrieve_uid}\"]/IP"]
             rc = OpenNebula::Error.new("address:#{eip} does not exist")
