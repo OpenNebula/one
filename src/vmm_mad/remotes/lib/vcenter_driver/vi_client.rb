25fbf0e051c84490330acc56882d3502ffffd9bf
mcabrerizo <mcabrerizo@opennebula.org>
2017-02-17 15:10:22 +0100

    F #4913: Add run in silence methods for uploading/downloading files

--- src/vmm_mad/remotes/lib/vcenter_driver/vi_client.rb
+++ src/vmm_mad/remotes/lib/vcenter_driver/vi_client.rb
@@ -86,6 +86,38 @@ class VIClient
             exit -1
         end
     end
+
+    def self.in_silence
+        begin
+          orig_stderr = $stderr.clone
+          orig_stdout = $stdout.clone
+          $stderr.reopen File.new('/dev/null', 'w')
+          $stdout.reopen File.new('/dev/null', 'w')
+          retval = yield
+        rescue Exception => e
+          $stdout.reopen orig_stdout
+          $stderr.reopen orig_stderr
+          raise e
+        ensure
+          $stdout.reopen orig_stdout
+          $stderr.reopen orig_stderr
+        end
+       retval
+    end
+
+    def self.in_stderr_silence
+        begin
+          orig_stderr = $stderr.clone
+          $stderr.reopen File.new('/dev/null', 'w')
+          retval = yield
+        rescue Exception => e
+          $stderr.reopen orig_stderr
+          raise e
+        ensure
+          $stderr.reopen orig_stderr
+        end
+       retval
+    end
 end
 
 end # module VCenterDriver
