e70caf371515253547f146cddacef85e2b5a19c3
Jaime Melis <jmelis@opennebula.org>
2017-02-21 10:07:35 +0100

    F #4913: Attach-nic requires the XML data of the VM
    This information is passed via STDIN and not as a positional argument.

--- src/vmm_mad/exec/one_vmm_exec.rb
+++ src/vmm_mad/exec/one_vmm_exec.rb
@@ -896,7 +896,7 @@ class ExecDriver < VirtualMachineDriver
             {
                 :driver     => :vmm,
                 :action     => :attach_nic,
-                :parameters => [:deploy_id, mac, source, model, vn_mad, target, xml_data]
+                :parameters => [:deploy_id, mac, source, model, vn_mad, target]
             },
             # Execute post-boot networking setup
             {
