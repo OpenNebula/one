ad1a52399e7fbe48f08dc5394ab00e326550500a
abelCoronado93 <acoronado@opennebula.org>
2017-05-10 12:51:41 +0200

    F #5143 Added the possibility of scaling the cpu

--- src/sunstone/public/app/sunstone-config.js
+++ src/sunstone/public/app/sunstone-config.js
@@ -149,6 +149,7 @@ define(function(require) {
     'enabledTabs': _config['view']['enabled_tabs'],
     'onedConf': _config['oned_conf'],
     'confirmVMActions': _config['view']['confirm_vms'],
+    'scaleFactor': _config['view']['features']['instanciate_cpu_factor'],
 
     "allTabs": function() {
       return Object.keys(_config['view']['tabs']);
