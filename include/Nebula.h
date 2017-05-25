8de979e42c1b3bbacd934615bb18fdde94ad5739
Javi Fontan <jfontan@opennebula.org>
2017-05-25 16:07:35 +0200

    Bump version 5.3.80

--- include/Nebula.h
+++ include/Nebula.h
@@ -1,5 +1,5 @@
 /* -------------------------------------------------------------------------- */
-/* Copyright 2002-2016, OpenNebula Project, OpenNebula Systems                */
+/* Copyright 2002-2017, OpenNebula Project, OpenNebula Systems                */
 /*                                                                            */
 /* Licensed under the Apache License, Version 2.0 (the "License"); you may    */
 /* not use this file except in compliance with the License. You may obtain    */
@@ -357,7 +357,7 @@ public:
      */
     static string code_version()
     {
-        return "5.2.0"; // bump version
+        return "5.3.80"; // bump version
     }
 
     /**
--- share/vendor/ruby/gems/rbvmomi/lib/rbvmomi/version.rb
+++ share/vendor/ruby/gems/rbvmomi/lib/rbvmomi/version.rb
@@ -1,3 +1,3 @@
 module RbVmomi
-  VERSION = '5.2.0'.freeze
+  VERSION = '5.3.80'.freeze
 end
