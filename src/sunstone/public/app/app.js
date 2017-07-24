a0cc06320faf10643de47b155bfa02c7cda5e886
Abel Coronado <acoronado@opennebula.org>
2017-07-24 15:44:33 +0200

    B #4978 Solved bug in date sorting 'Registration Time' (#410)

--- src/sunstone/public/app/app.js
+++ src/sunstone/public/app/app.js
@@ -294,6 +294,33 @@ define(function(require) {
       }
     }
 
+    jQuery.extend( jQuery.fn.dataTableExt.oSort, {
+      "date-euro-pre": function ( a ) {
+          var x;
+
+          if ( $.trim(a) !== '' ) {
+              var frDatea = $.trim(a).split(' ');
+              var frTimea = (undefined != frDatea[1]) ? frDatea[1].split(':') : [00,00,00];
+              var frDatea2 = frDatea[0].split('/');
+              x = (frDatea2[2] + frDatea2[1] + frDatea2[0] + frTimea[0] + frTimea[1] + ((undefined != frTimea[2]) ? frTimea[2] : 0)) * 1;
+          }
+          else {
+              x = Infinity;
+          }
+
+          return x;
+      },
+
+      "date-euro-asc": function ( a, b ) {
+          return a - b;
+      },
+
+      "date-euro-desc": function ( a, b ) {
+          return b - a;
+      }
+
+    });
+
     //source https://cdn.datatables.net/plug-ins/1.10.12/type-detection/ip-address.js (modified)
     jQuery.fn.dataTableExt.aTypes.unshift(
       function ( sData )
