39ba4c92ffe5e393bc106d940c2bd46b0bf8cad0
Sledge Sulaweyo <sledge.sulaweyo@gmail.com>
2017-08-18 14:12:07 +0200

    Update vip.sh
    Send 3 pings with -U and then 3 with -UA
    Do both with sudo as at least on Arch arping does not have the setuid flag. Imho it's more consistent this way as there already is a sudo call for the ip command.

--- share/hooks/raft/vip.sh
+++ share/hooks/raft/vip.sh
@@ -17,7 +17,8 @@ fi
 case $ACTION in
 leader)
     sudo ip address add $IP dev $INTERFACE
-    sudo arping -c 5 -U -I $INTERFACE ${IP%%/*}
+    sudo arping -c 3 -U -I $INTERFACE ${IP%%/*}
+    sudo arping -c 3 -UA -I $INTERFACE ${IP%%/*}
     ;;
 
 follower)
