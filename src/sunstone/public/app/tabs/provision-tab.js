59afcc29aa0513e48aafa38c3d0b5ede93d8abff
juanmont <jjmontiel@opennebula.org>
2017-09-04 11:06:12 +0200

    B #5308 Added revert dialog to provision-tab

--- src/sunstone/public/app/tabs/provision-tab.js
+++ src/sunstone/public/app/tabs/provision-tab.js
@@ -686,6 +686,7 @@ define(function(require) {
     require('./vms-tab/dialogs/disk-snapshot'),
     require('./vms-tab/dialogs/disk-saveas'),
     require('./vms-tab/dialogs/attach-nic'),
+    require('./vms-tab/dialogs/revert'),
     require('./vms-tab/dialogs/snapshot'),
     require('./vms-tab/dialogs/vnc'),
     require('./vms-tab/dialogs/spice'),
