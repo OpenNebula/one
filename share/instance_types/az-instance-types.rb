a39efe290177563fae6d19d4e243bae0a6ca63c4
semedi <ssemedi@opennebula.org>
2017-09-04 10:25:30 +0200

    B #5340 instance_types scripts now work properly

--- share/instance_types/az-instance-types.rb
+++ share/instance_types/az-instance-types.rb
@@ -9,12 +9,22 @@ doc = Nokogiri::HTML(content)
 
 types = {}
 
-doc.root.css("table tbody tr").each do |tr|
-    instanceType = tr.at_css("td[1]").text
-    cpu          = tr.at_css("td[2]").text.to_i
-    memory       = tr.at_css("td[3]").text.split[0].to_f rescue nil
+first = true
+doc.root.css("table").each do |table|
+    if first
+        first = false
+        next
+    end
+    table.css("tbody tr").each do |tr|
+        instanceType = tr.at_css("td[1]").text.gsub(/[^a-zA-Z0-9_.]/,"")
+        cpu          = tr.at_css("td[2]").text.to_i
+        memory       = tr.at_css("td[3]").text.split[0].to_f rescue nil
 
-    types[instanceType] = {"cpu" => cpu, "memory" => memory}
+        types[instanceType] = {"cpu" => cpu, "memory" => memory}
+    end
 end
 
-puts ({"instance_types" => types}).to_yaml(:indentation=>4)
+
+
+puts "proxy_uri:"
+puts ({"instance_types" => types}).to_yaml(:indentation=>4).gsub("---\n", '')
