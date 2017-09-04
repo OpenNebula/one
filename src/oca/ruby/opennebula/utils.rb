1928575a673744d5a26a02881dd70c6ada43e32f
semedi <ssemedi@opennebula.org>
2017-09-04 11:56:00 +0200

    B #5339 new handle_exceptions function inside oca

--- src/oca/ruby/opennebula/utils.rb
+++ src/oca/ruby/opennebula/utils.rb
@@ -47,4 +47,22 @@ module OpenNebula
         return opts
     end
 
+    # >> /var/log/one/oned.log
+    def self.handle_driver_exception(action, ex, host, did, id = nil, file = nil)
+
+        file    ||= ""
+        id      ||= ""
+        OpenNebula::log_error(action + " of VM #{id} #{did} on host #{host} #{file} "+
+                    "due to \"#{ex.message}\"" +
+                    "\n********* STACK TRACE *********\n" +
+                    "\t#{ex.backtrace.join("\n\t")}" +
+                    "\n*******************************\n")
+
+        OpenNebula.error_message("There is a problem: #{ex.message}")
+        exit (-1)
+    end
+
+
+
+
 end
