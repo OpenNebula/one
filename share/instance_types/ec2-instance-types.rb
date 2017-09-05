373890c0d3ed340b8360636d73b8d58670666f9a
Sergio Semedi Barranco <ssemedi@opennebula.org>
2017-09-05 14:29:23 +0200

    B #5340 instance_types scripts now work properly (#442)
    (cherry picked from commit 936ef46d0302ce7c0cbdea92baf83074d3c20456)

--- share/instance_types/ec2-instance-types.rb
+++ share/instance_types/ec2-instance-types.rb
@@ -5,8 +5,8 @@ require 'pp'
 require 'yaml'
 require 'open-uri'
 
-puts "Very slow script... hold on tight for a couple of minutes..."
-puts
+$stderr.reopen("/tmp/ec2_err", "w")
+STDERR.puts "Very slow script... hold on tight for a couple of minutes..."
 
 content = open('https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/index.json').read
 content = JSON.parse(content)
@@ -14,12 +14,17 @@ content = JSON.parse(content)
 types = {}
 content["products"].each do |k,v|
     instanceType = v["attributes"]["instanceType"]
+
+    next unless v["attributes"]["servicecode"] == "AmazonEC2"
+
     memory       = v["attributes"]["memory"].split[0].to_f rescue nil
     cpu          = v["attributes"]["vcpu"].to_i
 
     if types[instanceType].nil? && instanceType && memory && cpu
-        types[instanceType] = {"cpu" => cpu, "memory" => memory}
+        types[instanceType] = {"cpu" => cpu, "memory" => memory} if instanceType.include?(".")
     end
 end
 
-puts ({"instance_types" => types.sort.to_h}).to_yaml(:indentation=>4)
+puts "proxy_uri:"
+puts "state_wait_timeout_seconds: 300"
+puts (({"instance_types" => types.sort.to_h}).to_yaml(:indentation=>4)).gsub("---\n", '')
