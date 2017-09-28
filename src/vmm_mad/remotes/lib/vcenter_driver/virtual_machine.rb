5a6d0e2f9b433381e4e43b87826f00ccc1393aa7
Sergio Semedi Barranco <ssemedi@opennebula.org>
2017-09-28 16:02:11 +0200

    B #5367: vCenter Customizations works (#491)
    (cherry picked from commit 368e65188b696e0cff7b231e46577d9a063fa584)

--- src/vmm_mad/remotes/lib/vcenter_driver/virtual_machine.rb
+++ src/vmm_mad/remotes/lib/vcenter_driver/virtual_machine.rb
@@ -1075,7 +1075,7 @@ class VirtualMachine < Template
             custom_spec = vi_client.vim
                             .serviceContent
                             .customizationSpecManager
-                            .GetCustomizationSpec(:name => customization.text)
+                            .GetCustomizationSpec(:name => customization_spec)
 
             if custom_spec && (spec = custom_spec.spec)
                 return spec
@@ -1083,7 +1083,7 @@ class VirtualMachine < Template
                 raise "Error getting customization spec"
             end
         rescue
-            raise "Customization spec '#{customization.text}' not found"
+            raise "Customization spec '#{customization_spec}' not found"
         end
     end
 
