10a64b5adcaf9319b96952ddafc7e4ee4142c8fe
Javi Fontan <jfontan@opennebula.org>
2017-10-09 18:20:29 +0200

    Bump version to 5.4.2

--- src/cloud/common/CloudClient.rb
+++ src/cloud/common/CloudClient.rb
@@ -50,7 +50,7 @@ end
 module CloudClient
 
     # OpenNebula version
-    VERSION = '5.4.1'
+    VERSION = '5.4.2'
 
     # #########################################################################
     # Default location for the authentication file
--- src/oca/ruby/opennebula.rb
+++ src/oca/ruby/opennebula.rb
@@ -69,5 +69,5 @@ require 'opennebula/vm_group_pool'
 module OpenNebula
 
     # OpenNebula version
-    VERSION = '5.4.1'
+    VERSION = '5.4.2'
 end
