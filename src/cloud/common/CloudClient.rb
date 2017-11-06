98b1286e28dfab7441463b3d948a8e11e750f373
Vlastimil Holer <vlastimil.holer@gmail.com>
2017-11-06 18:23:14 +0100

    Bump to version 5.4.4 (#562)

--- src/cloud/common/CloudClient.rb
+++ src/cloud/common/CloudClient.rb
@@ -50,7 +50,7 @@ end
 module CloudClient
 
     # OpenNebula version
-    VERSION = '5.4.3'
+    VERSION = '5.4.4'
 
     # #########################################################################
     # Default location for the authentication file
--- src/oca/ruby/opennebula.rb
+++ src/oca/ruby/opennebula.rb
@@ -69,5 +69,5 @@ require 'opennebula/vm_group_pool'
 module OpenNebula
 
     # OpenNebula version
-    VERSION = '5.4.3'
+    VERSION = '5.4.4'
 end
