009ca508b8ebab4a1e1339144c630967e45fb90f
Vlastimil Holer <vlastimil.holer@gmail.com>
2017-12-04 18:10:04 +0100

    Bump to version 5.4.5 (#1561)
    * Bump to version 5.4.4
    
    * Bump to version 5.4.5

--- src/cloud/common/CloudClient.rb
+++ src/cloud/common/CloudClient.rb
@@ -50,7 +50,7 @@ end
 module CloudClient
 
     # OpenNebula version
-    VERSION = '5.4.4'
+    VERSION = '5.4.5'
 
     # #########################################################################
     # Default location for the authentication file
