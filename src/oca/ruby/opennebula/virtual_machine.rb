d872ece7bbb6abe1de7d90b22157c09528faa766
juanmont <jjmontiel@opennebula.org>
2017-12-21 12:34:32 +0100

    B #1600: Fixed bug with duplicated nics

--- src/oca/ruby/opennebula/virtual_machine.rb
+++ src/oca/ruby/opennebula/virtual_machine.rb
@@ -850,12 +850,11 @@ module OpenNebula
                         rc = Error.new('The NIC_ID is missing from the VM template')
                         raise
                     end
-                     REMOVE_VNET_ATTRS.each do |attr|
+                    REMOVE_VNET_ATTRS.each do |attr|
                         nic.delete_element(attr)
                     end
-
-                    replace << self.template_like_str(
-                        "TEMPLATE", true, "NIC[#{nic}]") << "\n"
+                    
+                    replace << "NIC = [ " << nic.template_like_str(".").tr("\n", ",\n") << " ] \n"
                 end
 
                 # Required by the Sunstone Cloud View
