1e17628ca54b84054f3f8dd389ba8940a7d1277c
juanmont <jjmontiel@opennebula.org>
2018-01-11 16:58:20 +0100

    F #1377: Added lock icon to header

--- src/sunstone/public/app/utils/hooks/header.js
+++ src/sunstone/public/app/utils/hooks/header.js
@@ -28,10 +28,9 @@ define(function(require) {
     $('.resource-info-header', '#' + contextTabId).text(element.NAME);
 
     if (element.LOCK){
-      $('.resource-lock-header-small', '#' + contextTabId).html("<i class='header-title fa fa-lock'> "+Locale.tr(Humanize.lock_to_str(element.LOCK.LOCKED))+"</i>");
-      $('.resource-lock-header-small', '#' + contextTabId).show();
+      $('.resource-lock-header-small', '#' + contextTabId).html("<span data-tooltip aria-haspopup='true' class='has-tip' data-disable-hover='false' tabindex='1' title="+Locale.tr(Humanize.lock_to_str(element.LOCK.LOCKED))+"><i class='fa fa-lock fa-2x'/></span>");
     } else {
-      $('.resource-lock-header-small', '#' + contextTabId).hide();
+      $('.resource-lock-header-small', '#' + contextTabId).html("<i style='color: #cacedd;' class='fa fa-unlock-alt fa-2x'/>");
     }
   }
 
