73e096825a6a8907ff2fa6bf3fbf587de7edd2b6
Ruben S. Montero <rsmontero@gmail.com>
2018-02-02 18:11:12 +0100

    Update RequestManagerUser.cc

--- src/rm/RequestManagerUser.cc
+++ src/rm/RequestManagerUser.cc
@@ -503,7 +503,7 @@ void UserLogin::request_execute(xmlrpc_c::paramList const& paramList,
     }
     else if (valid > 0 || valid == -1)
     {
-        if ( egid != -1 && (!user->is_in_group(egid) || att.group_ids.find(egid) == att.group_ids.end()) )
+        if ( egid != -1 && (!user->is_in_group(egid) || att.group_ids.count(egid) == 0) )
         {
             att.resp_msg = "EGID is not in user group list";
             failure_response(XML_RPC_API,  att);
