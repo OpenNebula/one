dd0763209f907d127e1b7a8752494e336f2e4826
Vlastimil Holer <vlastimil.holer@gmail.com>
2018-02-12 16:33:03 +0100

    B #1733: Make vip.sh less error prone (#1734)

--- share/hooks/raft/vip.sh
+++ share/hooks/raft/vip.sh
@@ -2,33 +2,47 @@
 
 ACTION="$1"
 INTERFACE="$2"
-IP="$3"
+IFADDR="$3"
+IP="${IFADDR%%/*}"
 
 if [ -z "$INTERFACE" ]; then
     echo "Missing interface." >&2
     exit 1
 fi
 
-if [ -z "$IP" ]; then
+if [ -z "$IFADDR" ]; then
     echo "Missing IP." >&2
     exit 1
 fi
 
 case $ACTION in
 leader)
-    sudo ip address add $IP dev $INTERFACE
+    sudo ip address add $IFADDR dev $INTERFACE
+
     for i in $(seq 5); do
-        sudo arping -c 1 -U -I $INTERFACE ${IP%%/*}
+        sudo arping -c 1 -U -I $INTERFACE ${IP}
         sleep 1
-        sudo arping -c 1 -A -I $INTERFACE ${IP%%/*}
+        sudo arping -c 1 -A -I $INTERFACE ${IP}
         sleep 1
     done
-    oneflow-server start
+
+    if which oneflow-server &>/dev/null &&
+        [ ! -e /var/run/one/oneflow.pid ];
+    then
+        oneflow-server start
+    fi
     ;;
 
 follower)
-    sudo ip address del $IP dev $INTERFACE
-    oneflow-server stop
+    if sudo ip address show dev $INTERFACE | grep -qi " ${IP}/"; then
+        sudo ip address del $IFADDR dev $INTERFACE
+    fi
+
+    if which oneflow-server &>/dev/null &&
+        [ -e /var/run/one/oneflow.pid ];
+    then
+        oneflow-server stop
+    fi
     ;;
 
 *)
