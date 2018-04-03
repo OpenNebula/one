a8756571204f4648fbb3a9cffbafd2950484e810
Tino Vazquez <tinova@opennebula.org>
2018-04-03 15:28:37 +0200

    Remove unneeded paths

--- src/docker_machine/generate.sh
+++ src/docker_machine/generate.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 GOCA_PATH=$PWD/../oca/go
 ADDON_PATH=$PWD
-BIN_LOCATION=/usr/share/one
 
 export GOPATH=$GOPATH:$GOCA_PATH:$ADDON_PATH
 
-cd src/docker_machine/
+cd src/docker_machine
 make build
