9ce0e3a8acee80886b5da158986a07c43581ba65
Ruben S. Montero <rsmontero@opennebula.systems>
2018-04-24 17:40:28 +0200

    B #2013: Removed duplicated header file

--- src/nebula/oned.cc
+++ src/nebula/oned.cc
@@ -15,7 +15,6 @@
 /* -------------------------------------------------------------------------- */
 
 #include <unistd.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
