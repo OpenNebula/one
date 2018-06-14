80585c9390e9fbd104b96451289677950c8057ef
Ruben S. Montero <rsmontero@opennebula.systems>
2018-06-14 11:14:16 +0200

    Feature #2159: Add support for RX queues for virt-io NICS (VIRTIO_QUEUES)

--- src/mad/sh/scripts_common.sh
+++ src/mad/sh/scripts_common.sh
@@ -928,6 +928,7 @@ function get_disk_information {
 # * MODEL
 # * IP
 # * FILTER
+# * VIRTIO_QUEUES
 # * VROUTER_IP
 # * INBOUND_AVG_BW
 # * INBOUND_PEAK_BW
@@ -960,6 +961,7 @@ function get_nic_information {
                         $NIC_XPATH/MODEL \
                         $NIC_XPATH/IP \
                         $NIC_XPATH/FILTER \
+                        $NIC_XPATH/VIRTIO_QUEUES \
                         $NIC_XPATH/VROUTER_IP \
                         $NIC_XPATH/INBOUND_AVG_BW \
                         $NIC_XPATH/INBOUND_PEAK_BW \
@@ -979,6 +981,7 @@ function get_nic_information {
     MODEL="${XPATH_ELEMENTS[j++]}"
     IP="${XPATH_ELEMENTS[j++]}"
     FILTER="${XPATH_ELEMENTS[j++]}"
+    VIRTIO_QUEUES="${XPATH_ELEMENTS[j++]}"
     VROUTER_IP="${XPATH_ELEMENTS[j++]}"
     INBOUND_AVG_BW="${XPATH_ELEMENTS[j++]}"
     INBOUND_PEAK_BW="${XPATH_ELEMENTS[j++]}"
