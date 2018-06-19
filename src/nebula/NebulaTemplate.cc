36ac4204e3b067152bc60318d90fbea27b4259e2
Ruben S. Montero <rsmontero@opennebula.systems>
2018-06-19 16:13:45 +0200

    Update NebulaTemplate.cc

--- src/nebula/NebulaTemplate.cc
+++ src/nebula/NebulaTemplate.cc
@@ -371,7 +371,7 @@ void OpenNebulaTemplate::set_conf_default()
 #  MONITORING_INTERVAL_HOST
 #  MONITORING_INTERVAL_VM
 #  MONITORING_INTERVAL_MARKET
-#  MONITORING_INTERVAL_IMAGE
+#  MONITORING_INTERVAL_DATASTORE
 #  MONITORING_THREADS
 #  HOST_PER_INTERVAL
 #  HOST_MONITORING_EXPIRATION_TIME
@@ -387,10 +387,10 @@ void OpenNebulaTemplate::set_conf_default()
 #*******************************************************************************
 */
     set_conf_single("MANAGER_TIMER", "15");
-    set_conf_single("MONITORING_INTERVAL_HOST", "60");
-    set_conf_single("MONITORING_INTERVAL_VM", "60");
-    set_conf_single("MONITORING_INTERVAL_MARKET", "60");
-    set_conf_single("MONITORING_INTERVAL_IMAGE", "60");
+    set_conf_single("MONITORING_INTERVAL_HOST", "180");
+    set_conf_single("MONITORING_INTERVAL_VM", "180");
+    set_conf_single("MONITORING_INTERVAL_MARKET", "600");
+    set_conf_single("MONITORING_INTERVAL_DATASTORE", "300");
     set_conf_single("MONITORING_THREADS", "50");
     set_conf_single("HOST_PER_INTERVAL", "15");
     set_conf_single("HOST_MONITORING_EXPIRATION_TIME", "43200");
