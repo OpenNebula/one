22230387dfe46a8c865a87510effd163d2a57889
joseangelgm <jagarrido@opennebula.systems>
2018-08-20 16:28:01 +0200

    F #2190 Onezone set should allow temporary zone changes

--- src/cli/one_helper/onezone_helper.rb
+++ src/cli/one_helper/onezone_helper.rb
@@ -83,25 +83,28 @@ class OneZoneHelper < OpenNebulaHelper::OneHelper
         table
     end
 
-    def set_zone(zone_id)
-      zone = factory(zone_id)
-      rc = zone.info
+    def set_zone(zone_id, temporary_zone)
+        zone = factory(zone_id)
+        rc = zone.info
 
-      if OpenNebula.is_error?(rc)
-        return -1, rc.message
-      end
+        if OpenNebula.is_error?(rc)
+            return -1, rc.message
+        end
 
-      if !zone['TEMPLATE/ENDPOINT']
-        return -1, "No Endpoint defined for Zone #{zone_id}"
-      end
+        if !zone['TEMPLATE/ENDPOINT']
+            return -1, "No Endpoint defined for Zone #{zone_id}"
+        end
 
-      File.open(ENV['HOME']+"/.one/one_endpoint", 'w'){|f|
-         f.puts zone['TEMPLATE/ENDPOINT']
-      }
-
-      puts "Endpoint changed to \"#{zone['TEMPLATE/ENDPOINT']}\" in " <<
-           "#{ENV['HOME']}/.one/one_endpoint"
-      return 0
+        if temporary_zone
+            puts "Type: export ONE_XMLRPC=#{zone['TEMPLATE/ENDPOINT']}"
+        else
+            File.open(ENV['HOME']+"/.one/one_endpoint", 'w'){|f|
+                f.puts zone['TEMPLATE/ENDPOINT']
+            }
+            puts "Endpoint changed to \"#{zone['TEMPLATE/ENDPOINT']}\" in " <<
+                "#{ENV['HOME']}/.one/one_endpoint"
+        end
+        return 0
     end
 
     private
