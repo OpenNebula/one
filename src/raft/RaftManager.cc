69e9bbfc8a8409714cfe32a23e6ca718f069219b
Ruben S. Montero <rsmontero@opennebula.systems>
2018-08-22 12:25:25 +0200

    Trigger replication when a follower is added to the zone

--- src/raft/RaftManager.cc
+++ src/raft/RaftManager.cc
@@ -329,6 +329,8 @@ void RaftManager::add_server(int follower_id, const std::string& endpoint)
 
 	heartbeat_manager.add_replica_thread(follower_id);
 
+    replica_manager.replicate(follower_id);
+
 	pthread_mutex_unlock(&mutex);
 };
 
