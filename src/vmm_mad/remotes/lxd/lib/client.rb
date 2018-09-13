af89f441784c0401a46f3c753f59de9cdb7c1ee9
dann1 <dann1telecom@gmail.com>
2018-09-13 11:07:55 -0500

    removed monkeypatch

--- src/vmm_mad/remotes/lxd/lib/client.rb
+++ src/vmm_mad/remotes/lxd/lib/client.rb
@@ -4,19 +4,6 @@ require 'net/http'
 require 'socket'
 require 'json'
 
-# Modified HTTPRequest class
-class Net::HTTPRequest
-  def initialize(path, initheader = { 'Host' => 'localhost' })
-    # api = '/1.0/' # Fix monkeypatch not working
-    # path = "#{api}#{path}"# Fix Monkeypatch not working
-
-    super self.class::METHOD,
-          self.class::REQUEST_HAS_BODY,
-          self.class::RESPONSE_HAS_BODY,
-          path, initheader
-  end
-end
-
 # Base requests to interact with LXD
 module LXDRequests
   attr_reader :api
@@ -24,10 +11,11 @@ module LXDRequests
   def initialize
     @sock = Net::BufferedIO.new(UNIXSocket.new('/var/lib/lxd/unix.socket'))
     @api = '/1.0/'
+    @header = { 'Host' => 'localhost' }
   end
 
   # Returns the HTTPResponse body as a hash
-  def get_response(request, data = nil)
+  def get_response(request, data)
     request.body = JSON.dump(data) unless data.nil?
     request.exec(@sock, '1.1', request.path)
     begin
@@ -37,28 +25,24 @@ module LXDRequests
     JSON.parse(response.body)
   end
 
-  def fix_uri(uri)# Fix monkeypatch not working
-    "#{@api}#{uri}"
+  def get(uri)
+    get_response(Net::HTTP::Get.new("#{@api}#{uri}", @header), data = nil)
   end
 
-  def get(uri)
-    uri = fix_uri(uri) # Fix monkeypatch not working
-    get_response(Net::HTTP::Get.new(uri))
+  def delete(uri)
+    get_response(Net::HTTP::Delete.new("#{@api}#{uri}", @header), data = nil)
   end
 
   def put(uri, data)
-    uri = fix_uri(uri) # Fix monkeypatch not working
-    get_response(Net::HTTP::Put.new(uri), data)
+    get_response(Net::HTTP::Put.new("#{@api}#{uri}", @header), data)
   end
 
   def post(uri, data)
-    uri = fix_uri(uri) # Fix monkeypatch not working
-    get_response(Net::HTTP::Post.new(uri), data)
+    get_response(Net::HTTP::Post.new("#{@api}#{uri}", @header), data)
   end
 
   def patch(uri, data)
-    uri = fix_uri(uri) # Fix monkeypatch not working
-    get_response(Net::HTTP::Patch.new(uri), data)
+    get_response(Net::HTTP::Patch.new("#{@api}#{uri}", @header), data)
   end
 end
 
@@ -73,34 +57,39 @@ class Client
   end
 
   # # Containers
-  # # Retrieve all containers.
-  # def all; end
+  # module Containers
+  #   # Retrieve all containers.
+  #   def all; end
 
-  # # Returns boolean indicating if the container exists.
-  # def exist(name); end
+  #   # Returns boolean indicating if the container exists.
+  #   def exist(name); end
 
-  # # Get a specific container, by its name
-  # def get(name); end
+  #   # Get a specific container, by its name
+  #   def get(name); end9
 
-  # # Create a new containerputs. This method requires the container config as the first parameter.
-  # def create(sock, uri)
-  #   request = Net::HTTP::Post.new("/1.0/#{uri}", initheader = { 'Host' => 'localhost' })
+  #   # Create a new containerputs. This method requires the container config as the first parameter.
+  #   def create(sock, uri)
+  #     request = Net::HTTP::Post.new("/1.0/#{uri}", initheader = { 'Host' => 'localhost' })
 
-  #   request.body = JSON.dump(
-  #     'name' => 'ruby',
-  #     'source' => {
-  #       'type' => 'none'
-  #     }
-  #   )
+  #     request.body = JSON.dump(
+  #       'name' => 'ruby',
+  #       'source' => {
+  #         'type' => 'none'
+  #       }
+  #     )
 
-  #   request.exec(sock, '1.1', "/1.0/#{uri}")
-  #   request
+  #     request.exec(sock, '1.1', "/1.0/#{uri}")
+  #     request
+  #   end
+
   # end
 
   # # Operations
-  # # Get a specific operation, by its id.
-  # def get(operation); end
+  # module Operations
+  #   # Get a specific operation, by its id.
+  #   def get(operation); end
 
-  # # get an operation, but wait until it is complete before returning the operation object.
-  # def wait_for(operation); end
+  #   # get an operation, but wait until it is complete before returning the operation object.
+  #   def wait_for(operation); end
+  # end
 end
