b63e9ec96301e3e922dc5bf865cf3c2f53c3bf44
Christian González <cgonzalez@opennebula.systems>
2018-09-25 12:10:27 +0200

    F #2253: Update 5.6.0 to 5.7.8.rb

--- src/onedb/local/5.6.0_to_5.7.8.rb
+++ src/onedb/local/5.6.0_to_5.7.8.rb
@@ -21,83 +21,95 @@ require 'pathname'
 require 'yaml'
 require 'opennebula'
 
-$: << File.dirname(__FILE__)
+$LOAD_PATH << File.dirname(__FILE__)
 
 include OpenNebula
 
 module Migrator
 
     def db_version
-        "5.6.0"
+        '5.7.8'
     end
 
     def one_version
-        "OpenNebula 5.6.0"
+        'OpenNebula 5.7.8'
     end
 
     def up
-        feature_2253()
+        feature_2253
 
-        return true
+        true
     end
 
     private
 
-    def feature_2253()
+    def feature_2253
+        @db.run 'DROP TABLE IF EXISTS old_network_pool;'
+        @db.run 'ALTER TABLE network_pool RENAME TO old_network_pool;'
+        create_table(:network_pool)
+
         @db.transaction do
             # update virtual networks
-            @db.fetch("SELECT * FROM network_pool") do |row|
-                doc = Nokogiri::XML(row[:body], nil, NOKOGIRI_ENCODING) {|c|
+            @db.fetch('SELECT * FROM old_network_pool') do |row|
+                doc = Nokogiri::XML(row[:body], nil, NOKOGIRI_ENCODING) do |c|
                     c.default_xml.noblanks
-                }
+                end
 
-                next unless doc.root.at_xpath("BRIDGE_TYPE").to_s.empty?
+                if doc.root.at_xpath('BRIDGE_TYPE').to_s.empty?
+                    vn_mad = doc.root.at_xpath('/VNET/VN_MAD').text
 
-                vn_mad = doc.root.at_xpath("/VNET/VN_MAD").text
+                    bridge_type = doc.create_element('BRIDGE_TYPE')
+                    bridge_type.add_child(bridge_type_by_vn_mad(vn_mad))
+                    doc.root.at_xpath('/VNET').add_child(bridge_type)
+                    row[:body] = doc.root.to_s
+                end
 
-                bridge_type = doc.create_element("BRIDGE_TYPE")
-                bridge_type.add_child(bridge_type_by_vn_mad(vn_mad))
-
-                doc.root.at_xpath("/VNET").add_child(bridge_type)
-
-                @db.run("UPDATE network_pool SET body = '#{doc.root.to_s}' "\
-                        "where oid = #{row[:oid]}")
+                @db[:network_pool].insert(row)
             end
         end
+
+        @db.run 'DROP TABLE old_network_pool;'
+
+        @db.run 'DROP TABLE IF EXISTS old_vm_pool;'
+        @db.run 'ALTER TABLE vm_pool RENAME TO old_vm_pool;'
+        create_table(:vm_pool)
 
         @db.transaction do
             # updates VM's nics
-            @db.fetch("SELECT * FROM vm_pool") do |row|
-                doc = Nokogiri::XML(row[:body], nil, NOKOGIRI_ENCODING) {|c|
+            @db.fetch('SELECT * FROM old_vm_pool') do |row|
+                doc = Nokogiri::XML(row[:body], nil, NOKOGIRI_ENCODING) do |c|
                     c.default_xml.noblanks
-                }
-
-                next if doc.root.at_xpath("TEMPLATE/NIC").to_s.empty?
-
-                doc.root.xpath("//NIC").map do |nic|
-                    next unless nic.xpath("BRIDGE_TYPE").to_s.empty?
-
-                    vn_mad = nic.xpath("VN_MAD").text
-
-                    bridge_type = doc.create_element("BRIDGE_TYPE")
-                    bridge_type.add_child(bridge_type_by_vn_mad(vn_mad))
-                    nic.add_child(bridge_type)
                 end
 
-                @db.run("UPDATE vm_pool SET body = '#{doc.root.to_s}' where" \
-                        "oid = #{row[:oid]}")
+                if !doc.root.at_xpath('TEMPLATE/NIC').to_s.empty?
+                    doc.root.xpath('//NIC').map do |nic|
+                        next unless nic.xpath('BRIDGE_TYPE').to_s.empty?
+
+                        vn_mad = nic.xpath('VN_MAD').text
+
+                        bridge_type = doc.create_element('BRIDGE_TYPE')
+                        bridge_type.add_child(bridge_type_by_vn_mad(vn_mad))
+                        nic.add_child(bridge_type)
+                    end
+
+                    row[:body] = doc.root.to_s
+                end
+
+                @db[:vm_pool].insert(row)
             end
         end
+
+        @db.run 'DROP TABLE old_vm_pool;'
     end
 
     def bridge_type_by_vn_mad(vn_mad)
-        #TODO CASE
-        if vn_mad == "vcenter"
-            return "vcenter_port_groups"
-        elsif vn_mad == "ovswitch" || vn_mad == "ovswitch_vxlan"
-            return "openvswitch"
+        case vn_mad
+        when 'vcenter'
+            return 'vcenter_port_groups'
+        when 'ovswitch', 'ovswitch_vxlan'
+            return 'openvswitch'
         else
-            return "linux"
+            return 'linux'
         end
     end
 
