1743a32b19df2c921dc250fa6662f86e7d0ba6a4
Ruben S. Montero <rsmontero@opennebula.systems>
2018-10-02 10:01:43 +0200

    F #1688: Fix bug with custom cpu mode
    (cherry picked from commit b226b829a87828d6eba93eced5a9455305995352)

--- src/vmm/LibVirtDriverKVM.cc
+++ src/vmm/LibVirtDriverKVM.cc
@@ -501,7 +501,7 @@ int LibVirtDriver::deployment_description_kvm(
         }
         else
         {
-            cpu_mode = "host-model";
+            cpu_mode = "custom";
         }
 
         //TODO #756 cache, feature
