28fedd7bb5b0ed0e9c76d116780b3947df915502
Ruben S. Montero <rsmontero@opennebula.systems>
2018-10-22 17:55:15 +0200

    F #2456: Regenerate VM attribute parsers

--- src/parsers/vm_file_var_syntax.h
+++ src/parsers/vm_file_var_syntax.h
@@ -1,8 +1,8 @@
-/* A Bison parser, made by GNU Bison 3.0.4.  */
+/* A Bison parser, made by GNU Bison 3.1.  */
 
 /* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -40,7 +40,7 @@
 extern int vm_file_var_debug;
 #endif
 /* "%code requires" blocks.  */
-#line 17 "vm_file_var_syntax.y" /* yacc.c:1909  */
+#line 17 "vm_file_var_syntax.y" /* yacc.c:1913  */
 
 #include <iostream>
 #include <vector>
@@ -63,7 +63,7 @@ typedef void * yyscan_t;
 int vm_file_var_parse (VirtualMachine * vm, vector<int> * img_ids,
     char ** errmsg, yyscan_t scanner);
 
-#line 67 "vm_file_var_syntax.hh" /* yacc.c:1909  */
+#line 67 "vm_file_var_syntax.hh" /* yacc.c:1913  */
 
 /* Token type.  */
 #ifndef YYTOKENTYPE
@@ -87,13 +87,13 @@ int vm_file_var_parse (VirtualMachine * vm, vector<int> * img_ids,
 
 union YYSTYPE
 {
-#line 205 "vm_file_var_syntax.y" /* yacc.c:1909  */
+#line 205 "vm_file_var_syntax.y" /* yacc.c:1913  */
 
     char * val_str;
     int    val_int;
     char   val_char;
 
-#line 97 "vm_file_var_syntax.hh" /* yacc.c:1909  */
+#line 97 "vm_file_var_syntax.hh" /* yacc.c:1913  */
 };
 
 typedef union YYSTYPE YYSTYPE;
