e6daa66e39bdb0592cab6de970c8dfab783b62db
Ruben S. Montero <rsmontero@opennebula.systems>
2019-02-25 10:42:41 +0100

    Sync fix for race condition on replica thread with master branch

--- src/raft/ReplicaThread.cc
+++ src/raft/ReplicaThread.cc
@@ -96,11 +96,6 @@ void ReplicaThread::do_replication()
 
             if ( pthread_cond_timedwait(&cond, &mutex, &timeout) == ETIMEDOUT )
             {
-                if ( _prending_requests == true )
-                {
-                    NebulaLog::log("RCM", Log::WARN, "Replication timeout with pending requests");
-                }
-
                 _pending_requests = retry_request || _pending_requests;
             }
 
