23893e23eecc7bd032b6423f5484fdf98401c6ed
Alejandro Huertas <ahuertas@opennebula.systems>
2019-03-18 12:47:27 +0100

    Development: fix bug with quotes

--- src/oca/ruby/opennebula/marketplaceapp.rb
+++ src/oca/ruby/opennebula/marketplaceapp.rb
@@ -192,8 +192,8 @@ module OpenNebula
             options[:vmtemplate_name] = name unless options[:vmtemplate_name]
 
             tmpl << "\n"
-            tmpl << 'NAME=\"' << name << '\"\n'
-            tmpl << 'FROM_APP=\"' << self['ID'] << '\"\n'
+            tmpl << "NAME=\"" << name << "\"\n"
+            tmpl << "FROM_APP=\"" << self['ID'] << "\"\n"
 
             case type_str
             when 'IMAGE'
