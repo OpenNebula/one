20fe05f41cd49e25cbde68e3b36c7e973a3ae197
Pierre Lafievre <pierre.lafievre@iguanesolutions.com>
2019-03-22 19:52:50 +0100

    F #2940: GOCA - add examples
    Signed-off-by: Pierre Lafievre <pierre.lafievre@iguanesolutions.com>

---
+++ src/oca/go/share/examples/vm_get_info.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/OpenNebula/one/src/oca/go/src/goca"
+)
+
+func main() {
+	client := goca.NewClient(
+		goca.NewConfig("", "", ""),
+	)
+	controller := goca.NewController(client)
+
+	// Get short informations of the VMs
+	vms, err := controller.VMs().Info()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; i < len(vms.VMs); i++ {
+		// This Info method, per VM instance, give us detailed informations on the instance
+		// Check xsd files to see the difference
+		vm, err := controller.VM(vms.VMs[i].ID).Info()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		//Do some others stuffs on vm
+		fmt.Printf("%+v\n", vm)
+	}
+}
