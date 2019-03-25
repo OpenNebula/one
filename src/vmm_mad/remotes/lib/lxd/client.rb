da1ab9df3b6866f8a56048aa344bc3aa951df692
Daniel Clavijo Coca <dclavijo@opennebula.systems>
2019-03-25 12:16:37 -0600

    Fix LXD wait for operation uri

--- src/vmm_mad/remotes/lib/lxd/client.rb
+++ src/vmm_mad/remotes/lib/lxd/client.rb
@@ -94,7 +94,7 @@ class LXDClient
     def wait(response, timeout)
         operation_id = response['operation'].split('/').last
 
-        timeout = "?timeout=#{timeout}" if [nil, ''].include?(timeout)
+        timeout = "?timeout=#{timeout}" unless [nil, ''].include?(timeout)
 
         response = get("operations/#{operation_id}/wait#{timeout}")
 
