624a035e3797ea8e6de9b5728ee708f7956c3e0e
Pierre Lafievre <pierre.lafievre@iguanesolutions.com>
2019-04-07 10:09:46 +0200

    F #2830: GOCA - rename xmlresource file
    Signed-off-by: Pierre Lafievre <pierre.lafievre@iguanesolutions.com>

--- src/oca/go/src/goca/parameters.go
+++ src/oca/go/src/goca/parameters.go
@@ -26,3 +26,23 @@ const (
 	// Merge to merge new template with existing one
 	Merge UpdateType = 1
 )
+
+// WhoPool is a parameter to pool info methods allowing to so some filtering
+//type WhoPool int
+
+const (
+	// PoolWhoPrimaryGroup resources belonging to the user’s primary group.
+	PoolWhoPrimaryGroup = -4
+
+	// PoolWhoMine to list resources that belong to the user that performs the
+	// query.
+	PoolWhoMine = -3
+
+	// PoolWhoAll to list all the resources seen by the user that performs the
+	// query.
+	PoolWhoAll = -2
+
+	// PoolWhoGroup to list all the resources that belong to the group that performs
+	// the query.
+	PoolWhoGroup = -1
+)
