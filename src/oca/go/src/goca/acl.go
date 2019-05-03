53259a3982eef1d6600f39c614595692ed14c349
Pierre Lafievre <pierre.lafievre@iguanesolutions.com>
2019-05-03 12:27:21 +0200

    F #3209: GOCA - Add ACL parameters constants
    Signed-off-by: Pierre Lafievre <pierre.lafievre@iguanesolutions.com>

--- src/oca/go/src/goca/acl.go
+++ src/oca/go/src/goca/acl.go
@@ -33,6 +33,47 @@ type ACLPool struct {
 	String   string `xml:"STRING"`
 }
 
+type AclUsers uint
+
+const (
+	UIDUsr     AclUsers = 0x100000000
+	GIDUsr     AclUsers = 0x200000000
+	AllUsr     AclUsers = 0x400000000
+	ClusterUsr AclUsers = 0x800000000
+)
+
+type AclResources uint
+
+const (
+	VMRsc             AclResources = 0x1000000000
+	HostRsc           AclResources = 0x2000000000
+	NetRsc            AclResources = 0x4000000000
+	ImageRsc          AclResources = 0x8000000000
+	UserRsc           AclResources = 0x10000000000
+	TemplateRsc       AclResources = 0x20000000000
+	GroupRsc          AclResources = 0x40000000000
+	DatastoreRsc      AclResources = 0x100000000000
+	ClusterRsc        AclResources = 0x200000000000
+	DocumentRsc       AclResources = 0x400000000000
+	ZoneRsc           AclResources = 0x800000000000
+	SecGroupRsc       AclResources = 0x1000000000000
+	VdcRsc            AclResources = 0x2000000000000
+	VRouterRsc        AclResources = 0x4000000000000
+	MarketPlaceRsc    AclResources = 0x8000000000000
+	MarketPlaceAppRsc AclResources = 0x10000000000000
+	VMGroupRsc        AclResources = 0x20000000000000
+	VNTemplateRsc     AclResources = 0x40000000000000
+)
+
+type AclRights uint
+
+const (
+	Use    AclRights = 0x1 // Auth. to use an object
+	Manage AclRights = 0x2 // Auth. to perform management actions
+	Admin  AclRights = 0x4 // Auth. to perform administrative actions
+	Create AclRights = 0x8 // Auth. to create an object
+)
+
 // ACLs returns a hosts controller.
 func (c *Controller) ACLs() *ACLsController {
 	return &ACLsController{c}
@@ -59,7 +100,7 @@ func (ac *ACLsController) Info() (*ACLPool, error) {
 // * user: User component of the new rule. A string containing a hex number.
 // * resource: Resource component of the new rule. A string containing a hex number.
 // * rights: Rights component of the new rule. A string containing a hex number.
-func (ac *ACLsController) CreateRule(user, resource, rights string) (uint, error) {
+func (ac *ACLsController) CreateRule(user AclUsers, resource AclResources, rights AclRights) (uint, error) {
 	response, err := ac.c.Client.Call("one.acl.addrule", user, resource, rights)
 	if err != nil {
 		return 0, err
