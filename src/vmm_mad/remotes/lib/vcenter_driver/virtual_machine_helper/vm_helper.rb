461c8602c79b935521d963424ae80a9bb38303a7
mouyaq <amoya@opennebula.systems>
2019-05-17 16:09:14 +0200

    F #3264: get out nic_model_class and state_to_c
    (cherry picked from commit 913b203b433cd20da2304a3fe16c835f53df1012)

---
+++ src/vmm_mad/remotes/lib/vcenter_driver/virtual_machine_helper/vm_helper.rb
@@ -0,0 +1,36 @@
+# -------------------------------------------------------------------------- #
+# Copyright 2002-2019, OpenNebula Project, OpenNebula Systems                #
+#                                                                            #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
+# not use this file except in compliance with the License. You may obtain    #
+# a copy of the License at                                                   #
+#                                                                            #
+# http://www.apache.org/licenses/LICENSE-2.0                                 #
+#                                                                            #
+# Unless required by applicable law or agreed to in writing, software        #
+# distributed under the License is distributed on an "AS IS" BASIS,          #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+# See the License for the specific language governing permissions and        #
+# limitations under the License.                                             #
+#--------------------------------------------------------------------------- #
+module VirtualMachineHelper
+
+    # Converts the VI string state to OpenNebula state convention
+    # Guest states are:
+    # - poweredOff   The virtual machine is currently powered off.
+    # - poweredOn    The virtual machine is currently powered on.
+    # - suspended    The virtual machine is currently suspended.
+    def state_to_c(state)
+        case state
+        when 'poweredOn'
+            VM_STATE[:active]
+        when 'suspended'
+            VM_STATE[:paused]
+        when 'poweredOff'
+            VM_STATE[:deleted]
+        else
+            VM_STATE[:unknown]
+        end
+    end
+
+end
