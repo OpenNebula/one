f6d766fa8ed78e547aaa40ba0240cec845520266
mouyaq <amoya@opennebula.systems>
2019-09-06 09:58:54 +0200

    F #3440: DFW - Initial function definition

---
+++ src/vmm_mad/remotes/lib/nsx_driver/dfw_nsxv.rb
@@ -0,0 +1,71 @@
+# -------------------------------------------------------------------------- #
+# Copyright 2002-2019, OpenNebula Project, OpenNebula Systems                #
+#                                                                            #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
+# not use this file except in compliance with the License. You may obtain    #
+# a copy of the License at                                                   #
+#                                                                            #
+# http://www.apache.org/licenses/LICENSE-2.0                                 #
+#                                                                            #
+# Unless required by applicable law or agreed to in writing, software        #
+# distributed under the License is distributed on an "AS IS" BASIS,          #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+# See the License for the specific language governing permissions and        #
+# limitations under the License.                                             #
+#--------------------------------------------------------------------------- #
+module NSXDriver
+
+    # Class Logical Switch
+    class DfwNsxv < NSXDriver::DistributedFirewall
+
+        # ATTRIBUTES
+        attr_reader :dfw_id
+        HEADER_XML = { :'Content-Type' => 'application/xml' }
+        SECTIONS = '/layer3sections'
+        SECTIONS_XPATH = '//sections'
+
+        # CONSTRUCTOR
+
+        def initialize(nsx_client)
+            super(nsx_client)
+            # Construct base URLs
+            @base_url = "#{@nsx_client.nsxmgr}/api/4.0/firewall/globalroot-0/config"
+            @url_sections = @base_url + SECTIONS
+        end
+
+        # Sections
+        # Get all sections
+        def sections
+            @nsx_client.get_xml(@base_url).xpath(SECTIONS_XPATH)
+        end
+
+        # Get section by id
+        def section_by_id(section_id)
+            url = @url_sections + '/' + section_id
+            @nsx_client.get_xml(url).xpath(SECTIONS_XPATH)
+        end
+
+        # Get section by name
+        def section_by_name(section_name)
+            url = @url_sections + '?name=' + section_name
+            @nsx_client.get_xml(url).xpath(SECTIONS_XPATH)
+        end
+
+        # Create new section
+        def create_section(section_name)
+            section_spec =
+                "<section name=\"#{section_name}\"\
+                stateless=\"false\" tcpStrict=\"true\" useSid=\"false\">\
+                </section>"
+            @nsx_client.post_xml(@url_sections, section_spec)
+        end
+
+        # Delete section
+        def delete_section(section_id)
+            url = @url_sections + '/' + section_id
+            @nsx_client.delete(url, HEADER_XML)
+        end
+
+    end
+
+end
---
+++ src/vmm_mad/remotes/lib/nsx_driver/distributed_firewall.rb
@@ -0,0 +1,65 @@
+# -------------------------------------------------------------------------- #
+# Copyright 2002-2019, OpenNebula Project, OpenNebula Systems                #
+#                                                                            #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
+# not use this file except in compliance with the License. You may obtain    #
+# a copy of the License at                                                   #
+#                                                                            #
+# http://www.apache.org/licenses/LICENSE-2.0                                 #
+#                                                                            #
+# Unless required by applicable law or agreed to in writing, software        #
+# distributed under the License is distributed on an "AS IS" BASIS,          #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+# See the License for the specific language governing permissions and        #
+# limitations under the License.                                             #
+#--------------------------------------------------------------------------- #
+module NSXDriver
+
+    # Class Logical Switch
+    class DistributedFirewall < NSXDriver::NsxComponent
+
+        # ATTRIBUTES
+
+        # CONSTRUCTOR
+        def initialize(nsx_client)
+            @nsx_client = nsx_client
+        end
+
+        # Sections
+        # Get all sections
+        def sections; end
+
+        # Get section by id
+        def section_by_id(section_id); end
+
+        # Get section by name
+        def section_by_name(section_name); end
+
+        # Create new section
+        def create_section(section_name); end
+
+        # Delete section
+        def delete_section(section_id); end
+
+        # Rules
+        # Get all rules
+        def rules; end
+
+        # Get rule by id
+        def rules_by_id; end
+
+        # Get rule by name
+        def rules_by_name; end
+
+        # Create new rule
+        def create_rule; end
+
+        # Update rule
+        def update_rule; end
+
+        # Delete rule
+        def delete_rule; end
+
+    end
+
+end
