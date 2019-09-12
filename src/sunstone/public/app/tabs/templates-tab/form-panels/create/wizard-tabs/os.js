145d65461879743f85a572e32e393085e00c9299
Jorge Lobo <jlobo@opennebula.systems>
2019-09-12 16:00:34 +0200

    B #3101: fix duplicate nics in OS boot
    Signed-off-by: Jorge Lobo <jlobo@opennebula.systems>

--- src/sunstone/public/app/tabs/templates-tab/form-panels/create/wizard-tabs/os.js
+++ src/sunstone/public/app/tabs/templates-tab/form-panels/create/wizard-tabs/os.js
@@ -134,6 +134,10 @@ define(function(require) {
     "winXPProGues"
   ];
 
+  var distinct = function(value, index, self){
+    return self.indexOf(value)===index;
+  };
+
   /*
     CONSTRUCTOR
    */
@@ -417,6 +421,7 @@ define(function(require) {
       if (!$.isArray(disks)){
         disks = [disks];
       }
+      disks = disks.filter(distinct);
 
       $.each(disks, function(i,disk){
         var label = "<i class=\"fas fa-fw fa-lg fa-server\"></i> ";
@@ -446,9 +451,6 @@ define(function(require) {
       if (!$.isArray(nics)){
         nics = [nics];
       }
-      var distinct = function(value, index, self){
-        return self.indexOf(value)===index;
-      };
       nics = nics.filter(distinct);
       nics.map(function(nic,i){
         var label = "<i class=\"fas fa-fw fa-lg fa-globe\"></i> ";
