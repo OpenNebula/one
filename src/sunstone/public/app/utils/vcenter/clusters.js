2cf8605fb92a7f7fa0b508f92b0c42b8afe8c0b4
Carlos J. Herrera Matos <cherrera@opennebula.systems>
2019-09-24 21:37:48 -0400

    F #1208: add support for sunstone, when import host
    Signed-off-by: Carlos J. Herrera Matos <cherrera@opennebula.systems>

--- src/sunstone/public/app/utils/vcenter/clusters.js
+++ src/sunstone/public/app/utils/vcenter/clusters.js
@@ -238,12 +238,19 @@ define(function(require) {
                 message : Locale.tr("Host created successfully. ID: %1$s", response.HOST.ID)
               });
               var template_raw =
-                "VCENTER_USER=\"" + that.opts.vcenter_user + "\"\n" +
-                "VCENTER_PASSWORD=\"" + that.opts.vcenter_password + "\"\n" +
-                "VCENTER_HOST=\"" + that.opts.vcenter_host + "\"\n" +
-                "VCENTER_INSTANCE_ID=\"" + vcenter_uuid + "\"\n" +
-                "VCENTER_CCR_REF=\"" + cluster_ref + "\"\n" +
-                "VCENTER_VERSION=\"" + vcenter_version + "\"\n";
+                  "VCENTER_USER=\"" + that.opts.vcenter_user + "\"\n" +
+                  "VCENTER_PASSWORD=\"" + that.opts.vcenter_password + "\"\n" +
+                  "VCENTER_INSTANCE_ID=\"" + vcenter_uuid + "\"\n" +
+                  "VCENTER_CCR_REF=\"" + cluster_ref + "\"\n" +
+                  "VCENTER_VERSION=\"" + vcenter_version + "\"\n";
+
+              if (that.opts.vcenter_host.split(":").length === 2) {
+                template_raw += "VCENTER_HOST=\"" + that.opts.vcenter_host.split(":")[0] + "\"\n";
+                template_raw += "VCENTER_PORT=\"" + that.opts.vcenter_host.split(":")[1] + "\"\n";
+              } else {
+                template_raw += "VCENTER_HOST=\"" + that.opts.vcenter_host + "\"\n";
+              }
+
               Sunstone.runAction("Host.update_template", response.HOST.ID, template_raw);
             },
             error: function (request, error_json) {
