6fb508496990370c9c1edb562e2e304482b6f86b
Christian González <cgonzalez@opennebula.systems>
2019-09-26 09:57:43 +0200

    M #-: Fix GOCA bug

--- src/oca/go/src/goca/user.go
+++ src/oca/go/src/goca/user.go
@@ -131,7 +131,12 @@ func (uc *UserController) Passwd(password string) error {
 // * timeSeconds: Valid period in seconds; 0 reset the token and -1 for a non-expiring token.
 // * effectiveGID: Effective GID to use with this token. To use the current GID and user groups set it to -1
 func (uc *UserController) Login(token string, timeSeconds int, effectiveGID int) error {
-	_, err := uc.c.Client.Call("one.user.login", uc.Name, token, timeSeconds, effectiveGID)
+	user, err := uc.Info(false)
+
+	if err != nil {
+		return err
+	}
+	_, err = uc.c.Client.Call("one.user.login", user.Name, token, timeSeconds, effectiveGID)
 	return err
 }
 
