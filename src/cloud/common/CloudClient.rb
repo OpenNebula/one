09adfe97e80cbdcf1faedddbf1a90e4b5d2a4bc6
Vlastimil Holer <vholer@opennebula.systems>
2019-11-11 18:49:42 +0100

    M #-: Bump version to 5.10.0 (#3932)

--- src/cloud/common/CloudClient.rb
+++ src/cloud/common/CloudClient.rb
@@ -51,7 +51,7 @@ end
 module CloudClient
 
     # OpenNebula version
-    VERSION = '5.9.90'
+    VERSION = '5.10.0'
 
     # #########################################################################
     # Default location for the authentication file
