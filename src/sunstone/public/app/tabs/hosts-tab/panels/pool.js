3e6e615454c867f7c9d7c098287700c3a94dd5eb
Carlos Herrera <cherrera@opennebula.systems>
2020-01-09 07:52:04 -0500

    F #1269: verify hypervisor
    Signed-off-by: Carlos Herrera <cherrera@opennebula.systems>

--- src/sunstone/public/app/tabs/hosts-tab/panels/pool.js
+++ src/sunstone/public/app/tabs/hosts-tab/panels/pool.js
@@ -13,63 +13,52 @@
 /* See the License for the specific language governing permissions and        */
 /* limitations under the License.                                             */
 /* -------------------------------------------------------------------------- */
-
 define(function(require) {
   /*
     DEPENDENCIES
    */
-
   require('datatables.net');
   require('datatables.foundation');
   var Locale = require('utils/locale');
   var CPUBars = require('../utils/cpu-bars');
   var MemoryBars = require('../utils/memory-bars');
-
   /*
     TEMPLATES
    */
-
-
+  var TemplatePool = require('hbs!./pool/html');
   /*
     CONSTANTS
    */
-
   var PANEL_ID = require('./pool/panelId');
   var RESOURCE = "Host"
-
   /*
     CONSTRUCTOR
    */
-
   function Panel(info) {
     this.title = Locale.tr("ESX");
     this.icon = "fa-hdd";
-
     this.element = info[RESOURCE.toUpperCase()];
-
     // Do not create an instance of this panel if no vcenter hypervisor
-    if (this.element.TEMPLATE.HYPERVISOR != "vcenter") {
+    // porque el hypervisor proviene de la monitorizacion de por si no esta en el template
+    if(this.element && this.element.TEMPLATE && this.element.TEMPLATE.HYPERVISOR){
+      if(this.element.TEMPLATE.HYPERVISOR != "vcenter"){
+        throw "Panel not available for this element";
+      }
+    }else{
       throw "Panel not available for this element";
     }
-
     return this;
   };
-
   Panel.PANEL_ID = PANEL_ID;
   Panel.prototype.html = _html;
   Panel.prototype.setup = _setup;
-
   return Panel;
-
   /*
     FUNCTION DEFINITIONS
    */
-
   function _html() {
-
+    return TemplatePool();
   }
-
   function _setup(context) {
-
   }
 })
