c80a0a95538729d7a800d45a8e609fe3c43f34b1
Anton Todorov <atodorov-storpool@users.noreply.github.com>
2020-02-26 23:01:46 +0200

    B #4269: revert escapeDoubleQuotes change
    in src/sunstone/public/app/utils/wizard-fields.js
    to restore doube quotes escape in `SCHED_REQUIREMENTS`
    and `SCHED_DS_REQUIREMENTS`
    
    Signed-off-by: Anton Todorov (a.todorov@storpool.com)

--- src/sunstone/public/app/utils/wizard-fields.js
+++ src/sunstone/public/app/utils/wizard-fields.js
@@ -52,10 +52,8 @@ define(function(require) {
     return templateJSON;
   }
 
-  function _retrieveInput(value) {
-    return (value instanceof $)
-      ? value.val()
-      : TemplateUtils.escapeDoubleQuotes( value );
+  function _retrieveInput(input) {
+    return TemplateUtils.escapeDoubleQuotes( input.val() );
   }
 
   // TODO: wizard_field_64 for fill method
