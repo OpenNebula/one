470b6986285b97f7a4b3897ee1f13a9548bf086b
Daniel Clavijo Coca <dclavijo@opennebula.systems>
2020-03-03 09:17:03 -0600

    WIP flow client

---
+++ src/oca/go/src/flow/httpclient/client.go
@@ -0,0 +1,89 @@
+package httpclient
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Auth struct {
+	User string
+	Pass string
+	Oned string
+}
+
+// HTTP METHODS
+
+// Get http
+func Get(url, user, password string) *http.Response {
+	return httpReq(url, user, password, "GET", nil)
+}
+
+// Delete http
+func Delete(url, user, password string) *http.Response {
+	return httpReq(url, user, password, "DELETE", nil)
+}
+
+// Post http
+func Post(url string, user string, password string, message map[string]interface{}) *http.Response {
+	return httpReq(url, user, password, "POST", message)
+}
+
+// Put http
+func Put(url string, user string, password string, message map[string]interface{}) *http.Response {
+	return httpReq(url, user, password, "PUT", message)
+}
+
+// General http request method for the client. Uses auth by default
+func httpReq(url string, user string, password string, method string, message map[string]interface{}) *http.Response {
+	req, err := http.NewRequest(method, url, bodyContent(message))
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	req.SetBasicAuth(user, password)
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+
+	resp, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return resp
+}
+
+// HELPERS
+
+// Btomap returns http body as map
+func Btomap(resp *http.Response) map[string]interface{} {
+	var result map[string]interface{}
+
+	json.NewDecoder(resp.Body).Decode(&result)
+
+	return result
+}
+
+// Btostr returns http body as string
+func Btostr(response *http.Response) string {
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return string(body)
+}
+
+// BodyContent prepares map for put/post http requests
+func bodyContent(message map[string]interface{}) *bytes.Buffer {
+	represent, err := json.Marshal(message)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return bytes.NewBuffer(represent)
+}
---
+++ src/oca/go/src/flow/template/template.go
@@ -0,0 +1,65 @@
+package template
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/oneflow-rest/httpclient"
+)
+
+var endpoint string
+
+func init() {
+	endpoint = "service_template"
+}
+
+// List the contents of the SERVICE_TEMPLATE collection.
+func List(class httpclient.Auth) *http.Response {
+	url := fmt.Sprintf("%s/%s", class.Oned, endpoint)
+
+	return httpclient.Get(url, class.User, class.Pass)
+}
+
+// Show the SERVICE_TEMPLATE resource identified by <id>
+func Show(class httpclient.Auth, id int) *http.Response {
+	url := fmt.Sprintf("%s/%s/%s", class.Oned, endpoint, strconv.Itoa(id))
+
+	return httpclient.Get(url, class.User, class.Pass)
+}
+
+// Delete the SERVICE_TEMPLATE resource identified by <id>
+func Delete(class httpclient.Auth, id int) *http.Response {
+	url := fmt.Sprintf("%s/%s/%s", class.Oned, endpoint, strconv.Itoa(id))
+
+	return httpclient.Delete(url, class.User, class.Pass)
+}
+
+// Create a new SERVICE_TEMPLATE resource.
+func Create(body map[string]interface{}) {
+
+}
+
+// Update the SERVICE_TEMPLATE resource identified by <id>.
+func Update() {
+
+}
+
+// Permissions
+
+func Chown(id int) {
+
+}
+
+func Chwgrp(id int) {
+
+}
+
+func Chwmod(id int) {
+
+}
+
+// Perform an action on the SERVICE_TEMPLATE resource identified by <id>. Available actions: instantiate, chown, chgrp, chmod
+func action(id int, action string) {
+
+}
