d6ac6508ef00da6536db0cd28b11de9631bd25a4
Pavel Czerný <pczerny@opennebula.systems>
2020-03-26 19:21:16 +0100

    B #4417: Check VM_*_OPERATIONS attribute is correct (#4438)

--- include/Group.h
+++ include/Group.h
@@ -148,6 +148,13 @@ public:
         return vm_actions.get_auth_op(action);
     }
 
+protected:
+    /* Checks the validity of template attributes
+     *    @param error string describing the error if any
+     *    @return 0 on success
+     */
+    int post_update_template(string& error) override;
+
 private:
 
     // -------------------------------------------------------------------------
