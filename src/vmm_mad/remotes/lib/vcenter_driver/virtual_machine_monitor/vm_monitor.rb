591472fbb8ac3e3badc9f91e2e3603fe09b29e36
Tino Vazquez <cvazquez@opennebula.io>
2020-04-06 18:37:19 +0200

    L #-: Linting for vm_monitor.rb
    vCenter driver

--- src/vmm_mad/remotes/lib/vcenter_driver/virtual_machine_monitor/vm_monitor.rb
+++ src/vmm_mad/remotes/lib/vcenter_driver/virtual_machine_monitor/vm_monitor.rb
@@ -467,10 +467,11 @@ module VirtualMachineMonitor
         str_info << 'VCENTER_RP_NAME="' << rp_name << '" '
 
         info_disks.each do |disk|
-            str_info << "DISK_#{disk[0]}_ACTUAL_PATH=\"[" << disk[1].ds.name << "] " << disk[1].path << "\" "
+            str_info << "DISK_#{disk[0]}_ACTUAL_PATH=\"[" <<
+                disk[1].ds.name << '] ' << disk[1].path << '" '
         end
 
-        return str_info
+        str_info
     end
 
     def reset_monitor
