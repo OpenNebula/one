29f549f9419c60354b1329b4b2fb43f5d6143757
Daniel Clavijo Coca <dclavijo@opennebula.systems>
2020-04-07 09:20:17 -0500

    F #4298: Update functions with go client

--- src/oca/go/src/flow/service/service.go
+++ src/oca/go/src/flow/service/service.go
@@ -15,24 +15,24 @@ func init() {
 }
 
 // List the contents of the SERVICE collection.
-func List(class httpclient.Auth) *http.Response {
-	url := fmt.Sprintf("%s/%s", class.Oned, endpoint)
+func List(client httpclient.Client) *http.Response {
+	url := fmt.Sprintf("%s/%s", client.Address, endpoint)
 
-	return httpclient.Get(url, class.User, class.Pass)
+	return httpclient.Get(client, url)
 }
 
 // Show the SERVICE resource identified by <id>
-func Show(class httpclient.Auth, id int) *http.Response {
-	url := fmt.Sprintf("%s/%s/%s", class.Oned, endpoint, strconv.Itoa(id))
+func Show(client httpclient.Client, id int) *http.Response {
+	url := fmt.Sprintf("%s/%s/%s", client.Address, endpoint, strconv.Itoa(id))
 
-	return httpclient.Get(url, class.User, class.Pass)
+	return httpclient.Get(client, url)
 }
 
 // Delete the SERVICE resource identified by <id>
-func Delete(class httpclient.Auth, id int) *http.Response {
-	url := fmt.Sprintf("%s/%s/%s", class.Oned, endpoint, strconv.Itoa(id))
+func Delete(client httpclient.Client, id int) *http.Response {
+	url := fmt.Sprintf("%s/%s/%s", client.Address, endpoint, strconv.Itoa(id))
 
-	return httpclient.Delete(url, class.User, class.Pass)
+	return httpclient.Delete(client, url)
 }
 
 // Update the ROLE identified by <name> of the SERVICE resource identified by <id>. Currently the only attribute that can be updated is the cardinality.
@@ -66,10 +66,10 @@ func Chwmod(id int) {
 
 // TODO: how to pass chmod/chrgrp/chow bits/id-name/id-name
 // Perform an action on the SERVICE resource identified by <id>. Available actions: shutdown, recover, chown, chgrp, chmod
-func action(class httpclient.Auth, id int, action string) *http.Response {
-	url := fmt.Sprintf("%s/%s/%s/%s", class.Oned, endpoint, strconv.Itoa(id), action)
+func action(client httpclient.Client, id int, action string) *http.Response {
+	url := fmt.Sprintf("%s/%s/%s/%s", client.Address, endpoint, strconv.Itoa(id), action)
 
-	return httpclient.Delete(url, class.User, class.Pass)
+	return httpclient.Delete(client, url)
 }
 
 // Perform an action on all the Virtual Machines belonging to the ROLE identified by <name> of the SERVICE resource identified by <id>. Available actions: shutdown, shutdown-hard, undeploy, undeploy-hard, hold, release, stop, suspend, resume, boot, delete, delete-recreate, reboot, reboot-hard, poweroff, poweroff-hard, snapshot-create
--- src/oca/go/src/flow/test/main.go
+++ src/oca/go/src/flow/test/main.go
@@ -8,11 +8,7 @@ import (
 
 func main() {
 
-	client := httpclient.Client{
-		User:    "oneadmin",
-		Pass:    "opennebula",
-		Address: "http://10.10.0.56:2474",
-	}
+	client := httpclient.New("oneadmin", "opennebula", "http://10.10.0.56:2474")
 
 	body := httpclient.Get(client, "service")
 
