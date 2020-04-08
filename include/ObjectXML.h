37f10c8618389ccd42a283ff89028e85812b1cb7
Abdullahaml1 <abdullah_aml@protonmail.com>
2020-04-08 23:05:40 +0200

    B #4503: checking nodestval before dereferencing
    Signed-off-by: Abdullahaml1 <abdullah_aml@protonmail.com>

--- include/ObjectXML.h
+++ include/ObjectXML.h
@@ -79,6 +79,10 @@ public:
                 break;
 
             case XPATH_NODESET:
+                // countering the case of not found ex: /VM_POOL/VM[ID=-1]/"something"
+                if (obj->nodesetval == 0)
+                    return;
+
                 for (int i = 0; i < obj->nodesetval->nodeNr ; ++i)
                 {
                     cur = obj->nodesetval->nodeTab[i];
