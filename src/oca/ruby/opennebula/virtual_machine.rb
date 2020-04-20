8f0f2e29392d87151b9e06fcd1d6fe6d9f7045cb
Christian González <cgonzalez@opennebula.systems>
2020-04-20 11:05:55 +0200

    M #-: Minor fix at save_as (#4554)
    (cherry picked from commit 78557ae30250a3215ed360d63387d41164e6ef63)

--- src/oca/ruby/opennebula/virtual_machine.rb
+++ src/oca/ruby/opennebula/virtual_machine.rb
@@ -780,7 +780,7 @@ module OpenNebula
         # @return [Integer, OpenNebula::Error] the new Template ID in case of
         #   success, error otherwise
         REMOVE_VNET_ATTRS = %w{AR_ID BRIDGE CLUSTER_ID IP MAC TARGET NIC_ID
-            NETWORK_ID VN_MAD SECURITY_GROUPS VLAN_ID}
+            VN_MAD SECURITY_GROUPS VLAN_ID}
 
         REMOVE_IMAGE_ATTRS = %w{DEV_PREFIX SOURCE ORIGINAL_SIZE SIZE
             DISK_SNAPSHOT_TOTAL_SIZE DRIVER IMAGE_STATE SAVE CLONE READONLY
@@ -916,6 +916,10 @@ module OpenNebula
 
                     REMOVE_VNET_ATTRS.each do |attr|
                         nic.delete_element(attr)
+                    end
+
+                    if !nic['NETWORK_NAME'].empty? && !nic['NETNETWORK_UNAME'].empty?
+                        nic.delete_element('NETWORK_ID')
                     end
 
                     replace << "NIC = [ " << nic.template_like_str(".").tr("\n", ",\n") << " ] \n"
