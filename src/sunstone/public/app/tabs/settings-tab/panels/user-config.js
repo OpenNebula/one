fb1294b386b105c54dba5113eda3d2ede49839f2
Dennis Felsch <dennis.felsch@ruhr-uni-bochum.de>
2020-04-27 18:43:45 +0200

    * F #962: Sunstone 2FA with WebAuthn
    This strengthens the login with e.g. U2F/FIDO2 authentication keys.
    
    Signed-off-by: Dennis Felsch <dennis.felsch@ruhr-uni-bochum.de>
    (cherry picked from commit 487a6247a9b59954ed1c6cb3cd8b43acfba74fb0)

--- src/sunstone/public/app/tabs/settings-tab/panels/user-config.js
+++ src/sunstone/public/app/tabs/settings-tab/panels/user-config.js
@@ -98,11 +98,7 @@ define(function(require) {
 
     $("#provision_user_views_select option[value=\"" + config["user_config"]["default_view"] + "\"]", context).attr("selected", "selected");
 
-    if (that.element.TEMPLATE.SUNSTONE && that.element.TEMPLATE.SUNSTONE.TWO_FACTOR_AUTH_SECRET) {
-      $(".provision_two_factor_auth_button", context).html(Locale.tr("Disable"));
-    } else {
-      $(".provision_two_factor_auth_button", context).html(Locale.tr("Manage two factor authentication"));
-    }
+    $(".provision_two_factor_auth_button", context).html(Locale.tr("Manage two factor authentication"));
 
     // Login token button
     context.off("click", ".provision_login_token_button");
@@ -116,23 +112,14 @@ define(function(require) {
     context.off("click", ".provision_two_factor_auth_button");
     context.on("click", ".provision_two_factor_auth_button", function(){
       var sunstone_setting = that.element.TEMPLATE.SUNSTONE || {};
-      if (sunstone_setting.TWO_FACTOR_AUTH_SECRET) {
-        Sunstone.runAction(
-          "User.disable_sunstone_two_factor_auth",
-          that.element.ID,
-          {current_sunstone_setting: sunstone_setting}
-        );
-      } else {
-        Sunstone.getDialog(TWO_FACTOR_AUTH_DIALOG_ID).setParams({
-          element: that.element, 
-          sunstone_setting: sunstone_setting
-        });
-        Sunstone.getDialog(TWO_FACTOR_AUTH_DIALOG_ID).reset();
-        Sunstone.getDialog(TWO_FACTOR_AUTH_DIALOG_ID).show();
-      }
+      Sunstone.getDialog(TWO_FACTOR_AUTH_DIALOG_ID).setParams({
+        element: that.element,
+        sunstone_setting: sunstone_setting
+      });
+      Sunstone.getDialog(TWO_FACTOR_AUTH_DIALOG_ID).reset();
+      Sunstone.getDialog(TWO_FACTOR_AUTH_DIALOG_ID).show();
     });
   
-
     $("#provision_change_password_form").submit(function() {
       var pw = $("#provision_new_password", this).val();
       var confirm_password = $("#provision_new_confirm_password", this).val();
