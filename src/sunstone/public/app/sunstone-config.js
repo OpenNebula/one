99f347916e1a3cb7234c11277e9cb2f1399afff2
Frederick <fborges@opennebula.io>
2020-04-27 17:17:18 +0200

    F #4068: Dashboard Thresholds

--- src/sunstone/public/app/sunstone-config.js
+++ src/sunstone/public/app/sunstone-config.js
@@ -168,6 +168,11 @@ define(function(require) {
 
     "allTabs": function() {
       return Object.keys(_config["view"]["tabs"]);
+    },
+    "thresholds":{
+      "min":_config["user_config"]["threshold_min"],
+      "low":_config["user_config"]["threshold_low"],
+      "high":_config["user_config"]["threshold_high"]
     }
   };
 
