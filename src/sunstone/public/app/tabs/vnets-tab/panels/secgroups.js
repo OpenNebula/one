f9c4f58c1b093d06e1c31c7266976201b4939349
Frederick <fborges@opennebula.io>
2020-05-18 11:20:15 +0200

    F #4453: Add Sec. Groups to VNets

--- src/sunstone/public/app/tabs/vnets-tab/panels/secgroups.js
+++ src/sunstone/public/app/tabs/vnets-tab/panels/secgroups.js
@@ -21,16 +21,22 @@ define(function(require) {
 
   var Locale = require('utils/locale');
   var SecurityGroupsTable = require('tabs/secgroups-tab/datatable');
+  var SecGroupsTemplate = require('hbs!./secgroups/html');
+  var Sunstone = require('sunstone');
+  var Config = require('sunstone-config');
+  var TemplateUtils = require("utils/template-utils");
 
   /*
     CONSTANTS
    */
-
+  var TAB_ID = require('../tabId');
   var PANEL_ID = require('./secgroups/panelId');
   var SG_TABLE_ID = PANEL_ID + "SecurityGroupsTable";
   var RESOURCE = "Network";
   var XML_ROOT = "VNET";
 
+  var ADD_SECGROUPS_DIALOG_ID = require('../dialogs/add-secgroups/dialogId');
+  var CONFIRM_DIALOG_ID = require('utils/dialogs/generic-confirm/dialogId');
   /*
     CONSTRUCTOR
    */
@@ -74,12 +80,115 @@ define(function(require) {
 
     this.secgroupTable = new SecurityGroupsTable(SG_TABLE_ID, opts);
 
-    return this.secgroupTable.dataTableHTML;
+    return SecGroupsTemplate(
+      {
+        'element': this.element,
+        'tab': "vnets-tab",
+        'action_add': "Network.add_secgroup",
+        'action_rm': "Network.remove_secgroup",
+        'dataTableHTML': this.secgroupTable.dataTableHTML
+      }
+    );
   }
 
   function _setup(context) {
-    this.secgroupTable.initialize();
-    this.secgroupTable.refreshResourceTableSelect();
+    var that = this;
+    $("#selected_ids_row_vnet_sg_list_tabSecurityGroupsTable",context).hide();
+    
+    if (that.secgroupTable){
+      that.secgroupTable.initialize();
+      that.secgroupTable.refreshResourceTableSelect();
+    }
+
+    var secGroupDatatable = $("#vnet_sg_list_tabSecurityGroupsTable", context).dataTable({
+      "bSortClasses" : false,
+      "bDeferRender": true,
+      "aoColumnDefs": [
+        //{ "bSortable": false, "aTargets": [3,4] },
+      ]
+    });
+
+    secGroupDatatable.off("click", 'tbody tr');
+    secGroupDatatable.on("click", 'tbody tr', function(e){
+      var aData = secGroupDatatable.fnGetData(this);
+      if (!aData) return true;
+      var id = aData[1];
+      if (!id) return true;
+      var name = aData[2];
+      if (!name) return true;
+
+      if(that.last_selected_row_secgroup) {
+        that.last_selected_row_secgroup.children().each(function(){
+          $(this).removeClass('markrowchecked');
+        });
+      }
+
+      that.last_selected_row_secgroup = $(this);
+      $(this).children().each(function(){
+        $(this).addClass('markrowchecked');
+      });
+
+      $("#rm_secgroup_button", context).attr("secgroups_id", id).attr("secgroups_name", name).removeAttr('disabled');
+
+      return false;
+    });
+    
+    if (Config.isTabActionEnabled("vnets-tab", "Network.remove_secgroup")) {
+      context.off("click", 'button#rm_secgroup_button');
+      context.on("click", 'button#rm_secgroup_button', function(){
+        var secgroups_id = $(this).attr('secgroups_id');
+        var secgroups_name = $(this).attr('secgroups_name');
+        Sunstone.getDialog(CONFIRM_DIALOG_ID).setParams({
+          //header :
+          headerTabId: TAB_ID,
+          body : Locale.tr("This will remove the security group") + ' "' + secgroups_name + '"', 
+          //question :
+          submit : function(){
+            var current_security_group = that.element.TEMPLATE.SECURITY_GROUPS.split(",");
+            var new_security_groups = [];
+            // Get current values of VNet.
+            var network_json ={        
+              "DESCRIPTION": that.element.TEMPLATE.DESCRIPTION,
+              "BRIDGE": that.element.TEMPLATE.BRIDGE,
+              "VN_MAD": that.element.TEMPLATE.VN_MAD,
+              "NETWORK_ADDRESS": that.element.TEMPLATE.NETWORK_ADDRESS,
+              "GATEWAY": that.element.TEMPLATE.GATEWAY,
+              "DNS": that.element.TEMPLATE.DNS,
+              "BRIDGE_TYPE": that.element.TEMPLATE.BRIDGE_TYPE
+            }
+
+            for (var i = 0; i < current_security_group.length; i++) {
+              var security_group = current_security_group[i];
+              if (security_group != secgroups_id){
+                new_security_groups.push(security_group);
+              }
+            }
+            
+            network_json["SECURITY_GROUPS"] = new_security_groups.join(",");
+            Sunstone.runAction('Network.rm_secgroup',that.element.ID,TemplateUtils.templateToString(network_json));
+
+            return false;
+          }
+        });
+
+        Sunstone.getDialog(CONFIRM_DIALOG_ID).reset();
+        Sunstone.getDialog(CONFIRM_DIALOG_ID).show();
+
+        return false;
+      });
+    }
+
+    if (Config.isTabActionEnabled("vnets-tab", "Network.add_secgroup")) {
+      context.off("click", 'button#add_secgroup_button');
+      context.on("click", 'button#add_secgroup_button', function(){
+        var id = that.element.ID;
+
+        Sunstone.getDialog(ADD_SECGROUPS_DIALOG_ID).setId(id);
+        Sunstone.getDialog(ADD_SECGROUPS_DIALOG_ID).show();
+
+        return false;
+      });
+    }
 
     return false;
   }
