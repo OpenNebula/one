61f623ea314e0bca54aa2520b9ecdb572669afb7
Vlastimil Holer <vholer@opennebula.io>
2020-06-02 17:41:56 +0200

    M #-: Bump version to 5.12.0

--- src/cloud/common/CloudClient.rb
+++ src/cloud/common/CloudClient.rb
@@ -51,7 +51,7 @@ end
 module CloudClient
 
     # OpenNebula version
-    VERSION = '5.11.90'
+    VERSION = '5.12.0'
 
     # #########################################################################
     # Default location for the authentication file
--- src/sunstone/sunstone-server.rb
+++ src/sunstone/sunstone-server.rb
@@ -274,7 +274,7 @@ SUPPORT = {
     :author_name => "OpenNebula Support Team",
     :support_subscription => "https://opennebula.io/support/",
     :account => "https://opennebula.io/buy-support",
-    :docs => "https://docs.opennebula.io/5.11/",
+    :docs => "https://docs.opennebula.io/5.12/",
     :community => "https://opennebula.io/usec",
     :project => "OpenNebula"
 }
