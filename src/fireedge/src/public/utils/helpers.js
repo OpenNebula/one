33f999473cea6321924fa235401efcd505d8a86a
Sergio Betanzos <sbetanzos@opennebula.io>
2020-09-09 10:25:09 +0200

    F #3951: Remove debounce function (#193)

--- src/fireedge/src/public/utils/helpers.js
+++ src/fireedge/src/public/utils/helpers.js
@@ -1,18 +1,1 @@
 export const fakeDelay = ms => new Promise(resolve => setTimeout(resolve, ms));
-
-export const debounce = (func, delay, immediate) => {
-  let timerId;
-  return (...args) => {
-    const boundFunc = func.bind(this, ...args);
-    clearTimeout(timerId);
-    if (immediate && !timerId) {
-      boundFunc();
-    }
-    const calleeFunc = immediate
-      ? () => {
-          timerId = null;
-        }
-      : boundFunc;
-    timerId = setTimeout(calleeFunc, delay);
-  };
-};
