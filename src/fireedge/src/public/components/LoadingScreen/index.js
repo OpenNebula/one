4848ef5f11d4b8cdd99ecc95f9bc4e08a105b973
Sergio Betanzos <sbetanzos@opennebula.io>
2020-09-11 14:44:38 +0200

    F #3951: Fix loading screen (#209)

--- src/fireedge/src/public/components/LoadingScreen/index.js
+++ src/fireedge/src/public/components/LoadingScreen/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
-import { styled, Box } from '@material-ui/core';
+import { Box } from '@material-ui/core';
 import Logo from 'client/icons/logo';
 
-const ScreenBox = styled(Box)({
-  width: '100%',
-  height: '100vh',
-  backgroundColor: '#ffffff',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  position: 'fixed',
-  zIndex: 10000
-});
-
 const LoadingScreen = () => (
-  <ScreenBox>
+  <Box
+    style={{
+      width: '100%',
+      height: '100vh',
+      backgroundColor: '#ffffff',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      position: 'fixed',
+      zIndex: 10000
+    }}
+  >
     <Logo width={360} height={360} spinner withText />
-  </ScreenBox>
+  </Box>
 );
 
 export default LoadingScreen;
