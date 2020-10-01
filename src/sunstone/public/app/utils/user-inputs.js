53300a13f509b21333e622a8a1d1e99bbbc5cfd6
Sergio Betanzos <sbetanzos@opennebula.io>
2020-10-01 12:09:29 +0200

    B #5107: Fix boolean user inputs (#274)

--- src/sunstone/public/app/utils/user-inputs.js
+++ src/sunstone/public/app/utils/user-inputs.js
@@ -854,7 +854,7 @@ define(function(require) {
         var id = UniqueId.id();
         if(value == "YES"){
           input = "<br>" + Locale.tr("YES ") + "<input style='margin-right: 20px' checked type='radio' name='bool_" +id + "' value='YES'" + wizard_field + " " + required + "/>";
-          input += Locale.tr("NO ") + "<input type='radio' name='bool_" + id + "\" value='NO'" + wizard_field + " " + required + "/>";
+          input += Locale.tr("NO ") + "<input type='radio' name='bool_" + id + "' value='NO'" + wizard_field + " " + required + "/>";
         } else if(value == "NO"){
           input = "<br>" + Locale.tr("YES ") + "<input style='margin-right: 20px' type='radio' name='bool_" + id + "' value='YES'" + wizard_field + " " + required + "/>";
           input += Locale.tr("NO ") + "<input checked type='radio' name='bool_" + id + "' value='NO'" + wizard_field + " " + required + "/>"
