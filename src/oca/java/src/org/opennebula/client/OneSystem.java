c3ac63b7e3172dd5edffc132ff913b57364cd8f2
Vlastimil Holer <vholer@opennebula.io>
2020-10-14 17:34:41 +0200

    M #-: Bump version to 5.12.0.4

--- src/oca/java/src/org/opennebula/client/OneSystem.java
+++ src/oca/java/src/org/opennebula/client/OneSystem.java
@@ -32,7 +32,7 @@ public class OneSystem
     private static final String GROUP_QUOTA_INFO    = "groupquota.info";
     private static final String GROUP_QUOTA_UPDATE  = "groupquota.update";
 
-    public static final String VERSION = "5.12.0.3";
+    public static final String VERSION = "5.12.0.4";
 
     public OneSystem(Client client)
     {
