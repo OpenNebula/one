6d4810de496a3be753dacd211a6f021fbd427ca1
Jan Orel <jorel@opennebula.systems>
2020-11-20 15:34:39 +0100

    M #-: Don't use unpack1 (#457)

--- src/vmm_mad/exec/one_vmm_exec.rb
+++ src/vmm_mad/exec/one_vmm_exec.rb
@@ -697,7 +697,7 @@ class ExecDriver < VirtualMachineDriver
         target_xpath = "VM/TEMPLATE/DISK[ATTACH='YES']/TARGET"
         target = ensure_xpath(xml_data, id, action, target_xpath) || return
 
-        target_index = target.downcase[-1..-1].unpack1('c') - 97
+        target_index = target.downcase[-1..-1].unpack('c').first - 97
 
         action = VmmAction.new(self, id, :attach_disk, drv_message)
 
@@ -744,7 +744,7 @@ class ExecDriver < VirtualMachineDriver
         target_xpath = "VM/TEMPLATE/DISK[ATTACH='YES']/TARGET"
         target = ensure_xpath(xml_data, id, action, target_xpath) || return
 
-        target_index = target.downcase[-1..-1].unpack1('c') - 97
+        target_index = target.downcase[-1..-1].unpack('c').first - 97
 
         action = VmmAction.new(self, id, :detach_disk, drv_message)
 
@@ -1361,7 +1361,7 @@ end
 #
 ################################################################################
 
-ENV_CONF = Hash.new("").merge!(ENV)
+ENV_CONF = Hash.new('').merge!(ENV)
 LIVE_DISK_SNAPSHOTS = ENV_CONF['LIVE_DISK_SNAPSHOTS'].split
 VNMAD_LOCAL_ACTIONS = ENV_CONF['VNMAD_LOCAL_ACTIONS'].split
 
