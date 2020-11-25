c039e4fb32e037e1541b7f6a907b0e4e952e18dd
Sergio Betanzos <sbetanzos@opennebula.io>
2020-11-25 16:47:11 +0100

    F #3951: Fix requests provisions (#478)

--- src/fireedge/src/client/services/provision/provision.js
+++ src/fireedge/src/client/services/provision/provision.js
@@ -54,7 +54,10 @@ export const createProvision = ({ data = {} }) =>
     method: POST,
     error: err => err?.message
   }).then(res => {
-    if (!res?.id || res?.id !== httpCodes.ok.id) throw res
+    if (!res?.id || res?.id !== httpCodes.ok.id) {
+      if (res?.id === httpCodes.accepted.id) return res
+      throw res
+    }
 
     return res?.data ?? {}
   })
@@ -64,7 +67,10 @@ export const deleteProvision = ({ id }) =>
     method: DELETE,
     error: err => err?.message
   }).then(res => {
-    if (!res?.id || res?.id !== httpCodes.ok.id) throw res
+    if (!res?.id || res?.id !== httpCodes.ok.id) {
+      if (res?.id === httpCodes.accepted.id) return res
+      throw res
+    }
 
     return res?.data ?? {}
   })
