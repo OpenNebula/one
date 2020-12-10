7c748da499e3e0993753ca2c1fbe94242db47b90
Sergio Betanzos <sbetanzos@opennebula.io>
2020-12-10 13:06:52 +0100

    F #3951: Fix warning in provider form (#538)

--- src/fireedge/src/client/containers/Providers/Form/Create/Steps/Connection/index.js
+++ src/fireedge/src/client/containers/Providers/Form/Create/Steps/Connection/index.js
@@ -24,15 +24,24 @@ const Connection = () => ({
   content: useCallback(() => {
     const [fields, setFields] = useState([])
     const { providersTemplates } = useProvision()
-    const { watch } = useFormContext()
+    const { watch, reset } = useFormContext()
 
     useEffect(() => {
-      const { [PROVIDER_ID]: provider } = watch()
+      const {
+        [PROVIDER_ID]: provider,
+        [STEP_ID]: currentConnections
+      } = watch()
       const providerTemplate = providersTemplates
         .find(({ name }) => name === provider?.[0])
 
       connection = providerTemplate?.connection ?? {}
       setFields(FORM_FIELDS(connection))
+
+      // set defaults connection values when first render
+      !currentConnections && reset({
+        ...watch(),
+        [STEP_ID]: STEP_FORM_SCHEMA(connection).default()
+      })
     }, [])
 
     return (fields?.length === 0) ? (
