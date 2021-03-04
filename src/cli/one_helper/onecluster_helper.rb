c1c4fd12a1755d2b06d9ae82ad9480a02566783b
Игорь <67561108+none0nfg@users.noreply.github.com>
2021-03-04 12:46:16 +0300

    Update onecluster_helper.rb
    Added description of cluster resources
    
    (cherry picked from commit e48be29da1e3e6a8921d2a8fe0fd1b5c63aa0633)

--- src/cli/one_helper/onecluster_helper.rb
+++ src/cli/one_helper/onecluster_helper.rb
@@ -102,13 +102,38 @@ class OneClusterHelper < OpenNebulaHelper::OneHelper
         CLIHelper.print_header(str_h1 % "CLUSTER #{cluster['ID']} INFORMATION")
         puts str % ["ID",   cluster.id.to_s]
         puts str % ["NAME", cluster.name]
+        
         puts
+        CLIHelper.print_header(str_h1 % "CLUSTER RESOURCES", false)
+        cluster.info!
+        total_cpu = 0
+        used_cpu = 0
+        total_ram = 0
+        used_ram = 0
+        
+        hosts = cluster.to_hash['CLUSTER']['HOSTS']['ID']
+        hosts.each do |h|
+          h = OpenNebula::Host.new_with_id(h, @client)
+          h.info!
+          h = h.to_hash
+          total_cpu += h['HOST']['HOST_SHARE']['TOTAL_CPU'].to_i/100
+          used_cpu += h['HOST']['HOST_SHARE']['CPU_USAGE'].to_i/100
+          total_ram += h['HOST']['HOST_SHARE']['TOTAL_MEM'].to_i/1024/1024
+          used_ram += h['HOST']['HOST_SHARE']['MEM_USAGE'].to_i/1024/1024
+        end
+        puts "TOTAL CPUs: "+total_cpu.to_s
+        puts "OCCUPIED CPUs: "+used_cpu.to_s
+        puts "AVAILABLE CPUs: "+(total_cpu - used_cpu).to_s         
+        puts
+        puts "TOTAL RAM: "+total_ram.to_s
+        puts "OCCUPIED RAM: "+used_ram.to_s
+        puts "AVAILABLE RAM: "+(total_ram - used_ram).to_s
 
+        puts
         CLIHelper.print_header(str_h1 % "CLUSTER TEMPLATE", false)
         puts cluster.template_str
 
-        puts
-
+        puts        
         CLIHelper.print_header("%-15s" % ["HOSTS"])
         cluster.host_ids.each do |id|
             puts "%-15s" % [id]
