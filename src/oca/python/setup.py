710cb6182931397336c2058a5dd88970a39a14ba
Vlastimil Holer <vholer@opennebula.systems>
2021-04-07 11:29:43 +0200

    M #-: Bump version to 6.0.1 (#1075)

--- src/oca/python/setup.py
+++ src/oca/python/setup.py
@@ -40,7 +40,7 @@ install_requires = [
 if sys.version_info[0] < 3:
     install_requires.append('future')
 
-version = '6.0.0'
+version = '6.0.1'
 
 # mark pre-release
 v1 = int(version.split('.')[1])
--- src/onecfg/lib/version.rb
+++ src/onecfg/lib/version.rb
@@ -16,6 +16,6 @@
 
 module OneCfg
 
-    VERSION = '6.0.0'
+    VERSION = '6.0.1'
 
 end
