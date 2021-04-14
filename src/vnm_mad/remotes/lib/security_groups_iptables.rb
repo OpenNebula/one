4b4a09c08bde5bf3cf575280566f4b422acf5a17
Jan Orel <jorel@opennebula.systems>
2021-04-14 15:33:28 +0200

    M #-: SG for NIC_ALIAS (#1103)

--- src/vnm_mad/remotes/lib/security_groups_iptables.rb
+++ src/vnm_mad/remotes/lib/security_groups_iptables.rb
@@ -391,7 +391,11 @@ module SGIPTables
         end
 
         if bridged
-            nri = "#{base_br} --physdev-out #{nic[:tap]} -j #{chain_in}"
+            if nic[:alias_id]
+                nri = "#{base_br} --physdev-out #{nic[:parent_nic][:tap]} -d #{nic[:ip]} -j #{chain_in}"
+            else
+                nri = "#{base_br} --physdev-out #{nic[:tap]} -j #{chain_in}"
+            end
         else
             nri = "-I #{GLOBAL_CHAIN} -d #{nic[:ip]} -j #{chain_in}"
         end
@@ -434,7 +438,7 @@ module SGIPTables
             "-j RETURN"
 
         # Mac-spofing
-        if nic[:filter_mac_spoofing] == "YES"
+        if nic[:filter_mac_spoofing] == "YES" && nic[:alias_id].nil?
             commands.add :iptables, "-A #{chain_out} -m mac ! "\
                 "--mac-source #{nic[:mac]} -j DROP"
             commands.add :ip6tables, "-A #{chain_out} -m mac ! "\
