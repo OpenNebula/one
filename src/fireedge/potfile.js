869fba8ecfa948773954a3592dbd53e9c170aae6
Jorge Lobo <jlobo@opennebula.systems>
2021-07-14 17:18:58 +0200

    F #5422: comment in webpack files
    Signed-off-by: Jorge Lobo <jlobo@opennebula.io>

--- src/fireedge/potfile.js
+++ src/fireedge/potfile.js
@@ -1,17 +1,18 @@
-/* Copyright 2002-2021, OpenNebula Project, OpenNebula Systems                */
-/*                                                                            */
-/* Licensed under the Apache License, Version 2.0 (the "License"); you may    */
-/* not use this file except in compliance with the License. You may obtain    */
-/* a copy of the License at                                                   */
-/*                                                                            */
-/* http://www.apache.org/licenses/LICENSE-2.0                                 */
-/*                                                                            */
-/* Unless required by applicable law or agreed to in writing, software        */
-/* distributed under the License is distributed on an "AS IS" BASIS,          */
-/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   */
-/* See the License for the specific language governing permissions and        */
-/* limitations under the License.                                             */
-/* -------------------------------------------------------------------------- */
+/* ------------------------------------------------------------------------- *
+ * Copyright 2002-2021, OpenNebula Project, OpenNebula Systems               *
+ *                                                                           *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may   *
+ * not use this file except in compliance with the License. You may obtain   *
+ * a copy of the License at                                                  *
+ *                                                                           *
+ * http://www.apache.org/licenses/LICENSE-2.0                                *
+ *                                                                           *
+ * Unless required by applicable law or agreed to in writing, software       *
+ * distributed under the License is distributed on an "AS IS" BASIS,         *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ * See the License for the specific language governing permissions and       *
+ * limitations under the License.                                            *
+ * ------------------------------------------------------------------------- */
 
 const { createReadStream, generateFile } = require('opennebula-generatepotfile')
 const clientConstants = require('./src/client/constants/translates')
@@ -20,12 +21,12 @@ const clientCode = './src/client'
 const exportFile = `${clientCode}/assets/languages/messages.pot`
 
 /**
-  if the constants have an object indicator. where are the definitions you must place it
-
-  example:
-  - indicator: T.Cluster
-  - definition: {T: {Cluster: "Cluster"}} (e)
-*/
+ * if the constants have an object indicator. where are the definitions you must place it
+ *
+ * example:
+ * - indicator: T.Cluster
+ * - definition: {T: {Cluster: "Cluster"}} (e)
+ */
 
 const definitions = { T: { ...clientConstants } }
 
--- src/fireedge/webpack.config.prod.client.js
+++ src/fireedge/webpack.config.prod.client.js
@@ -1,17 +1,18 @@
-/* Copyright 2002-2021, OpenNebula Project, OpenNebula Systems                */
-/*                                                                            */
-/* Licensed under the Apache License, Version 2.0 (the "License"); you may    */
-/* not use this file except in compliance with the License. You may obtain    */
-/* a copy of the License at                                                   */
-/*                                                                            */
-/* http://www.apache.org/licenses/LICENSE-2.0                                 */
-/*                                                                            */
-/* Unless required by applicable law or agreed to in writing, software        */
-/* distributed under the License is distributed on an "AS IS" BASIS,          */
-/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   */
-/* See the License for the specific language governing permissions and        */
-/* limitations under the License.                                             */
-/* -------------------------------------------------------------------------- */
+/* ------------------------------------------------------------------------- *
+ * Copyright 2002-2021, OpenNebula Project, OpenNebula Systems               *
+ *                                                                           *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may   *
+ * not use this file except in compliance with the License. You may obtain   *
+ * a copy of the License at                                                  *
+ *                                                                           *
+ * http://www.apache.org/licenses/LICENSE-2.0                                *
+ *                                                                           *
+ * Unless required by applicable law or agreed to in writing, software       *
+ * distributed under the License is distributed on an "AS IS" BASIS,         *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ * See the License for the specific language governing permissions and       *
+ * limitations under the License.                                            *
+ * ------------------------------------------------------------------------- */
 
 const path = require('path')
 const webpack = require('webpack')
@@ -27,9 +28,10 @@ const js = {
 }
 
 /**
- * bundle app
- * @param {object} {assets, appname}
- * @returns {object} webpack config
+ * Bundle app.
+ *
+ * @param {{ assets: boolean, name: string }} params - webpack build app
+ * @returns {webpack.Configuration} webpack config
  */
 const bundle = ({ assets = false, name = 'sunstone' }) => {
   const plugins = [
@@ -78,6 +80,9 @@ const bundle = ({ assets = false, name = 'sunstone' }) => {
   }
 }
 
+/**
+ * @returns {webpack.Configuration[]} - list of configuration
+ */
 module.exports = () => Object.entries(defaultApps).map(([key, values]) =>
   bundle({ ...values, name: key })
 )
