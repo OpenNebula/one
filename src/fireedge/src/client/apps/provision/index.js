3f9e4436ea207933a09974a2e5b7795c6e0a1005
Jorge Miguel Lobo Escalona <47326048+jloboescalona2@users.noreply.github.com>
2021-10-01 11:10:21 +0200

    M #-: Add Core-js in fireedge (#1499)

--- src/fireedge/src/client/apps/provision/index.js
+++ src/fireedge/src/client/apps/provision/index.js
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and       *
  * limitations under the License.                                            *
  * ------------------------------------------------------------------------- */
+import 'core-js'
 import { JSXElementConstructor } from 'react'
 import PropTypes from 'prop-types'
 
