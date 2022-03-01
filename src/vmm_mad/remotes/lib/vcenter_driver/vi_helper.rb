0dd55399a50f04f73a962e0347068672b3cce552
Tino Vazquez <cvazquez@opennebula.io>
2022-03-01 18:53:28 +0100

    M #-: Decrypt Virtual Machine
    so we have the proper descrypted password for context info

--- src/vmm_mad/remotes/lib/vcenter_driver/vi_helper.rb
+++ src/vmm_mad/remotes/lib/vcenter_driver/vi_helper.rb
@@ -97,7 +97,12 @@ module VCenterDriver
 
         def self.one_item(the_class, id, exit_if_fail = true)
             item = the_class.new_with_id(id, client)
-            rc = item.info
+            rc=nil
+            if the_class == OpenNebula::VirtualMachine
+                rc = item.info(true)
+            else
+                rc = item.info
+            end
             return_if_error(rc, item, exit_if_fail)
         end
 
