5b988776484b03e8f7c470a91e20b8bc903a3eb7
Sergio Betanzos <sbetanzos@opennebula.io>
2022-03-03 10:50:25 +0100

    F #5757: Improve capacity range on VM instantiation (#1824)

--- src/sunstone/public/app/tabs/templates-tab/form-panels/create/wizard-tabs/general/capacity-inputs.js
+++ src/sunstone/public/app/tabs/templates-tab/form-panels/create/wizard-tabs/general/capacity-inputs.js
@@ -110,6 +110,10 @@ define(function(require) {
       attr.min = 0.01;
     }
 
+    if (['range', 'range-float'].includes(attr.type)) {
+      attr.readonly = true
+    }
+
     input = UserInputs.attributeInput(attr);
 
     if (attr.type != "range-float"){
@@ -130,6 +134,10 @@ define(function(require) {
 
     if(attr.min == undefined){
       attr.min = 1;
+    }
+
+    if (['range', 'range-float'].includes(attr.type)) {
+      attr.readonly = true
     }
 
     input = UserInputs.attributeInput(attr);
@@ -236,6 +244,14 @@ define(function(require) {
       attr.initial = element.TEMPLATE.MEMORY;
     } else {
       attr.mandatory = true;
+    }
+
+    if (String(element.TEMPLATE.HYPERVISOR).toLowerCase() === "vcenter") {
+      attr.step = 4;
+    }
+
+    if (['range', 'range-float'].includes(attr.type)) {
+      attr.readonly = true
     }
 
     if(attr.min == undefined){
