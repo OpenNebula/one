b6421dff85ea58595b63f0a011c0a01626bf37db
Tino Vazquez <cvazquez@opennebula.io>
2022-04-18 19:44:46 +0200

    M #-: Bump version 6.9.80

--- src/oca/python/setup.py
+++ src/oca/python/setup.py
@@ -40,7 +40,7 @@ install_requires = [
 if sys.version_info[0] < 3:
     install_requires.append('future')
 
-version = '6.3.85'
+version = '6.9.80'
 
 # mark pre-release
 v1 = int(version.split('.')[1])
--- src/oca/ruby/opennebula.rb
+++ src/oca/ruby/opennebula.rb
@@ -77,5 +77,5 @@ require 'opennebula/flow'
 module OpenNebula
 
     # OpenNebula version
-    VERSION = '6.3.85'
+    VERSION = '6.9.80'
 end
