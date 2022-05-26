3b9d74e59e308a28adbfb8ea09d38d4ab2a65915
Alejandro Huertas Herrero <ahuertas@opennebula.io>
2022-05-26 19:43:41 +0200

    B #5846: fix KVM wild VMs monitoring (#2094)

--- src/im_mad/remotes/kvm-probes.d/host/system/wild_vm.rb
+++ src/im_mad/remotes/kvm-probes.d/host/system/wild_vm.rb
@@ -5,7 +5,7 @@ require_relative '../../../lib/kvm'
 KVM.load_conf
 
 begin
-    puts DomainList.info
+    puts DomainList.wilds_info
 rescue StandardError => e
     STDERR.puts e.message
     exit(-1)
