c1be05cfb9a460a122872cf667e0a15a7855c9b7
Ruben S. Montero <rsmontero@opennebula.org>
2022-07-03 11:55:49 +0200

    F #5890: Initial support of not up-to-date hosts
    * Adjust :ip[6]tables commands to work with legacy versions. Prior to
      version 1.6.1 -W option is not supported (e.g. ubuntu 16.04)
    * Check sudo access for vgpu commands. Added in 6.4.0 release
    
    co-authoted-by: Christian González <cgonzalez@opennebula.io>

--- src/vnm_mad/remotes/lib/command.rb
+++ src/vnm_mad/remotes/lib/command.rb
@@ -37,6 +37,16 @@ module VNMMAD
             :ipset    => 'sudo -n ipset'
         }
 
+        # Adjust :ip[6]tables commands to work with legacy version
+        stdout = Open3.capture3('iptables --version')[0]
+        regex = /.*v(?<version>\d+.\d+.\d+)/
+        iptables_version = Gem::Version.new(stdout.match(regex)[:version])
+
+        if Gem::Version.new('1.6.1') > iptables_version
+            COMMANDS[:iptables]  = 'sudo -n iptables -w 3'
+            COMMANDS[:ip6tables] = 'sudo -n ip6tables -w 3'
+        end
+
         # Represents an Array of commands to be executed by the networking
         # drivers # The commands
         class Commands < Array
