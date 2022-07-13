dbf1314ae2d65207334e9a295982108315a53b6d
Tino Vazquez <cvazquez@opennebula.io>
2022-07-13 12:00:49 +0200

    M #-: Bump version 6.4.2

--- src/oca/ruby/opennebula.rb
+++ src/oca/ruby/opennebula.rb
@@ -77,5 +77,5 @@ require 'opennebula/flow'
 module OpenNebula
 
     # OpenNebula version
-    VERSION = '6.4.1'
+    VERSION = '6.4.2'
 end
--- src/onecfg/lib/version.rb
+++ src/onecfg/lib/version.rb
@@ -16,6 +16,6 @@
 
 module OneCfg
 
-    VERSION = '6.4.1'
+    VERSION = '6.4.2'
 
 end
