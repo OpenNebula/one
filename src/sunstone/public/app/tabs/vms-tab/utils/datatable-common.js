5c56e5bfc0d8dace7308500566b58113ae5d1248
Frederick Borges <fborges@opennebula.io>
2023-02-20 16:03:23 +0100

    B OpenNebula/one#6144: Fix scheduled action leases bug (#2512)
    preventing to show VM information
    
    (cherry picked from commit 5e80b63356136b0a12fa7d626ff4e771d1c4a54e)

--- src/sunstone/public/app/tabs/vms-tab/utils/datatable-common.js
+++ src/sunstone/public/app/tabs/vms-tab/utils/datatable-common.js
@@ -173,7 +173,7 @@ define(function(require) {
           !isNaN(parseInt(leases[action.ACTION].time)) &&
           leases[action.ACTION].color
         ){
-          var endTime = (action.TIME.startsWith("+")? action.TIME : action.TIME - startTime).toString();
+          var endTime = (action.TIME.startsWith("+")? action.TIME : action.TIME - element.STIME).toString();
 
           if(checkTime(element.STIME, endTime)){
             rtn = $("<i/>",{class:"describeCharter fa fa-clock",data_start:element.STIME, data_add:endTime, data_action:action.ACTION}).css({"position":"relative","color":leases[action.ACTION].color});
