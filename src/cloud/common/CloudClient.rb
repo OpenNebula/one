8e1da2e730a98ae5f55a0a4175c7bc598c434ad9
Tino Vázquez <cvazquez@opennebula.io>
2023-03-07 10:16:03 +0100

    M #-: Bump version 6.6.1

--- src/cloud/common/CloudClient.rb
+++ src/cloud/common/CloudClient.rb
@@ -51,7 +51,7 @@ end
 module CloudClient
 
     # OpenNebula version
-    VERSION = '6.6.0'
+    VERSION = '6.6.1'
 
     # #########################################################################
     # Default location for the authentication file
