f1b9ad7473c6b66d85053a7c211a35e6cc3ab8f7
Jan Orel <jorel@opennebula.systems>
2023-03-09 10:29:50 +0100

    M #-: Improve conf_branch_protect (#2549)
    (cherry picked from commit 33af6695ce5d9ce47807d18f88f485c73b0d7947)

--- share/smoke_tests/tests/01-conf_branch_protect.sh
+++ share/smoke_tests/tests/01-conf_branch_protect.sh
@@ -1,40 +1,57 @@
 #!/bin/bash -xv
 
-# **previous** refers to the code base in the main repository
-# **current**  refers to the code base of the PR (or the new commit)
+# GITHUB_BASE_REF refers to the target branch from the PR
+BRANCH="$GITHUB_BASE_REF"
 
-if [[ $TRAVIS_BRANCH =~ (^one-) ]]; then
-    export PREVIOUS_ONE=~/previous.one
-    export CURRENT_ONE=$TRAVIS_BUILD_DIR
-    export PREVIOUS_ONE_INSTALL=~/previous.install
-    export CURRENT_ONE_INSTALL=~/current.install
-
-    # Checkout previous code base
-    git clone https://github.com/OpenNebula/one $PREVIOUS_ONE
-    (cd $PREVIOUS_ONE ; git checkout $TRAVIS_BRANCH)
-    # In a PR we compare with the head of $TRAVIS_BRANCH
-    # Otherwise, we want to check with the previous commit
-    if [[ $TRAVIS_PULL_REQUEST == false ]]; then
-        (cd $PREVIOUS_ONE ; git checkout HEAD~1)
-    fi
-
-    # Debug information
-    echo "Previous commit:"
-    (cd $PREVIOUS_ONE ; git --no-pager log -1)
-    echo "----------------"
-    echo "Current commit:"
-    (cd $CURRENT_ONE ; git --no-pager log -1)
-    echo "----------------"
-
-    # Install previous and current code base
-    (cd $PREVIOUS_ONE ; ./install.sh -d $PREVIOUS_ONE_INSTALL &> /dev/null)
-    (cd $CURRENT_ONE  ; ./install.sh -d $CURRENT_ONE_INSTALL &> /dev/null)
-
-    echo "Testing conf changes:"
-    diff -r $PREVIOUS_ONE_INSTALL/etc $CURRENT_ONE_INSTALL/etc
-    rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-    diff -r $PREVIOUS_ONE_INSTALL/var/remotes/etc $CURRENT_ONE_INSTALL/var/remotes/etc
-    rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+# or GITHUB_REF_NAME refers to the target branch from PUSH
+if [ -z "$BRANCH" ]; then
+    BRANCH="$GITHUB_REF_NAME"
 fi
 
-exit 0
+if [ -z "$BRANCH" ]; then
+    echo "\$GITHUB_REF_NAME and \$GITHUB_BASE_REF empty"
+    exit 0
+fi
+
+# only if target is stable branch like one-X.Y
+if [[ $BRANCH =~ (^one-[0-9]*\.[0-9]*$) ]]; then
+
+    PREVIOUS_ONE_INSTALL=~/previous.install
+    mkdir $PREVIOUS_ONE_INSTALL
+    CURRENT_ONE_INSTALL=~/current.install
+    mkdir $CURRENT_ONE_INSTALL
+
+    FIRST_RELEASE=$(echo "$BRANCH" | sed -r 's/^one-([0-9]+\.[0-9]+)/release-\1.0/')
+
+    cd "$GITHUB_WORKSPACE"
+    git fetch --tags origin --quiet
+
+    # if already exists release-X.Y.0 branch
+    if git rev-list ${FIRST_RELEASE}.. --quiet; then
+        ORIGINAL_HEAD=$(git rev-parse HEAD)
+
+        # Always restore original HEAD for following some tests
+        trap "git reset --hard $ORIGINAL_HEAD"  EXIT
+
+        # reset and install first stable release
+        git reset --hard $FIRST_RELEASE
+        echo "First release $FIRST_RELEASE commit:"
+        git --no-pager log -1
+        ./install.sh -d $PREVIOUS_ONE_INSTALL &>/dev/null
+
+        # reset and install current
+        git reset --hard $GITHUB_SHA
+        echo "Current commit $GITHUB_SHA:"
+        git --no-pager log -1
+        ./install.sh -d $CURRENT_ONE_INSTALL &>/dev/null
+
+        echo "Testing conf changes:"
+        diff -r $PREVIOUS_ONE_INSTALL/etc $CURRENT_ONE_INSTALL/etc
+        rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+        diff -r $PREVIOUS_ONE_INSTALL/var/remotes/etc $CURRENT_ONE_INSTALL/var/remotes/etc
+        rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+    else
+        echo "Tag $FIRST_RELEASE not found, considering safe"
+    fi
+fi
