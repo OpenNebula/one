551e3800cbb8aa6fefac8d69ff5309827086a12f
Pavel Czerny <pczerny@opennebula.io>
2023-09-11 09:17:22 +0200

    F #6311: Refector MarketPlace(Apps) tests to use gocheck

--- src/oca/go/src/goca/marketplaceapp_test.go
+++ src/oca/go/src/goca/marketplaceapp_test.go
@@ -17,41 +17,48 @@
 package goca
 
 import (
-	"strconv"
-	"testing"
-
 	"github.com/OpenNebula/one/src/oca/go/src/goca/schemas/image"
 	imgkeys "github.com/OpenNebula/one/src/oca/go/src/goca/schemas/image/keys"
 	"github.com/OpenNebula/one/src/oca/go/src/goca/schemas/marketplace"
 	mktkeys "github.com/OpenNebula/one/src/oca/go/src/goca/schemas/marketplace/keys"
 	mktapp "github.com/OpenNebula/one/src/oca/go/src/goca/schemas/marketplaceapp"
 	"github.com/OpenNebula/one/src/oca/go/src/goca/schemas/marketplaceapp/keys"
+	"github.com/OpenNebula/one/src/oca/go/src/goca/schemas/shared"
+	"github.com/OpenNebula/one/src/oca/go/src/goca/parameters"
+	. "gopkg.in/check.v1"
 )
 
-func TestMarketplaceApp(t *testing.T) {
-	var mkt_app_name string = "new_mkt_app"
-	var mkt_app *mktapp.MarketPlaceApp
-	var mkt_img_id int
-	var market_id int
-	var err error
+type MarketPlaceAppSuite struct {
+	marketID    int
+	imgID       int
+	appID       int
+	appName     string
+}
 
-	mkt_app_tmpl := mktapp.NewTemplate()
-	mkt_app_tmpl.Add(keys.Name, mkt_app_name)
-	mkt_app_tmpl.Add(keys.Size, "1")
-	mkt_app_tmpl.SetType(mktapp.Image)
+var _ = Suite(&MarketPlaceAppSuite{})
 
-	//Create an image
+func (s *MarketPlaceAppSuite) SetUpSuite(c *C) {
+	// Create a marketplace
+	mkt_tmpl := marketplace.NewTemplate()
+	mkt_tmpl.Add(mktkeys.Name, "mkt-app-test")
+	mkt_tmpl.Add(mktkeys.MarketMAD, "http")
+	mkt_tmpl.Add(mktkeys.BaseUrl, "http://url/")
+	mkt_tmpl.Add(mktkeys.PublicDir, "/var/local/market-http")
 
+	id, err := testCtrl.MarketPlaces().Create(mkt_tmpl.String())
+
+	c.Assert(err, IsNil)
+	s.marketID = id
+
+	// Create an image
 	img_tmpl := image.NewTemplate()
 
 	img_tmpl.Add(imgkeys.Name, "test_img_go")
 	img_tmpl.Add(imgkeys.Size, "1")
 	img_tmpl.SetType(image.Datablock)
 
-	mkt_img_id, err = testCtrl.Images().Create(img_tmpl.String(), 1)
-	if err != nil {
-		t.Fatalf("Test failed:\n" + err.Error())
-	}
+	s.imgID, err = testCtrl.Images().Create(img_tmpl.String(), 1)
+	c.Assert(err, IsNil)
 
 	WaitResource(func() bool {
 		id, _ := testCtrl.Images().ByName("test_img_go")
@@ -61,64 +68,159 @@ func TestMarketplaceApp(t *testing.T) {
 
 		return state == image.Ready
 	})
+}
 
-	if err != nil {
-		t.Errorf("Test failed:\n" + err.Error())
-	}
+func (s *MarketPlaceAppSuite) TearDownSuite(c *C) {
+	// Delete image
+	err := testCtrl.Image(s.imgID).Delete()
+	c.Assert(err, IsNil)
 
-	mkt_app_tmpl.Add(keys.OriginID, strconv.Itoa(int(mkt_img_id)))
+	// Delete marketplace
+	err = testCtrl.MarketPlace(s.marketID).Delete()
+	c.Assert(err, IsNil)
+}
 
-	//Create a marketplace
+func (s *MarketPlaceAppSuite) SetUpTest(c *C) {
+	// Create Marketplace App
+	s.appName = "new_mkt_app"
 
-	mkt_tmpl := marketplace.NewTemplate()
-	mkt_tmpl.Add(mktkeys.Name, "mkt-app-test")
-	mkt_tmpl.Add(mktkeys.MarketMAD, "http")
-	mkt_tmpl.Add(mktkeys.BaseUrl, "http://url/")
-	mkt_tmpl.Add(mktkeys.PublicDir, "/var/loca/market-http")
+	mkt_app_tmpl := mktapp.NewTemplate()
+	mkt_app_tmpl.Add(keys.Name, s.appName)
+	mkt_app_tmpl.Add(keys.Size, "1")
+	mkt_app_tmpl.SetType(mktapp.Image)
+	mkt_app_tmpl.Add(keys.OriginID, s.imgID)
+	mkt_app_tmpl.Add(keys.MarketPlaceID, s.marketID)
 
-	market_id, err = testCtrl.MarketPlaces().Create(mkt_tmpl.String())
+	app_id, err := testCtrl.MarketPlaceApps().Create(mkt_app_tmpl.String(), s.marketID)
 
-	if err != nil {
-		t.Errorf("Test failed:\n" + err.Error())
-	}
+	c.Assert(err, IsNil)
+	s.appID = app_id
+}
 
-	mkt_app_tmpl.Add(keys.MarketPlaceID, strconv.Itoa(int(market_id)))
-	//Create MarketplaceApp
-	app_id, err := testCtrl.MarketPlaceApps().Create(mkt_app_tmpl.String(), int(market_id))
+func (s *MarketPlaceAppSuite) TearDownTest(c *C) {
+	// Delete Marketplace App
+	appC := testCtrl.MarketPlaceApp(s.appID)
+	appC.Delete()
 
-	if err != nil {
-		t.Errorf("Test failed:\n" + err.Error())
-	}
+	// Wait App deleted
+	WaitResource(func() bool {
+		_, err := appC.Info(false)
 
-	mkt_app, err = testCtrl.MarketPlaceApp(app_id).Info(false)
-	if err != nil {
-		t.Errorf("Test failed:\n" + err.Error())
-	}
+		return err != nil
+	})
+}
 
-	actual := mkt_app.Name
+func (s *MarketPlaceAppSuite) TestGetByNameAndID(c *C) {
+	// Get MarketPlace App by ID
+	app, err := testCtrl.MarketPlaceApp(s.appID).Info(false)
 
-	if actual != mkt_app_name {
-		t.Errorf("Test failed, expected: '%s', got:  '%s'", mkt_app_name, actual)
-	}
+	c.Assert(err, IsNil)
+	c.Assert(app.ID, Equals, s.appID)
+	c.Assert(app.Name, Equals, s.appName)
 
-	//Delete MarketplaceApp
-	err = testCtrl.MarketPlaceApp(app_id).Delete()
+	state, err := app.State()
+	c.Assert(err, IsNil)
+	c.Assert(state, Equals, mktapp.Locked);
 
-	if err != nil {
-		t.Errorf("Test failed:\n" + err.Error())
-	}
+	// Get MarketPlace App by Name
+	id, err := testCtrl.MarketPlaceApps().ByName(s.appName)
+	c.Assert(err, IsNil)
+	c.Assert(id, Equals, s.appID)
+}
 
-	//delete image
-	err = testCtrl.Image(mkt_img_id).Delete()
+func (s *MarketPlaceAppSuite) TestUpdate(c *C) {
+	appC := testCtrl.MarketPlaceApp(s.appID)
+	err := appC.Update(`DESCRIPTION = "Testing"`, parameters.Merge)
 
-	if err != nil {
-		t.Errorf("Test failed:\n" + err.Error())
-	}
+	c.Assert(err, IsNil)
 
-	//delete marketplace
-	err = testCtrl.MarketPlace(market_id).Delete()
+	app, err := testCtrl.MarketPlaceApp(s.appID).Info(false)
+	c.Assert(err, IsNil)
 
-	if err != nil {
-		t.Errorf("Test failed:\n" + err.Error())
-	}
+	descr, err := app.Template.Get(keys.Description)
+
+	c.Assert(err, IsNil)
+	c.Assert(descr, Equals, "Testing")
+}
+
+func (s *MarketPlaceAppSuite) TestRename(c *C) {
+	appC := testCtrl.MarketPlaceApp(s.appID)
+	appC.Rename("new_name")
+
+	app, err := testCtrl.MarketPlaceApp(s.appID).Info(false)
+	c.Assert(err, IsNil)
+	c.Assert(app.Name, Equals, "new_name");
+}
+
+func (s *MarketPlaceAppSuite) TestChown(c *C) {
+	// Test only if the call exists, no real change
+	appC := testCtrl.MarketPlaceApp(s.appID)
+	err := appC.Chown(1, 1)
+
+	c.Assert(err, IsNil)
+}
+
+func (s *MarketPlaceAppSuite) TestChmod(c *C) {
+	new_permissions := shared.Permissions{1, 1, 1, 1, 1, 1, 1, 1, 1}
+
+	appC := testCtrl.MarketPlaceApp(s.appID)
+
+	err := appC.Chmod(new_permissions)
+
+	c.Assert(err, IsNil)
+
+	app, err := appC.Info(false)
+
+	c.Assert(err, IsNil)
+	c.Assert(*app.Permissions, Equals, new_permissions);
+}
+
+func (s *MarketPlaceAppSuite) TestLock(c *C) {
+	// Lock
+	appC := testCtrl.MarketPlaceApp(s.appID)
+	err := appC.Lock(shared.LockUse)
+
+	c.Assert(err, IsNil)
+
+	app, err := appC.Info(false)
+
+	c.Assert(err, IsNil)
+	c.Assert(app.LockInfos.Locked, Equals, 1);
+
+	// Unlock
+	err = appC.Unlock()
+	c.Assert(err, IsNil)
+
+	app, err = appC.Info(false)
+
+	c.Assert(err, IsNil)
+	c.Assert(app.LockInfos, IsNil)
+}
+
+func (s *MarketPlaceAppSuite) TestEnable(c *C) {
+	id, err := testCtrl.MarketPlaceApps().ByName("Custom via netboot.xyz")
+	c.Assert(err, IsNil)
+
+	// Disable
+	appC := testCtrl.MarketPlaceApp(id)
+	err = appC.Enable(false)
+	c.Assert(err, IsNil)
+
+	app, err := appC.Info(false)
+	c.Assert(err, IsNil)
+
+	state, err := app.State()
+	c.Assert(err, IsNil)
+	c.Assert(state, Equals, mktapp.Disabled);
+
+	// Enable
+	err = appC.Enable(true)
+	c.Assert(err, IsNil)
+
+	app, err = appC.Info(false)
+	c.Assert(err, IsNil)
+
+	state, err = app.State()
+	c.Assert(err, IsNil)
+	c.Assert(state, Equals, mktapp.Ready);
 }
