5303923e794aa4ccd172de578c00b9d262dc93d1
Jan Orel <jorel@opennebula.systems>
2024-06-10 15:47:12 +0200

    M #-: Use one-apps for context download (#3101)
    Co-authored-by: Ruben S. Montero <rsmontero@opennebula.org>
    (cherry picked from commit 10664952de3aeec3b9935b8bff2d44da60e9e6a4)

--- share/context/download_context.sh
+++ share/context/download_context.sh
@@ -15,13 +15,11 @@ function get_tag_version {
     done
 }
 
-CONTEXT_API="https://api.github.com/repos/OpenNebula/addon-context-linux/releases"
-CONTEXT_API_WINDOWS="https://api.github.com/repos/OpenNebula/addon-context-windows/releases"
-
+CONTEXT_API="https://api.github.com/repos/OpenNebula/one-apps/releases"
 VERSION=`cat ../../src/im_mad/remotes/VERSION`
 
 ###############################################################################
-# Download linux packages
+# Download linux & windows packages
 ###############################################################################
 
 TAG_VERSION=`get_tag_version $CONTEXT_API`
@@ -36,18 +34,3 @@ TAG="v$TAG_VERSION"
 curl -s $CONTEXT_API | \
         jq -r --arg TAG "$TAG" '.[] | select(.tag_name == $TAG) | .assets[].browser_download_url' | \
 		xargs wget -P .
-
-###############################################################################
-# Download windows .msi
-###############################################################################
-
-TAG_VERSION=`get_tag_version $CONTEXT_API_WINDOWS`
-
-# If the current ONE version is greater than every context version the last one is retrieved
-if [ -z "$TAG_VERSION" ]; then
-    TAG_VERSION=`curl -s $CONTEXT_API_WINDOWS | jq -r '.[0].tag_name' | cut -d 'v' -f 2`
-fi
-
-TAG="v$TAG_VERSION"
-
-wget -P . https://github.com/OpenNebula/addon-context-windows/releases/download/$TAG/one-context-$TAG_VERSION.msi
