82f87e987ff228d2c439b87f404696176415a276
Jan Orel <jorel@opennebula.io>
2024-09-03 14:08:07 +0200

    B #6587: Switch to opennebula-augeas gem (#3079)
    Switch to OpenNebula fork of the discontinued augeas gem.

--- src/onedb/onedb.rb
+++ src/onedb/onedb.rb
@@ -127,15 +127,11 @@ class OneDB
 
     def read_credentials(ops)
         begin
-            # Suppress augeas require warning message
-            $VERBOSE = nil
-
-            gem 'augeas', '~> 0.6'
             require 'augeas'
         rescue Gem::LoadError
             STDERR.puts(
                 'Augeas gem is not installed, run `gem install ' \
-                'augeas -v \'0.6\'` to install it'
+                'opennebula-augeas` to install it'
             )
             exit(-1)
         end
