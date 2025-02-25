8a6a990c7372d63bff1c9b03705ed99686dd5bf9
josepselga <jselga@opennebula.io>
2025-02-25 13:40:28 +0100

    B OpenNebula/one#6666: Restic passwords: remove quotes and sanitize
    Signed-off-by: josepselga <jselga@opennebula.io>

--- src/datastore_mad/remotes/restic/restic.rb
+++ src/datastore_mad/remotes/restic/restic.rb
@@ -97,7 +97,7 @@ class Restic
 
         @restic_bin = RESTIC_BIN_PATHS[@options[:host_type]]
 
-        @passwd = @doc.elements["#{prefix}TEMPLATE/RESTIC_PASSWORD"].text.delete(%("'))
+        @passwd = @doc.elements["#{prefix}TEMPLATE/RESTIC_PASSWORD"].text.sub(/\A['"](.*)['"]\z/, '\1')
 
         @bwlimit = safe_get("#{prefix}TEMPLATE/RESTIC_BWLIMIT", -1)
         @maxproc = Integer(safe_get("#{prefix}TEMPLATE/RESTIC_MAXPROC", -1))
