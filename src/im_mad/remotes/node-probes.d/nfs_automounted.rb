76be9fc0f7d2ffc7763185cf12fc4bdd558932cd
Guillemo Ramos <gramos@opennebula.io>
2025-04-16 11:37:38 +0200

    F #6752: Add system probe to report AUTONFS_MOUNT (#3568)
    Signed-off-by: Guillermo Ramos <gramos@opennebula.io>

---
+++ src/im_mad/remotes/node-probes.d/nfs_automounted.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+# -------------------------------------------------------------------------- #
+# Copyright 2002-2025, OpenNebula Project, OpenNebula Systems                #
+#                                                                            #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
+# not use this file except in compliance with the License. You may obtain    #
+# a copy of the License at                                                   #
+#                                                                            #
+# http://www.apache.org/licenses/LICENSE-2.0                                 #
+#                                                                            #
+# Unless required by applicable law or agreed to in writing, software        #
+# distributed under the License is distributed on an "AS IS" BASIS,          #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+# See the License for the specific language governing permissions and        #
+# limitations under the License.                                             #
+#--------------------------------------------------------------------------- #
+
+require 'pathname'
+require 'rexml/document'
+
+xml_txt = STDIN.read
+config  = REXML::Document.new(xml_txt).root
+ds_loc  = config.elements['DATASTORE_LOCATION'].text.to_s
+
+shares = []
+Dir.glob("#{ds_loc}/*/.automounted").each do |dot_automounted|
+    path = Pathname.new(dot_automounted).dirname
+    share = `findmnt --noheadings --output SOURCE "#{path}"`.chomp
+    shares << share unless share.empty?
+end
+
+puts "AUTONFS_MOUNT = \"#{shares.join(',')}\""
