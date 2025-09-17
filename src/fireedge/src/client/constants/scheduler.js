a72bb5ba2214c7a7b4948638720aba088fb0589c
Ko <rcanny@opennebula.io>
2025-09-17 09:21:06 +0100

    B OpenNebula/one#7031 re-arrange time orders on vm creation (#3803)
    Signed-off-by: miracleheras <rcanny@opennebula.io>
    (cherry picked from commit 605ec525765c3661da920e51cbaa0adb758a257d)

--- src/fireedge/src/client/constants/scheduler.js
+++ src/fireedge/src/client/constants/scheduler.js
@@ -56,11 +56,11 @@ export const END_TYPE_VALUES = {
 
 /** @enum {string} Values to repeat an action */
 export const REPEAT_VALUES = {
-  DAILY: '-1',
-  WEEKLY: '0',
-  MONTHLY: '1',
-  YEARLY: '2',
-  HOURLY: '3',
+  HOURLY: '-1',
+  DAILY: '0',
+  WEEKLY: '1',
+  MONTHLY: '2',
+  YEARLY: '3',
 }
 
 /** {string} All days string */
