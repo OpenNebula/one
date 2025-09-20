378684229bd1b71ba29e51d932cbb9d20edac64c
Ruben S. Montero <rsmontero@opennebula.org>
2025-09-20 14:04:55 +0200

    M #-: Skip NUMA CPU cores information for non-cpu nodes
    For example in some ARM architectures

--- src/cli/one_helper/onehost_helper.rb
+++ src/cli/one_helper/onehost_helper.rb
@@ -768,11 +768,13 @@ class OneHostHelper < OpenNebulaHelper::OneHelper
     end
 
     def get_numa_cores(cores)
+        return unless cores
         ret  = ''
         free = 0
         used = 0
 
         [cores].flatten.each do |info|
+            next unless info['CUPS']
             core = info['CPUS'].split(',')
 
             core.uniq! do |c|
