d7977ea10a9afb5911194625b373c2d67f9b4762
Pavel Czerný <pczerny@opennebula.io>
2026-04-20 11:25:34 +0200

    B #7349: Correctly revert all changes in case of VM deploy failure (#4156)
    * New generic class to implement complex rollback logic (ScopeGuard)
    * Specialization for VirtualMachine API (DeployGuard)
    
    (cherry picked from commit 1521a12d4588a74608a20ec099d7256d8005036b)

---
+++ src/common/ScopeGuard.cc
@@ -0,0 +1,70 @@
+/* -------------------------------------------------------------------------- */
+/* Copyright 2002-2026, OpenNebula Project, OpenNebula Systems                */
+/*                                                                            */
+/* Licensed under the Apache License, Version 2.0 (the "License"); you may    */
+/* not use this file except in compliance with the License. You may obtain    */
+/* a copy of the License at                                                   */
+/*                                                                            */
+/* http://www.apache.org/licenses/LICENSE-2.0                                 */
+/*                                                                            */
+/* Unless required by applicable law or agreed to in writing, software        */
+/* distributed under the License is distributed on an "AS IS" BASIS,          */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   */
+/* See the License for the specific language governing permissions and        */
+/* limitations under the License.                                             */
+/* -------------------------------------------------------------------------- */
+
+#include "ScopeGuard.h"
+
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+
+one_util::ScopeGuard::ScopeGuard(std::function<void()> f)
+    : _f(std::move(f))
+    , _active(true)
+{
+}
+
+one_util::ScopeGuard::~ScopeGuard()
+{
+    if (_active && _f)
+    {
+        _f();
+    }
+}
+
+one_util::ScopeGuard::ScopeGuard(ScopeGuard&& other) noexcept
+    : _f(std::move(other._f))
+    , _active(other._active)
+{
+    other._active = false;
+}
+
+one_util::ScopeGuard& one_util::ScopeGuard::operator=(ScopeGuard&& other) noexcept
+{
+    if (this != &other)
+    {
+        if (_active && _f)
+        {
+            _f();
+        }
+
+        _f      = std::move(other._f);
+        _active = other._active;
+
+        other._active = false;
+    }
+
+    return *this;
+}
+
+void one_util::ScopeGuard::release()
+{
+    _active = false;
+}
+
+void one_util::ScopeGuard::set_callback(std::function<void()> f)
+{
+    _f      = std::move(f);
+    _active = true;
+}
